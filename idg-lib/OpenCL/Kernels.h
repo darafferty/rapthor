6c1ba9acb01640851d968f85c5190d94c3155f6e
Bram Veenboer <bram.veenboer@gmail.com>
2015-07-01 10:05:46 +0200

    Added Kernels files to hide complexity for launching kernels.

---
+++ idg-lib/OpenCL/Kernels.h
@@ -0,0 +1,53 @@
+#include <stdint.h>
+#include <complex.h>
+
+#include <iostream>
+
+#define __CL_ENABLE_EXCEPTIONS
+#include <CL/cl.hpp>
+
+#include "Types.h"
+
+
+/*
+    Gridder
+*/
+class KernelGridder {
+    public:
+        KernelGridder(cl::Program &program, const char *kernel_name);
+        void launchAsync(
+            cl::CommandQueue &queue, int jobsize, int bl_offset,
+            cl::Buffer &d_uvw, cl::Buffer &d_wavenumbers,
+            cl::Buffer &d_visibilities, cl::Buffer &d_spheroidal,
+            cl::Buffer &d_aterm, cl::Buffer &d_baselines,
+            cl::Buffer &d_subgrid);
+    	static uint64_t flops(int jobsize);
+		static uint64_t bytes(int jobsize);
+	
+	private:
+	    cl::Kernel kernel;
+};
+
+
+#if 0
+/*
+    FFT
+*/
+#define FFT_LAYOUT_YXP (-1)
+#define FFT_LAYOUT_PYX (+1)
+
+class KernelFFT {
+	public:
+        KernelFFT();
+        void plan(int size, int batch, int layout);
+        void launchAsync(cu::Stream &stream, cu::DeviceMemory &data, int direction);
+		static uint64_t flops(int size, int batch);
+		static uint64_t bytes(int size, int batch);
+
+    private:
+        int planned_size;
+        int planned_batch;
+        int planned_layout;
+        cufft::C2C_2D *fft;
+};
+#endif
