dd8eddb26c1048b4ac868dc270b762133960172b
Bram Veenboer <bram.veenboer@gmail.com>
2015-07-07 14:29:36 +0200

    Added initial Degridder kernel.

--- idg-lib/OpenCL/Kernels.cpp
+++ idg-lib/OpenCL/Kernels.cpp
@@ -58,6 +58,64 @@ uint64_t KernelGridder::bytes(int jobsize) {
 }
 
 
+KernelDegridder::KernelDegridder(cl::Program &program, const char *kernel_name) :
+    counter("degridder"),
+    kernel(program, kernel_name) {}
+
+void KernelDegridder::launchAsync(
+    cl::CommandQueue &queue, int jobsize, int bl_offset,
+    cl::Buffer &d_uvw, cl::Buffer &d_wavenumbers,
+    cl::Buffer &d_visibilities, cl::Buffer &d_spheroidal,
+    cl::Buffer &d_aterm, cl::Buffer &d_baselines,
+    cl::Buffer &d_subgrid) {
+    int wgSize = 128;
+    cl::NDRange globalSize(jobsize * wgSize, 1);
+    cl::NDRange localSize(wgSize, 1);
+    kernel.setArg(0, bl_offset);
+    kernel.setArg(1, d_subgrid);
+    kernel.setArg(2, d_uvw);
+    kernel.setArg(3, d_wavenumbers);
+    kernel.setArg(4, d_aterm);
+    kernel.setArg(5, d_baselines);
+    kernel.setArg(6, d_spheroidal);
+    kernel.setArg(7, d_visibilities);
+    try {
+        cl::Event event;
+        queue.enqueueNDRangeKernel(kernel, cl::NullRange, globalSize, localSize, NULL, &event);
+        counter.doOperation(event, flops(jobsize), bytes(jobsize));
+    } catch (cl::Error &error) {
+        std::cerr << "Error launching degridder: " << error.what() << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+uint64_t KernelDegridder::flops(int jobsize) {
+    return 1ULL * jobsize * SUBGRIDSIZE * SUBGRIDSIZE * (
+    // ATerm
+    NR_POLARIZATIONS * 32 +
+    // Spheroidal
+    NR_POLARIZATIONS * 2 +
+    // LMN
+    14 +
+    // Phase
+    10 +
+    // Phasor
+    NR_TIME * NR_CHANNELS * 4 +
+    // Degrid
+    NR_TIME * NR_CHANNELS * NR_POLARIZATIONS * 8);
+}
+
+uint64_t KernelDegridder::bytes(int jobsize) {
+    return 1ULL * jobsize * (
+    // ATerm
+    2 * SUBGRIDSIZE * SUBGRIDSIZE * NR_POLARIZATIONS * sizeof(FLOAT_COMPLEX) +
+    // UV grid
+    SUBGRIDSIZE * SUBGRIDSIZE * NR_POLARIZATIONS * sizeof(FLOAT_COMPLEX) +
+    // Visibilities
+    NR_TIME * NR_CHANNELS * NR_POLARIZATIONS * sizeof(FLOAT_COMPLEX));
+}
+
+
 KernelFFT::KernelFFT() : counter("fft") {
     uninitialized = true;
 }
