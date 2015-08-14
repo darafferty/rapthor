be2a8cd1aff12060f33e6052f825c27742b0dc94
Bram Veenboer <bram.veenboer@gmail.com>
2015-08-14 09:40:58 +0200

    Removed everything except for the Sandbox folder. This will be the base for the new master branch.

--- idg-bin/Common/Memory.h
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-
-size_t free_memory() {
-    long pages = sysconf(_SC_PHYS_PAGES);
-    long page_size = sysconf(_SC_PAGE_SIZE);
-    return pages * page_size;
-}
--- idg-bin/External/README.md
+++
@@ -1,2 +0,0 @@
-# Code from external sources (used to in Lib folder)
-
