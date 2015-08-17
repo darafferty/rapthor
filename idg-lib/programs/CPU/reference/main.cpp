8ebca05fcecd9204328e327b4d27f69bdc74659d
Bram Veenboer <bram.veenboer@gmail.com>
2015-08-17 13:38:43 +0200

    Resolved some parameter related bugs. The main program now reads parameters from environmental variables.

--- idg-lib/programs/CPU/reference/main.cpp
+++ idg-lib/programs/CPU/reference/main.cpp
@@ -12,38 +12,19 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  // Get parameters passed as arguments
-  int nr_stations = 0;
-  int nr_time = 0;
-  int nr_channels = 0;
-  int w_planes = 0;
-  int gridsize = 0;
-  int subgridsize = 0;
-  int chunksize = 0;
-  int jobsize = 0;
-  idg::get_parameters(argc, argv, &nr_stations, &nr_time, &nr_channels, 
-                      &w_planes, &gridsize, &subgridsize, &chunksize, &jobsize);
-
-  // Set other parameters
-  int nr_polarizations = 4;
-
   // Set constants explicitly in the parameters parameter
   clog << ">>> Configuration"  << endl;
-  idg::Parameters params;    
-  params.set_nr_stations(nr_stations);
-  params.set_nr_timesteps(nr_time);
-  params.set_nr_channels(nr_channels);
-  params.set_grid_size(gridsize);
+  idg::Parameters params;
+  params.read_parameters_from_env();
 
   // retrieve constants for memory allocation
-  nr_stations = params.get_nr_stations();
-  auto nr_baselines = params.get_nr_baselines();
-  nr_time = params.get_nr_timesteps();
-  nr_channels = params.get_nr_channels();
-  gridsize = params.get_grid_size();
-
-  //  int jobsize = algparams.get_job_size();
-  subgridsize = params.get_subgrid_size();
+  int nr_stations = params.get_nr_stations();
+  int nr_baselines = params.get_nr_baselines();
+  int nr_time = params.get_nr_timesteps();
+  int nr_channels = params.get_nr_channels();
+  int gridsize = params.get_grid_size();
+  int subgridsize = params.get_subgrid_size();
+  int nr_polarizations = 4;
 
   // Print configuration
   clog << params;
@@ -71,7 +52,7 @@ int main(int argc, char *argv[])
   idg::init_visibilities(visibilities, nr_baselines, nr_time, nr_channels, 
                          nr_polarizations);
   idg::init_uvw(uvw, nr_stations, nr_baselines, nr_time, gridsize, 
-                subgridsize, w_planes);
+                subgridsize, 0);
   idg::init_wavenumbers(wavenumbers, nr_channels);
   idg::init_aterm(aterm, nr_stations, nr_polarizations, subgridsize);
   idg::init_spheroidal(spheroidal, subgridsize);
