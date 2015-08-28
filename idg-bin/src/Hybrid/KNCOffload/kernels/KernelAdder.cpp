7e829c5b25e89dcfd1c3d1618df6ec1144cab775
Matthias Petschow <m.petschow@googlemail.com>
2015-08-28 15:39:46 +0200

    KNC offload code running all gridding routines, only actual gridding offloaded to device though.

--- idg-bin/src/Hybrid/KNCOffload/kernels/KernelAdder.cpp
+++ idg-bin/src/Hybrid/KNCOffload/kernels/KernelAdder.cpp
@@ -1,3 +1,5 @@
+#pragma omp declare target
+
 #include <complex>
 
 #include <stdio.h>
@@ -6,31 +8,44 @@
 
 #include "Types.h"
 
+namespace idg {
 
-extern "C" {
 void kernel_adder(
     const int jobsize,
-    const MetadataType __restrict__ *metadata,
-    const SubGridType  __restrict__ *subgrid,
-    GridType           __restrict__ *grid
-    ) {
+    const void *_metadata,
+    const void *_subgrid,
+          void *_grid,
+    const int gridsize,
+    const int subgridsize,
+    const int nr_polarizations)
+    {
+    TYPEDEF_BASELINE
+    TYPEDEF_COORDINATE
+    TYPEDEF_METADATA
+    TYPEDEF_METADATA_TYPE
+    TYPEDEF_SUBGRID_TYPE
+    TYPEDEF_GRID_TYPE
+    
+    MetadataType *metadata = (MetadataType *) _metadata;
+    SubGridType *subgrid = (SubGridType *) _subgrid;
+    GridType *grid = (GridType *) _grid;
 
     #pragma omp parallel for
-    for (int pol = 0; pol < NR_POLARIZATIONS; pol++) {
+    for (int pol = 0; pol < nr_polarizations; pol++) {
         for (int s = 0; s < jobsize; s++) {
             // Load position in grid
             int grid_x = metadata[s]->coordinate.x;
             int grid_y = metadata[s]->coordinate.y;
 
             // Check wheter subgrid fits in grid
-            if (grid_x >= 0 && grid_x < GRIDSIZE-SUBGRIDSIZE &&
-                grid_y >= 0 && grid_y < GRIDSIZE-SUBGRIDSIZE) {
+            if (grid_x >= 0 && grid_x < gridsize-subgridsize &&
+                grid_y >= 0 && grid_y < gridsize-subgridsize) {
 
-                for (int y = 0; y < SUBGRIDSIZE; y++) {
-                    for (int x = 0; x < SUBGRIDSIZE; x++) {
+                for (int y = 0; y < subgridsize; y++) {
+                    for (int x = 0; x < subgridsize; x++) {
                         // Compute shifted position in subgrid
-                        int x_src = (x + (SUBGRIDSIZE/2)) % SUBGRIDSIZE;
-                        int y_src = (y + (SUBGRIDSIZE/2)) % SUBGRIDSIZE;
+                        int x_src = (x + (subgridsize/2)) % subgridsize;
+                        int y_src = (y + (subgridsize/2)) % subgridsize;
 
                         // Add subgrid value to grid
                         (*grid)[pol][grid_y+y][grid_x+x] += (*subgrid)[s][pol][y_src][x_src];
@@ -41,8 +56,8 @@ void kernel_adder(
     }
 }
 
-uint64_t kernel_adder_flops(int jobsize) {
-    return 1ULL * jobsize * SUBGRIDSIZE * SUBGRIDSIZE * (
+    uint64_t kernel_adder_flops(int jobsize, int subgridsize) {
+    return 1ULL * jobsize * subgridsize * subgridsize * (
     // Shift
     8 +
     // Add
@@ -50,11 +65,14 @@ uint64_t kernel_adder_flops(int jobsize) {
     );
 }
 
-uint64_t kernel_adder_bytes(int jobsize) {
-    return 1ULL * jobsize * SUBGRIDSIZE * SUBGRIDSIZE * (
+    uint64_t kernel_adder_bytes(int jobsize, int subgridsize, int nr_polarizations) {
+    return 1ULL * jobsize * subgridsize * subgridsize * (
     // Coordinate
     2 * sizeof(unsigned) +
     // Pixels
-    3 * NR_POLARIZATIONS * sizeof(FLOAT_COMPLEX));
+    3 * nr_polarizations * sizeof(FLOAT_COMPLEX));
 }
-}
+
+} // end namespace idg
+
+#pragma omp end declare target
