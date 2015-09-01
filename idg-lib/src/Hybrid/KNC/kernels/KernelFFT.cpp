082799c0d74730bf40184ba37d9313aeaad93f5b
Bram Veenboer <bram.veenboer@gmail.com>
2015-09-01 14:11:49 +0200

    Hybrid KNC optimization: different parallelization strategy for fft work distribution

--- idg-lib/src/Hybrid/KNC/kernels/KernelFFT.cpp
+++ idg-lib/src/Hybrid/KNC/kernels/KernelFFT.cpp
@@ -21,36 +21,39 @@ void kernel_fft(
     int nr_polarizations)
     {
 
-        fftwf_complex __restrict__ *data = (fftwf_complex *) _data;
+        #pragma omp parallel for
+        for (int i = 0; i < batch; i++) {
+            fftwf_complex __restrict__ *data = (fftwf_complex *) _data + (nr_polarizations * size * size);
 
-        // 2D FFT
-        int rank = 2;
+            // 2D FFT
+            int rank = 2;
     
-        // For grids of size*size elements
-        int n[] = {size, size};
-        
-        // Set stride
-        int istride = 1;
-        int ostride = istride;
+            // For grids of size*size elements
+            int n[] = {size, size};
+            
+            // Set stride
+            int istride = 1;
+            int ostride = istride;
     
-        // Set dist
-        int idist = n[0] * n[1];
-        int odist = idist;
+            // Set dist
+            int idist = n[0] * n[1];
+            int odist = idist;
 
-        // Planner flags
-        int flags = FFTW_ESTIMATE;
-        
-        // Create plan
-        fftwf_plan plan = fftwf_plan_many_dft(
-            rank, n, batch * nr_polarizations, data, n,
-            istride, idist, data, n,
-            ostride, odist, sign, flags);
+            // Planner flags
+            int flags = FFTW_ESTIMATE;
+            
+            // Create plan
+            fftwf_plan plan = fftwf_plan_many_dft(
+                rank, n, nr_polarizations, data, n,
+                istride, idist, data, n,
+                ostride, odist, sign, flags);
 
-        // Execute FFTs
-        fftwf_execute_dft(plan, data, data);
-        
-        // Destroy plan
-        fftwf_destroy_plan(plan);
+            // Execute FFTs
+            fftwf_execute_dft(plan, data, data);
+            
+            // Destroy plan
+            fftwf_destroy_plan(plan);
+        }
     }
 
     uint64_t kernel_fft_flops(int size, int batch, int nr_polarizations) {
