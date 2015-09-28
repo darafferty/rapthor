80b5a0b4f8cab8524ffa86ebb40f18a294959bec
Bram Veenboer <bram.veenboer@gmail.com>
2015-09-28 14:28:00 +0200

    Added OpenCL data initialization

--- idg-lib/examples/OpenCL/Reference/main.cpp
+++ idg-lib/examples/OpenCL/Reference/main.cpp
@@ -69,74 +69,61 @@ int main(int argc, char *argv[]) {
 
     // Show OpenCL devices
     printDevices(deviceNumber);
-#if 0
 
     // Allocate data structures
     clog << ">>> Allocate data structures" << endl;
-    auto size_visibilities = 1ULL * nr_baselines*nr_timesteps*nr_timeslots*nr_channels*nr_polarizations;
-    auto size_uvw = 1ULL * nr_baselines*nr_timesteps*nr_timeslots*3;
-    auto size_wavenumbers = 1ULL * nr_channels;
-    auto size_aterm = 1ULL * nr_stations*nr_timeslots*nr_polarizations*subgridsize*subgridsize;
-    auto size_spheroidal = 1ULL * subgridsize*subgridsize;
-    auto size_grid = 1ULL * nr_polarizations*gridsize*gridsize;
-    auto size_metadata = 1ULL * nr_subgrids*5;
-    auto size_subgrids = 1ULL * nr_subgrids*nr_polarizations*subgridsize*subgridsize;
+    auto size_visibilities = 1ULL * nr_baselines*nr_timesteps*nr_timeslots*nr_channels*nr_polarizations*sizeof(complex<float>);
+    auto size_uvw = 1ULL * nr_baselines*nr_timesteps*nr_timeslots*3*sizeof(float);
+    auto size_wavenumbers = 1ULL * nr_channels*sizeof(float);
+    auto size_aterm = 1ULL * nr_stations*nr_timeslots*nr_polarizations*subgridsize*subgridsize*sizeof(complex<float>);
+    auto size_spheroidal = 1ULL * subgridsize*subgridsize*sizeof(float);
+    auto size_grid = 1ULL * nr_polarizations*gridsize*gridsize*sizeof(complex<float>);
+    auto size_metadata = 1ULL * nr_subgrids*5*sizeof(int);
+    auto size_subgrids = 1ULL * nr_subgrids*nr_polarizations*subgridsize*subgridsize*sizeof(complex<float>);
 
-    cu::HostMemory h_visibilities(sizeof(complex<float>) * size_visibilities);
-    cu::HostMemory h_uvw(sizeof(float) * size_uvw);
-    cu::DeviceMemory d_wavenumbers(sizeof(float) * size_wavenumbers);
-    cu::DeviceMemory d_aterm(sizeof(complex<float>) * size_aterm);
-    cu::DeviceMemory d_spheroidal(sizeof(float) * size_spheroidal);
-    cu::HostMemory h_grid(sizeof(complex<float>) * size_grid);
-    cu::HostMemory h_metadata(sizeof(int) * size_metadata);
-    cu::HostMemory h_subgrids(sizeof(complex<float>) * size_subgrids);
+    cl::Buffer h_visibilities = cl::Buffer(context, CL_MEM_ALLOC_HOST_PTR, size_visibilities, NULL);
+    cl::Buffer h_uvw          = cl::Buffer(context, CL_MEM_ALLOC_HOST_PTR, size_uvw,          NULL);
+    cl::Buffer d_wavenumbers  = cl::Buffer(context, CL_MEM_READ_WRITE, size_wavenumbers,      NULL);
+    cl::Buffer d_aterm        = cl::Buffer(context, CL_MEM_READ_WRITE, size_aterm,            NULL);
+    cl::Buffer d_spheroidal   = cl::Buffer(context, CL_MEM_READ_WRITE, size_spheroidal,       NULL);
+    cl::Buffer h_grid         = cl::Buffer(context, CL_MEM_ALLOC_HOST_PTR, size_grid,         NULL);
+    cl::Buffer h_metadata     = cl::Buffer(context, CL_MEM_ALLOC_HOST_PTR, size_metadata,     NULL);
+    cl::Buffer h_subgrids     = cl::Buffer(context, CL_MEM_ALLOC_HOST_PTR, size_subgrids,     NULL);
 
-    clog << ">>> Initialize data structures" << endl;
-    void *wavenumbers = idg::init_wavenumbers(nr_channels);
-    void *aterm       = idg::init_aterm(nr_stations, nr_timeslots, nr_polarizations, subgridsize);
-    void *spheroidal  = idg::init_spheroidal(subgridsize);
-    void *grid        = idg::init_grid(gridsize, nr_polarizations);
-    idg::init_visibilities(h_visibilities, nr_baselines, nr_timesteps*nr_timeslots, nr_channels, nr_polarizations);
-    idg::init_uvw(h_uvw, nr_stations, nr_baselines, nr_timesteps*nr_timeslots, gridsize, subgridsize);
-    idg::init_metadata(h_metadata, h_uvw, wavenumbers, nr_stations, nr_baselines, nr_timesteps, nr_timeslots, nr_channels, gridsize, subgridsize, imagesize);
-    d_wavenumbers.set(wavenumbers);
-    d_aterm.set(aterm);
-    d_spheroidal.set(spheroidal);
-
+    // Initialize data structures
+    std::clog << ">>> Initialize data structures" << std::endl;
+    void *visibilities = idg::init_visibilities(nr_baselines, nr_timesteps*nr_timeslots, nr_channels, nr_polarizations);
+    void *uvw          = idg::init_uvw(nr_stations, nr_baselines, nr_timesteps*nr_timeslots, gridsize, subgridsize);
+    void *wavenumbers  = idg::init_wavenumbers(nr_channels);
+    void *aterm        = idg::init_aterm(nr_stations, nr_timeslots, nr_polarizations, subgridsize);
+    void *spheroidal   = idg::init_spheroidal(subgridsize);
+    void *metadata     = idg::init_metadata(uvw, wavenumbers, nr_stations, nr_baselines, nr_timesteps, nr_timeslots, nr_channels, gridsize, subgridsize, imagesize);
+    queue.enqueueWriteBuffer(d_wavenumbers,  CL_TRUE, 0, size_wavenumbers,  wavenumbers,   0, NULL);
+    queue.enqueueWriteBuffer(d_aterm,        CL_TRUE, 0, size_aterm,        aterm,         0, NULL);
+    queue.enqueueWriteBuffer(d_spheroidal,   CL_TRUE, 0, size_spheroidal,   spheroidal,    0, NULL);
+    queue.enqueueWriteBuffer(h_metadata,     CL_TRUE, 0, size_metadata,     metadata,      0, NULL);
+    queue.enqueueWriteBuffer(h_visibilities, CL_TRUE, 0, size_visibilities, visibilities,  0, NULL);
+    queue.enqueueWriteBuffer(h_uvw,          CL_TRUE, 0, size_uvw,          uvw,           0, NULL);
+ 
     // Initialize interface to kernels
     clog << ">>> Initialize proxy" << endl;
-    idg::proxy::cuda::Reference cuda(params, deviceNumber);
+    idg::proxy::opencl::Reference opencl(params, deviceNumber);
     clog << endl;
-
-    // Start profiling
-    cuProfilerStart();
 
     // Run gridder
     clog << ">>> Run gridder" << endl;
-    cuda.grid_onto_subgrids(context, nr_subgrids, 0, h_uvw, d_wavenumbers, h_visibilities, d_spheroidal, d_aterm, h_metadata, h_subgrids);
-
-//    clog << ">> Run adder" << endl;
-//    int jobsize_adder = params.get_job_size_adder();
-//    cuda.add_subgrids_to_grid(jobsize_adder, nr_subgrids, metadata, subgrids, grid);
-//
-//    clog << ">>> Run fft" << endl;
-//    cuda.transform(idg::FourierDomainToImageDomain, grid);
-//
-//    clog << ">>> Run splitter" << endl;
-//    int jobsize_splitter = params.get_job_size_splitter();
-//    cuda.split_grid_into_subgrids(jobsize_splitter, nr_subgrids, metadata, subgrids, grid);
+    opencl.grid_onto_subgrids(context, nr_subgrids, 0, h_uvw, d_wavenumbers, h_visibilities, d_spheroidal, d_aterm, h_metadata, h_subgrids);
 
     clog << ">>> Run degridder" << endl;
-    cuda.degrid_from_subgrids(context, nr_subgrids, 0, h_uvw, d_wavenumbers, h_visibilities, d_spheroidal, d_aterm, h_metadata, h_subgrids);
-
-    // Stop profiling
-    cuProfilerStop();
+    opencl.degrid_from_subgrids(context, nr_subgrids, 0, h_uvw, d_wavenumbers, h_visibilities, d_spheroidal, d_aterm, h_metadata, h_subgrids);
 
     // free memory for data structures
+    free(visibilities);
+    free(uvw);
     free(wavenumbers);
     free(aterm);
     free(spheroidal);
+    free(metadata);
 
-#endif
     return EXIT_SUCCESS;
 }
