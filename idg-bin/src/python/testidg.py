c8795e9f4327cc287c431dd5a6da5db86cacb8aa
Bas van der Tol <basvandertol@gmail.com>
2015-09-30 14:36:25 +0000

    First working demo (testidg.py) of python bindings...

--- idg-bin/src/python/testidg.py
+++ idg-bin/src/python/testidg.py
@@ -4,6 +4,7 @@ import numpy
 import time
 import idg
 import pyrap.tables
+import matplotlib.pyplot as plt
 
 
 class BaselineBuffer :
@@ -11,7 +12,7 @@ class BaselineBuffer :
     self.antenna1 = antenna1
     self.antenna2 = antenna2
     self.count = 0
-    self.uvw = numpy.zeros((parameters.nr_timesteps, 3), dtype = float32)
+    self.uvw = numpy.zeros((parameters.nr_timesteps, 3), dtype = numpy.float32)
     self.visibilities =  numpy.zeros((parameters.nr_timesteps, p.nr_channels, p.nr_polarizations), dtype = numpy.complex64)
     self.N_timesteps = parameters.nr_timesteps
     
@@ -36,15 +37,15 @@ class DataBuffer :
     N_ant = parameters.nr_stations
     N_timesteps = parameters.nr_timesteps
     
-    self.uvw = numpy.zeros((nr_subgrids, p.nr_timesteps, 3), dtype = float32)
+    self.uvw = numpy.zeros((nr_subgrids, p.nr_timesteps, 3), dtype = numpy.float32)
     
     speed_of_light = 299792458.0
-    self.wavelengths = speed_of_light / freqs
-    self.wavenumbers = 2*numpy.pi / self.wavelengths
+    self.wavelengths = numpy.array(speed_of_light / freqs, dtype=numpy.float32)
+    self.wavenumbers = numpy.array(2*numpy.pi / self.wavelengths, dtype=numpy.float32)
 
     self.visibilities =  numpy.zeros((nr_subgrids, p.nr_timesteps, p.nr_channels, p.nr_polarizations), dtype = numpy.complex64)
 
-    self.spheroidal = numpy.ones((p.subgrid_size, p.subgrid_size), dtype = float32)
+    self.spheroidal = numpy.ones((p.subgrid_size, p.subgrid_size), dtype = numpy.float32)
 
     self.aterm = numpy.zeros((p.nr_stations, p.nr_timeslots, p.nr_polarizations, p.subgrid_size, p.subgrid_size), dtype = numpy.complex64)
 
@@ -52,6 +53,8 @@ class DataBuffer :
     self.aterm[:,:,3,:,:] = 1.0
 
     self.subgrids = numpy.zeros((nr_subgrids, p.nr_polarizations, p.subgrid_size, p.subgrid_size), dtype = numpy.complex64)
+
+    self.grid = numpy.zeros((p.nr_polarizations, p.grid_size, p.grid_size), dtype = numpy.complex64)
 
     baselinetype = numpy.dtype([('station1', numpy.int32), ('station2', numpy.int32)])
     coordinatetype = numpy.dtype([('x', numpy.int32), ('y', numpy.int32)])
@@ -67,16 +70,19 @@ class DataBuffer :
         self.baselinebuffers[i,j] = BaselineBuffer(i,j,parameters)
   
   def append(self, row):
+    if row['ANTENNA1'] == row['ANTENNA2']:
+      return
     baselinebuffer = self.baselinebuffers[row['ANTENNA1'], row['ANTENNA2']]
     if baselinebuffer.append(row) :
-      self.append_subgrid(baselinebuffer)
+      r = self.append_subgrid(baselinebuffer)
       baselinebuffer.clear()
+      return r
       
   def append_subgrid(self, baselinebuffer):
     u = numpy.mean(baselinebuffer.uvw[:,0])*numpy.mean(self.wavenumbers)/(2*numpy.pi)
     v = numpy.mean(baselinebuffer.uvw[:,1])*numpy.mean(self.wavenumbers)/(2*numpy.pi)
-    coordinate_x = int(u * self.parameters.imagesize) + self.parameters.grid_size/2 - self.parameters.subgrid_size/2
-    coordinate_y = int(v * self.parameters.imagesize) + self.parameters.grid_size/2 - self.parameters.subgrid_size/2
+    coordinate_x = round(u * self.parameters.imagesize) + self.parameters.grid_size/2 - self.parameters.subgrid_size/2
+    coordinate_y = round(v * self.parameters.imagesize) + self.parameters.grid_size/2 - self.parameters.subgrid_size/2
     
     if ((coordinate_x>=0) and 
       (coordinate_y>=0) and 
@@ -90,6 +96,7 @@ class DataBuffer :
       self.count += 1
       if self.count == self.nr_subgrids:
         self.flush()
+        return True
     
   
   def flush(self):
@@ -106,6 +113,24 @@ class DataBuffer :
       self.aterm, 
       self.metadata, 
       self.subgrids)
+    
+    proxy.add_subgrids_to_grid(
+      jobsize,
+      self.count,
+      self.metadata,
+      self.subgrids,
+      self.grid)
+    print "***"
+    plt.ion()
+    plt.figure(1)
+    plt.clf()
+    subplot(1,2,1)
+    plt.imshow(log(abs(self.grid[0,:,:])))
+    subplot(1,2,2)
+    plt.imshow(real(fft.fftshift(fft.fft2(fft.fftshift(databuffer.grid[0,:,:])))))
+    plt.show()
+    plt.draw()
+    
     self.count = 0
     
   
@@ -113,16 +138,16 @@ p = idg.Parameters()
 
 w_offset = 0.0
 
-nr_subgrids = 100000
+nr_subgrids = 2000
 
-#t = pyrap.tables.taql('SELECT TIME, ANTENNA1, ANTENNA2 FROM /home/vdtol/cep1home/imagtest2/COV.beam_on.one.MS')
-t = pyrap.tables.table('/home/vdtol/cep1home/imagtest2/COV.beam_on.one.MS')
+#t = pyrap.tables.table('/home/vdtol/cep1home/imagtest2/COV.beam_on.one.MS')
+t = pyrap.tables.table('/data/scratch/vdtol/idg_test1/RX42_SB100-109.2ch10s.ms')
 t_ant = pyrap.tables.table(t.getkeyword("ANTENNA"))
 t_spw = pyrap.tables.table(t.getkeyword("SPECTRAL_WINDOW"))
 freqs = t_spw[0]['CHAN_FREQ']
 p.nr_stations = len(t_ant)
 p.nr_channels = t[0]["CORRECTED_DATA"].shape[0]
-p.nr_timesteps = 1
+p.nr_timesteps = 10
 p.nr_timeslots = 1
 p.imagesize = 0.1
 p.grid_size = 1000
@@ -142,24 +167,24 @@ N = 1000
 t0 = time.time()
 
 rowtype = numpy.dtype([
-  ('TIME', float32), 
+  ('TIME', numpy.float32), 
   ('ANTENNA1', int), 
   ('ANTENNA2', int), 
-  ('UVW', float32, (3,)),
+  ('UVW', numpy.float32, (3,)),
   ('DATA', complex, (p.nr_channels, p.nr_polarizations))
 ])
 
 block = numpy.zeros(N, dtype = rowtype)
 
-#for i in range(t.nrows()):
-for i in range(10000):
+for i in range(t.nrows()):
+#for i in range(800000):
   if (j == 0):
     block = block[0:min(t.nrows() - i, N)]
     block[:]['TIME'] = t.getcol('TIME', startrow = i, nrow = N)
     block[:]['ANTENNA1'] = t.getcol('ANTENNA1', startrow = i, nrow = N)
     block[:]['ANTENNA2'] = t.getcol('ANTENNA2', startrow = i, nrow = N)
     block[:]['UVW'] = t.getcol('UVW', startrow = i, nrow = N)
-    block[:]['DATA'] = t.getcol('CORRECTED_DATA', startrow = i, nrow = N)
+    block[:]['DATA'] = t.getcol('CORRECTED_DATA', startrow = i, nrow = N) * -t.getcol('FLAG', startrow = i, nrow = N)
   databuffer.append(block[j])
   j += 1
   if j == N:
@@ -169,7 +194,13 @@ t1 = time.time()
 databuffer.flush()
 print t1-t0
 
-  
+
+
+#for i in range(150,160):
+  #plt.figure()
+  #plt.imshow(numpy.fft.fftshift(abs(databuffer.subgrids[i,0,:,:])), interpolation='nearest')
+#plt.show()
+
 
 
 #proxy.grid_onto_subgrids(jobsize, nr_subgrids, w_offset, uvw, wavenumbers, visibilities, spheroidal, aterm, metadata, subgrids)
