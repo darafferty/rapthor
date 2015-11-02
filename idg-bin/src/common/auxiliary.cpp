8382df7f3149b6c78bbbe6300cd4e9cc7aee7646
Bram Veenboer <bram.veenboer@gmail.com>
2015-11-02 15:22:45 +0100

    Improved performance reporting for HaswellEP. The output is more consistent, hence easier to parse.

--- idg-bin/src/common/auxiliary.cpp
+++ idg-bin/src/common/auxiliary.cpp
@@ -11,43 +11,40 @@ using namespace std;
 namespace idg {
     namespace auxiliary {
 
+        #define FW1 14
+        #define FW2 10
 
-        void report(const char *name,
-              double runtime,
-              uint64_t flops,
-              uint64_t bytes,
-              double watt)
+        void report(
+            const char *name,
+            double runtime,
+            uint64_t flops,
+            uint64_t bytes,
+            double watt)
         {
-            const int fw1 = 12;
-            const int fw2 = 10;
-            const int fw3 = 10;
-
-            string name_str(name);
-
             #pragma omp critical (clog)
             {
-                clog << setw(fw1) << left << name_str + ": "
-                     << setw(fw2) << right << scientific << setprecision(4)
+                clog << setw(FW1) << left << string(name) + ": "
+                     << setw(FW2) << right << scientific << setprecision(4)
                      << runtime << " s";
                 if (flops != 0) {
                     clog << ", ";
                     double gflops = (flops / runtime) * 1e-9;
-                        clog << setw(fw2) << right << fixed << setprecision(2)
+                        clog << setw(FW2) << right << fixed << setprecision(2)
                                           << gflops << " GFLOPS";
                 }
                 if (bytes != 0) {
                     clog << ", ";
-                    clog << setw(fw3) << right << fixed << setprecision(2)
+                    clog << setw(FW2) << right << fixed << setprecision(2)
                                       << bytes / runtime * 1e-9 << " GB/s";
                 }
                 if  (watt != 0) {
                     clog << ", ";
-                    clog << setw(fw3) << right << fixed << setprecision(2)
+                    clog << setw(FW2) << right << fixed << setprecision(2)
                                       << watt << " Watt";
                 }
                 if (flops != 0 && watt != 0) {
                     clog << ", ";
-                    clog << setw(fw3) << right << fixed << setprecision(2)
+                    clog << setw(FW2) << right << fixed << setprecision(2)
                                       << (flops / runtime * 1e-9) / watt << " GFLOPS/W";
                 }
             }
@@ -56,40 +53,60 @@ namespace idg {
 
         void report_runtime(double runtime)
         {
-            const int fw1 = 12;
-            const int fw2 = 10;
-
-            clog << setw(fw1) << left << "runtime: "
-                 << setw(fw2) << right << scientific << setprecision(4)
+            clog << setw(FW1) << left << "runtime: "
+                 << setw(FW2) << right << scientific << setprecision(4)
                  << runtime << " s" << endl;
         }
 
-        void report_visibilities(double runtime,
-                       uint64_t nr_baselines,
-                       uint64_t nr_time,
-                       uint64_t nr_channels)
+        void report_visibilities(
+            double runtime,
+            uint64_t nr_baselines,
+            uint64_t nr_time,
+            uint64_t nr_channels)
         {
-            const int fw1 = 12;
-            const int fw2 = 10;
-
             uint64_t nr_visibilities = nr_baselines * nr_time * nr_channels;
 
-            clog << setw(fw1) << left << "throughput: "
-                 << setw(fw2) << right << scientific << setprecision(4)
+            clog << setw(FW1) << left << "throughput: "
+                 << setw(FW2) << right << scientific << setprecision(4)
                  << 1e-6 * nr_visibilities / runtime
                  << " Mvisibilities/s" << endl;
         }
 
-        void report_subgrids(double runtime,
-                   uint64_t nr_baselines)
+        void report_visibilities(
+            const char *name,
+            double runtime,
+            uint64_t nr_baselines,
+            uint64_t nr_time,
+            uint64_t nr_channels)
+        {
+            uint64_t nr_visibilities = nr_baselines * nr_time * nr_channels;
+            clog << setw(FW1) << left << string(name) + ": "
+                 << 1e-6 * nr_visibilities / runtime
+                 << " Mvisibilities/s" << endl;
+        }
+
+        void report_subgrids(
+            double runtime,
+            uint64_t nr_baselines)
         {
             clog << "throughput: " << 1e-3 * nr_baselines / runtime
                  << " Ksubgrids/s" << endl;
         }
 
-        void report_power(double runtime,
-                          double watt,
-                          double joules)
+        void report_subgrids(
+            const char *name,
+            double runtime,
+            uint64_t nr_baselines)
+        {
+            clog << setw(FW1) << left << string(name) + ": "
+                 << 1e-3 * nr_baselines / runtime
+                 << " Ksubgrids/s" << endl;
+        }
+
+        void report_power(
+            double runtime,
+            double watt,
+            double joules)
         {
             clog << fixed
                  << "   runtime: " << runtime << "s, "
--- idg-bin/src/common/auxiliary.h
+++ idg-bin/src/common/auxiliary.h
@@ -6,25 +6,42 @@
 namespace idg {
   namespace auxiliary {
 
-    void report(const char *name,
-         		double runtime,
-         		uint64_t flops,
-         		uint64_t bytes,
-                double watt=0);
+    void report(
+        const char *name,
+        double runtime,
+        uint64_t flops,
+        uint64_t bytes,
+        double watt=0);
 
-    void report_runtime(double runtime);
+    void report_runtime(
+        double runtime);
 
-    void report_visibilities(double runtime,
-			                 uint64_t nr_baselines,
-			                 uint64_t nr_time,
-			                 uint64_t nr_channels);
+    void report_visibilities(
+        const char *name,
+        double runtime,
+		uint64_t nr_baselines,
+		uint64_t nr_time,
+		uint64_t nr_channels);
 
-    void report_subgrids(double runtime,
-			             uint64_t nr_baselines);
+    void report_visibilities(
+        double runtime,
+		uint64_t nr_baselines,
+		uint64_t nr_time,
+		uint64_t nr_channels);
 
-    void report_power(double runtime,
-                      double watt,
-                      double joules);
+    void report_subgrids(
+        const char *name,
+        double runtime,
+        uint64_t nr_baselines);
+
+    void report_subgrids(
+        double runtime,
+        uint64_t nr_baselines);
+
+    void report_power(
+        double runtime,
+        double watt,
+        double joules);
 
   } // namespace auxiliary
 } // namespace idg
