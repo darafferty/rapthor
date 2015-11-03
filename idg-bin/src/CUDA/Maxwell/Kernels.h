b5886c9bf34697200d9374fa8f6808f91d7f620d
Bram Veenboer <bram.veenboer@gmail.com>
2015-11-03 16:26:12 +0100

    Moved flop and byte count from Kernels.cpp to Kernels.h

--- idg-bin/src/CUDA/Maxwell/Kernels.h
+++ idg-bin/src/CUDA/Maxwell/Kernels.h
@@ -31,13 +31,38 @@ namespace idg {
                 cu::DeviceMemory &d_visibilities, cu::DeviceMemory &d_spheroidal,
                 cu::DeviceMemory &d_aterm, cu::DeviceMemory &d_metadata,
                 cu::DeviceMemory &d_subgrid);
-        	uint64_t flops(int jobsize);
-    		uint64_t bytes(int jobsize);
+
+            uint64_t flops(int jobsize) {
+                int subgridsize = parameters.get_subgrid_size();
+                int nr_timesteps = parameters.get_nr_timesteps();
+                int nr_channels = parameters.get_nr_channels();
+                int nr_polarizations = parameters.get_nr_polarizations();
+                uint64_t flops = 0;
+                flops += 1ULL * jobsize * nr_timesteps * subgridsize * subgridsize * 5; // phase index
+                flops += 1ULL * jobsize * nr_timesteps * subgridsize * subgridsize * 5; // phase offset
+                flops += 1ULL * jobsize * nr_timesteps * subgridsize * subgridsize * nr_channels * 2; // phase
+                flops += 1ULL * jobsize * nr_timesteps * subgridsize * subgridsize * nr_channels * (nr_polarizations * 8); // update
+                flops += 1ULL * jobsize * subgridsize * subgridsize * nr_polarizations * 30; // aterm
+                flops += 1ULL * jobsize * subgridsize * subgridsize * nr_polarizations * 2; // spheroidal
+                flops += 1ULL * jobsize * subgridsize * subgridsize * nr_polarizations * 6; // shift
+                return flops;
+            }
+
+            uint64_t bytes(int jobsize) {
+                int subgridsize = parameters.get_subgrid_size();
+                int nr_timesteps = parameters.get_nr_timesteps();
+                int nr_channels = parameters.get_nr_channels();
+                int nr_polarizations = parameters.get_nr_polarizations();
+                uint64_t bytes = 0;
+                bytes += 1ULL * jobsize * nr_timesteps * 3 * sizeof(float); // uvw
+                bytes += 1ULL * jobsize * nr_timesteps * nr_channels * nr_polarizations * sizeof(cuFloatComplex); // visibilities
+                bytes += 1ULL * jobsize * nr_polarizations * subgridsize * subgridsize  * sizeof(cuFloatComplex); // subgrids
+                return bytes;
+            }
 
     	private:
     	    cu::Function function;
             Parameters &parameters;
-
     };
 
 
@@ -50,9 +75,36 @@ namespace idg {
                 cu::DeviceMemory &d_visibilities, cu::DeviceMemory &d_spheroidal,
                 cu::DeviceMemory &d_aterm, cu::DeviceMemory &d_metadata,
                 cu::DeviceMemory &d_subgrid);
-           	uint64_t flops(int jobsize);
-    		uint64_t bytes(int jobsize);
 
+            uint64_t flops(int jobsize) {
+                int subgridsize = parameters.get_subgrid_size();
+                int nr_timesteps = parameters.get_nr_timesteps();
+                int nr_channels = parameters.get_nr_channels();
+                int nr_polarizations = parameters.get_nr_polarizations();
+                uint64_t flops = 0;
+                flops += 1ULL * jobsize * nr_timesteps * subgridsize * subgridsize * 5; // phase index
+                flops += 1ULL * jobsize * nr_timesteps * subgridsize * subgridsize * 5; // phase offset
+                flops += 1ULL * jobsize * nr_timesteps * subgridsize * subgridsize * nr_channels * 2; // phase
+                flops += 1ULL * jobsize * nr_timesteps * subgridsize * subgridsize * nr_channels * (nr_polarizations * 8); // update
+                flops += 1ULL * jobsize * subgridsize * subgridsize * nr_polarizations * 30; // aterm
+                flops += 1ULL * jobsize * subgridsize * subgridsize * nr_polarizations * 2; // spheroidal
+                flops += 1ULL * jobsize * subgridsize * subgridsize * nr_polarizations * 6; // shift
+                return flops;
+            }
+        
+            uint64_t bytes(int jobsize) {
+                int subgridsize = parameters.get_subgrid_size();
+                int nr_time = parameters.get_nr_timesteps();
+                int nr_channels = parameters.get_nr_channels();
+                int nr_polarizations = parameters.get_nr_polarizations();
+                return 1ULL * jobsize * (
+                // ATerm
+                2 * subgridsize * subgridsize * nr_polarizations * sizeof(cuFloatComplex) +
+                // UV grid
+                subgridsize * subgridsize * nr_polarizations * sizeof(cuFloatComplex) +
+                // Visibilities
+                nr_time * nr_channels * nr_polarizations * sizeof(cuFloatComplex));
+            }
     	private:
     	    cu::Function function;
             Parameters &parameters;
@@ -64,8 +116,16 @@ namespace idg {
             GridFFT(cu::Module &module, Parameters &parameters);
             void plan(int size, int batch);
             void launchAsync(cu::Stream &stream, cu::DeviceMemory &data, int direction);
-    		uint64_t flops(int size, int batch);
-    		uint64_t bytes(int size, int batch);
+
+            uint64_t flops(int size, int batch) {
+                int nr_polarizations = parameters.get_nr_polarizations();
+            	return 1ULL * batch * nr_polarizations * 5 * size * size * log(size * size);
+            }
+
+            uint64_t bytes(int size, int batch) {
+                int nr_polarizations = parameters.get_nr_polarizations();
+            	return 1ULL * 2 * batch * size * size * nr_polarizations * sizeof(cuFloatComplex);
+            }
 
         private:
             cu::Function function;
