e408762e1dbb051a39ab8ad194978acafa40a24d
Bram Veenboer <bram.veenboer@gmail.com>
2015-11-16 15:20:15 +0100

    Added PerformanceCounter that uses two events.
    This currently only applies to 2D fft, since it requires two kernel invocations. The same mechanism can however be used with any number of events. For additional events, add a stopTimingMeasurement callback to add runtime for these events to the total runtime.

--- idg-bin/src/OpenCL/common/PerformanceCounter.cpp
+++ idg-bin/src/OpenCL/common/PerformanceCounter.cpp
@@ -2,6 +2,23 @@
 
 using namespace idg;
 
+/*
+    Utility
+*/
+double get_runtime(cl_event event) {
+    cl_ulong start, end;
+    double runtime = 0;
+    if (clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START, sizeof(start), &start, NULL) == CL_SUCCESS &&
+        clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END, sizeof(end), &end, NULL) == CL_SUCCESS) {
+        runtime = (end - start) * 1e-9;
+    }
+    return runtime;
+}
+
+
+/*
+    Performance Counter for one event
+*/
 void PerformanceCounter::startPowerMeasurement(cl_event event, cl_int, void *user_data) {
     #if defined(MEASURE_POWER_ARDUINO)
     Descriptor *descriptor = static_cast<Descriptor *>(user_data);
@@ -22,12 +39,7 @@ void PerformanceCounter::report(cl_event event, cl_int, void *user_data) {
     #if defined(MEASURE_POWER_ARDUINO)
     watts = PowerSensor::Watt(descriptor->startState, descriptor->stopState);
     #endif
-    double runtime = 0;
-    cl_ulong start, end;
-    if (clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START, sizeof(start), &start, NULL) == CL_SUCCESS &&
-        clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END, sizeof(end), &end, NULL) == CL_SUCCESS) {
-        runtime = (end - start) * 1e-9;
-    }
+    double runtime = get_runtime(event);
     auxiliary::report(descriptor->name, runtime, descriptor->flops, descriptor->bytes, watts);
     delete descriptor;
 }
@@ -43,6 +55,49 @@ void PerformanceCounter::doOperation(cl::Event &event, const char *name, uint64_
     event.setCallback(CL_RUNNING, &PerformanceCounter::stopPowerMeasurement, descriptor);
     #endif
     event.setCallback(CL_COMPLETE, &PerformanceCounter::report, descriptor);
+}
+
+
+/*
+    Performance Counter for two events
+*/
+void PerformanceCounter::stopTimingMeasurement(cl_event event, cl_int, void *user_data) {
+    Descriptor *descriptor = static_cast<Descriptor *>(user_data);
+    descriptor->runtime += get_runtime(event);
+}
+
+void PerformanceCounter::stopPowerAndTimingMeasurement(cl_event event, cl_int, void *user_data) {
+    Descriptor *descriptor = static_cast<Descriptor *>(user_data);
+    #if defined(MEASURE_POWER_ARDUINO)
+    descriptor->stopState = descriptor->powerSensor->read();
+    #endif
+    descriptor->runtime += get_runtime(event);
+}
+
+void PerformanceCounter::report2(cl_event event, cl_int, void *user_data) {
+    Descriptor *descriptor = static_cast<Descriptor *>(user_data);
+    double watts = 0;
+    #if defined(MEASURE_POWER_ARDUINO)
+    watts = PowerSensor::Watt(descriptor->startState, descriptor->stopState);
+    #endif
+    auxiliary::report(descriptor->name, descriptor->runtime, descriptor->flops, descriptor->bytes, watts);
+    delete descriptor;
+}
+
+void PerformanceCounter::doOperation(cl::Event &start, cl::Event &end, const char *name, uint64_t flops, uint64_t bytes) {
+    Descriptor *descriptor = new Descriptor;
+    descriptor->name = name;
+    descriptor->flops = flops;
+    descriptor->bytes = bytes;
+    descriptor->runtime = 0;
+    #if defined(MEASURE_POWER_ARDUINO)
+    descriptor->powerSensor = powerSensor;
+    start.setCallback(CL_SUBMITTED, &PerformanceCounter::startPowerMeasurement, descriptor);
+    #endif
+    start.setCallback(CL_RUNNING, &PerformanceCounter::stopTimingMeasurement, descriptor);
+    end.setCallback(CL_RUNNING, &PerformanceCounter::stopPowerAndTimingMeasurement, descriptor);
+    end.setCallback(CL_COMPLETE, &PerformanceCounter::report2, descriptor);
+
 }
 
 #if defined(MEASURE_POWER_ARDUINO)
