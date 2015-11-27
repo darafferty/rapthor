391d04c69040f3b0dd596153b26c26d85756fffa
Bram Veenboer <bram.veenboer@gmail.com>
2015-11-27 15:01:35 +0100

    Reintroduced grid_visibilities and degrid_visibilities high level routines. Removed jobsize as a parameter from low level routines

--- idg-bin/src/CPU/Reference/Reference.cpp
+++ idg-bin/src/CPU/Reference/Reference.cpp
@@ -16,65 +16,61 @@ namespace idg {
     namespace proxy {
         namespace cpu {
 
-        /// Constructors
-        Reference::Reference(
-            Parameters params,
-            Compiler compiler,
-            Compilerflags flags,
-            ProxyInfo info)
-            : CPU(params, compiler, flags, info)
-        {
-            #if defined(DEBUG)
-            cout << "Reference::" << __func__ << endl;
-            cout << "Compiler: " << compiler << endl;
-            cout << "Compiler flags: " << flags << endl;
-            cout << params;
-            #endif
-        }
+            Reference::Reference(
+                Parameters params,
+                Compiler compiler,
+                Compilerflags flags,
+                ProxyInfo info)
+                : CPU(params, compiler, flags, info)
+            {
+                #if defined(DEBUG)
+                cout << "Reference::" << __func__ << endl;
+                cout << "Compiler: " << compiler << endl;
+                cout << "Compiler flags: " << flags << endl;
+                cout << params;
+                #endif
+            }
 
 
-        ProxyInfo Reference::default_info()
-        {
-            #if defined(DEBUG)
-            cout << "Reference::" << __func__ << endl;
-            #endif
+            ProxyInfo Reference::default_info()
+            {
+                #if defined(DEBUG)
+                cout << "Reference::" << __func__ << endl;
+                #endif
 
-            string  srcdir = string(IDG_SOURCE_DIR) 
-                + "/src/CPU/Reference/kernels";
+                string  srcdir = string(IDG_SOURCE_DIR)
+                    + "/src/CPU/Reference/kernels";
 
-            #if defined(DEBUG)
-            cout << "Searching for source files in: " << srcdir << endl;
-            #endif
- 
-            // Create temp directory
-            string tmpdir = CPU::make_tempdir();
- 
-            // Create proxy info
-            ProxyInfo p = CPU::default_proxyinfo(srcdir, tmpdir);
+                #if defined(DEBUG)
+                cout << "Searching for source files in: " << srcdir << endl;
+                #endif
 
-            return p;
-        }
+                // Create temp directory
+                string tmpdir = CPU::make_tempdir();
 
-        
-        string Reference::default_compiler() 
-        {
-            #if defined(USING_INTEL_CXX_COMPILER)
-            return "icpc";
-            #else 
-            return "g++";
-            #endif
-        }
-        
+                // Create proxy info
+                ProxyInfo p = CPU::default_proxyinfo(srcdir, tmpdir);
 
-        string Reference::default_compiler_flags() 
-        {
-            #if defined(USING_INTEL_CXX_COMPILER)
-            return "-Wall -O3 -openmp -mkl -lmkl_def";
-            #else 
-            return "-Wall -O3 -fopenmp -lfftw3f";
-            #endif
-        }
+                return p;
+            }
 
+            string Reference::default_compiler()
+            {
+                #if defined(USING_INTEL_CXX_COMPILER)
+                return "icpc";
+                #else
+                return "g++";
+                #endif
+            }
+
+            string Reference::default_compiler_flags()
+            {
+                #if defined(USING_INTEL_CXX_COMPILER)
+                return "-Wall -O3 -openmp -mkl -lmkl_def";
+                #else
+                return "-Wall -O3 -fopenmp -lfftw3f";
+                #endif
+            }
 
         } // namespace cpu
     } // namespace proxy
