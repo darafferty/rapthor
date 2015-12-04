134db1d718d3d0e6af16abe59a7848acf7afc5aa
Bram Veenboer <bram.veenboer@gmail.com>
2015-12-04 16:25:06 +0100

    Added namespace CPU to CPU.cpp and CPU.h. Added read_power method.

--- idg-bin/src/CPU/common/CPU.h
+++ idg-bin/src/CPU/common/CPU.h
@@ -18,130 +18,130 @@
 
 namespace idg {
     namespace proxy {
+        namespace cpu {
+            // Power sensor
+            static LikwidPowerSensor *powerSensor;
+    
+            class CPU : public Proxy {
+                public:
+                    /// Constructors
+                    CPU(Parameters params,
+                        Compiler compiler = default_compiler(),
+                        Compilerflags flags = default_compiler_flags(),
+                        ProxyInfo info = default_info());
+    
+                    /// Copy constructor
+                    //CPU(const CPU& v) = delete;
+    
+                    /// Destructor
+                    virtual ~CPU();
+    
+                    /// Assignment
+                    CPU& operator=(const CPU& rhs) = delete;
+    
+                    // Get default values
+                    static ProxyInfo default_info();
+                    static std::string default_compiler();
+                    static std::string default_compiler_flags();
+    
+                    // Get parameters of proxy
+                    const Parameters& get_parameters() const { return mParams; }
+                    const ProxyInfo& get_info() const { return mInfo; }
+    
+                    // High level interface, inherited from Proxy
+                    virtual void grid_visibilities(
+                        const std::complex<float> *visibilities,
+                        const float *uvw,
+                        const float *wavenumbers,
+                        const int *metadata,
+                        std::complex<float> *grid,
+                        const float w_offset,
+                        const std::complex<float> *aterm,
+                        const float *spheroidal) override;
+    
+                    virtual void degrid_visibilities(
+                        std::complex<float> *visibilities,
+                        const float *uvw,
+                        const float *wavenumbers,
+                        const int *metadata,
+                        const std::complex<float> *grid,
+                        const float w_offset,
+                        const std::complex<float> *aterm,
+                        const float *spheroidal) override;
+    
+                    virtual void transform(DomainAtoDomainB direction,
+                                           std::complex<float>* grid) override;
+    
+                    // Low level routines
+                    virtual void grid_onto_subgrids(
+                        unsigned nr_subgrids,
+                        float w_offset,
+                        float *uvw,
+                        float *wavenumbers,
+                        std::complex<float> *visibilities,
+                        float *spheroidal,
+                        std::complex<float> *aterm,
+                        int *metadata,
+                        std::complex<float> *subgrids);
+    
+                    virtual void add_subgrids_to_grid(
+                        unsigned nr_subgrids,
+                        int *metadata,
+                        std::complex<float> *subgrids,
+                        std::complex<float> *grid);
+    
+                    virtual void split_grid_into_subgrids(
+                        unsigned nr_subgrids,
+                        int *metadata,
+                        std::complex<float> *subgrids,
+                        std::complex<float> *grid);
+    
+                    virtual void degrid_from_subgrids(
+                        unsigned nr_subgrids,
+                        float w_offset,
+                        float *uvw,
+                        float *wavenumbers,
+                        std::complex<float> *visibilities,
+                        float *spheroidal,
+                        std::complex<float> *aterm,
+                        int *metadata,
+                        std::complex<float> *subgrids);
+    
+                public:
+                    kernel::cpu::GridFFT get_kernel_fft() {
+                        return kernel::cpu::GridFFT(*(modules[which_module[kernel::cpu::name_fft]]), mParams);
+    
+                    }
+    
+                    kernel::cpu::Adder get_kernel_adder() {
+                        return kernel::cpu::Adder(*(modules[which_module[kernel::cpu::name_adder]]), mParams);
+                    }
+    
+                    kernel::cpu::Splitter get_kernel_splitter() {
+                        return kernel::cpu::Splitter(*(modules[which_module[kernel::cpu::name_splitter]]), mParams);
+                    }
 
-        class CPU : public Proxy {
-            public:
-                /// Constructors
-                CPU(Parameters params,
-                    Compiler compiler = default_compiler(),
-                    Compilerflags flags = default_compiler_flags(),
-                    ProxyInfo info = default_info());
-
-                /// Copy constructor
-                //CPU(const CPU& v) = delete;
-
-                /// Destructor
-                virtual ~CPU();
-
-                /// Assignment
-                CPU& operator=(const CPU& rhs) = delete;
-
-                // Get default values
-                static ProxyInfo default_info();
-                static std::string default_compiler();
-                static std::string default_compiler_flags();
-
-                // Get parameters of proxy
-                const Parameters& get_parameters() const { return mParams; }
-                const ProxyInfo& get_info() const { return mInfo; }
-
-                // High level interface, inherited from Proxy
-
-                virtual void grid_visibilities(
-                    const std::complex<float> *visibilities,
-                    const float *uvw,
-                    const float *wavenumbers,
-                    const int *metadata,
-                    std::complex<float> *grid,
-                    const float w_offset,
-                    const std::complex<float> *aterm,
-                    const float *spheroidal) override;
-
-                virtual void degrid_visibilities(
-                    std::complex<float> *visibilities,
-                    const float *uvw,
-                    const float *wavenumbers,
-                    const int *metadata,
-                    const std::complex<float> *grid,
-                    const float w_offset,
-                    const std::complex<float> *aterm,
-                    const float *spheroidal) override;
-
-                virtual void transform(DomainAtoDomainB direction,
-                                       std::complex<float>* grid) override;
-
-            // Low level routines
-            public:
-                // virtual void grid_onto_subgrids(GRIDDER_PARAMETERS) override;
-                // virtual void add_subgrids_to_grid(ADDER_PARAMETERS) override;
-                // virtual void split_grid_into_subgrids(SPLITTER_PARAMETERS) override;
-                // virtual void degrid_from_subgrids(DEGRIDDER_PARAMETERS) override;
-
-                virtual void grid_onto_subgrids(
-                    unsigned nr_subgrids,
-                    float w_offset,
-                    float *uvw,
-                    float *wavenumbers,
-                    std::complex<float> *visibilities,
-                    float *spheroidal,
-                    std::complex<float> *aterm,
-                    int *metadata,
-                    std::complex<float> *subgrids);
-
-                virtual void add_subgrids_to_grid(
-                    unsigned nr_subgrids,
-                    int *metadata,
-                    std::complex<float> *subgrids,
-                    std::complex<float> *grid);
-
-                virtual void split_grid_into_subgrids(
-                    unsigned nr_subgrids,
-                    int *metadata,
-                    std::complex<float> *subgrids,
-                    std::complex<float> *grid);
-
-                virtual void degrid_from_subgrids(
-                    unsigned nr_subgrids,
-                    float w_offset,
-                    float *uvw,
-                    float *wavenumbers,
-                    std::complex<float> *visibilities,
-                    float *spheroidal,
-                    std::complex<float> *aterm,
-                    int *metadata,
-                    std::complex<float> *subgrids);
-
-            public:
-                kernel::cpu::GridFFT get_kernel_fft() {
-                    return kernel::cpu::GridFFT(*(modules[which_module[kernel::cpu::name_fft]]), mParams);
-
-                }
-
-                kernel::cpu::Adder get_kernel_adder() {
-                    return kernel::cpu::Adder(*(modules[which_module[kernel::cpu::name_adder]]), mParams);
-                }
-
-                kernel::cpu::Splitter get_kernel_splitter() {
-                    return kernel::cpu::Splitter(*(modules[which_module[kernel::cpu::name_splitter]]), mParams);
-                }
-
-            protected:
-                static std::string make_tempdir();
-                static ProxyInfo default_proxyinfo(std::string srcdir, std::string tmpdir);
-
-                void compile(Compiler compiler, Compilerflags flags);
-                void parameter_sanity_check();
-                void load_shared_objects();
-                void find_kernel_functions();
-
-                // data
-                ProxyInfo mInfo; // info about shared object files
-
-                // store the ptr to Module, which each loads an .so-file
-                std::vector<runtime::Module*> modules;
-                std::map<std::string,int> which_module;
-        }; // class CPU
-
+                    LikwidPowerSensor::State read_power() {
+                        return cpu::powerSensor->read();
+                    }
+    
+                protected:
+                    static std::string make_tempdir();
+                    static ProxyInfo default_proxyinfo(std::string srcdir, std::string tmpdir);
+    
+                    void compile(Compiler compiler, Compilerflags flags);
+                    void parameter_sanity_check();
+                    void load_shared_objects();
+                    void find_kernel_functions();
+    
+                    // data
+                    ProxyInfo mInfo; // info about shared object files
+    
+                    // store the ptr to Module, which each loads an .so-file
+                    std::vector<runtime::Module*> modules;
+                    std::map<std::string,int> which_module;
+            }; // class CPU
+        } // namespace cpu
     } // namespace proxy
 } // namespace idg
 
