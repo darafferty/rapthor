7c6847f88593daa602b447211b2a6d4116f5eec9
Matthias Petschow <m.petschow@googlemail.com>
2015-12-09 12:53:36 +0100

    Started to clean up header files for CPU and common code, so that a user just includes "idg-cpu.h" in the include folder, when he/she uses libidg-cpu.so.

---
+++ idg-bin/src/idg-cpu.h
@@ -0,0 +1,10 @@
+#ifndef IDG_CPU_HEADER
+#define IDG_CPU_HEADER
+
+#include "idg-common.h"
+#include "CPU/common/CPU.h"
+#include "CPU/Reference/Reference.h"
+#include "CPU/SandyBridgeEP/SandyBridgeEP.h"
+#include "CPU/HaswellEP/HaswellEP.h"
+
+#endif
