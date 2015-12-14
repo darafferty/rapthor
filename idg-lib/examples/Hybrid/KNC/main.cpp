e63dfb5979d447f8333fbdceab8d7790544cc105
Matthias Petschow <m.petschow@googlemail.com>
2015-12-14 12:05:36 +0100

    Made KNC example compile again.

--- idg-lib/examples/Hybrid/KNC/main.cpp
+++ idg-lib/examples/Hybrid/KNC/main.cpp
@@ -1,19 +1,20 @@
-#include "Hybrid/KNC/idg.h"
+#include "idg-knc.h"
 
-#include "common.h"
+#include "../../CPU/common/common.h"
+
+using namespace std;
 
 void run(
     idg::Parameters params,
     int nr_subgrids,
-    void *uvw,
-    void *wavenumbers,
-    void *visibilities,
-    void *spheroidal,
-    void *aterm,
-    void *metadata,
-    void *subgrids,
-    void *grid
-) {
+    float *uvw,
+    float *wavenumbers,
+    complex<float> *visibilities,
+    float *spheroidal,
+    complex<float> *aterm,
+    int *metadata,
+    complex<float> *subgrids,
+    complex<float> *grid) {
     // Initialize interface to kernels
     clog << ">>> Initialize proxy" << endl;
     idg::proxy::hybrid::KNC knc(params);
@@ -22,20 +23,20 @@ void run(
     // Run gridder
     clog << ">>> Run gridder" << endl;
     int jobsize_gridder = params.get_job_size_gridder();
-    knc.grid_onto_subgrids(jobsize_gridder, nr_subgrids, 0, uvw, wavenumbers, visibilities, spheroidal, aterm, metadata, subgrids);
+    knc.grid_onto_subgrids(nr_subgrids, 0, uvw, wavenumbers, visibilities, spheroidal, aterm, metadata, subgrids);
 
     clog << ">> Run adder" << endl;
     int jobsize_adder = params.get_job_size_adder();
-    knc.add_subgrids_to_grid(jobsize_adder, nr_subgrids, metadata, subgrids, grid);
+    knc.add_subgrids_to_grid(nr_subgrids, metadata, subgrids, grid);
 
     clog << ">> Run fft" << endl;
     knc.transform(idg::FourierDomainToImageDomain, grid);
 
     clog << ">>> Run splitter" << endl;
     int jobsize_splitter = params.get_job_size_splitter();
-    knc.split_grid_into_subgrids(jobsize_splitter, nr_subgrids, metadata, subgrids, grid);
+    knc.split_grid_into_subgrids(nr_subgrids, metadata, subgrids, grid);
 
     clog << ">>> Run degridder" << endl;
     int jobsize_degridder = params.get_job_size_degridder();
-    knc.degrid_from_subgrids(jobsize_degridder, nr_subgrids, 0, uvw, wavenumbers, visibilities, spheroidal, aterm, metadata, subgrids);
+    knc.degrid_from_subgrids(nr_subgrids, 0, uvw, wavenumbers, visibilities, spheroidal, aterm, metadata, subgrids);
 }
