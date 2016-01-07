12b3a537d417f162060494a9570882ff479fee49
Matthias Petschow <petschow@astron.nl>
2016-01-07 15:50:13 +0100

    Corrected A-term application in gridder kernels for CPUs.

--- idg-bin/src/CPU/Reference/kernels/KernelGridder.cpp
+++ idg-bin/src/CPU/Reference/kernels/KernelGridder.cpp
@@ -94,28 +94,36 @@ void kernel_gridder(
                 FLOAT_COMPLEX aYX2 = conj((*aterm)[station2][time_nr][2][y][x]);
                 FLOAT_COMPLEX aYY2 = conj((*aterm)[station2][time_nr][3][y][x]);
 
-                // Load uv values
+                // Apply aterm to subgrid: P*A1
+                // [ pixels[0], pixels[1];    [ aXX1, aXY1;
+                //   pixels[2], pixels[3] ] *   aYX1, aYY1 ]
                 FLOAT_COMPLEX pixelsXX = pixels[0];
                 FLOAT_COMPLEX pixelsXY = pixels[1];
                 FLOAT_COMPLEX pixelsYX = pixels[2];
                 FLOAT_COMPLEX pixelsYY = pixels[3];
-
-                // Apply aterm to subgrid
                 pixels[0]  = (pixelsXX * aXX1);
                 pixels[0] += (pixelsXY * aYX1);
-                pixels[0] += (pixelsXX * aXX2);
-                pixels[0] += (pixelsYX * aYX2);
                 pixels[1]  = (pixelsXX * aXY1);
                 pixels[1] += (pixelsXY * aYY1);
-                pixels[1] += (pixelsXY * aXX2);
-                pixels[1] += (pixelsYY * aYX2);
                 pixels[2]  = (pixelsYX * aXX1);
                 pixels[2] += (pixelsYY * aYX1);
-                pixels[2] += (pixelsXX * aXY2);
-                pixels[2] += (pixelsYX * aYY2);
                 pixels[3]  = (pixelsYX * aXY1);
                 pixels[3] += (pixelsYY * aYY1);
-                pixels[3] += (pixelsXY * aXY2);
+
+                // Apply aterm to subgrid: A2^H*P
+                // [ aXX2, aYX1;      [ pixels[0], pixels[1];
+                //   aXY1, aYY2 ]  *    pixels[2], pixels[3] ]
+                pixelsXX = pixels[0];
+                pixelsXY = pixels[1];
+                pixelsYX = pixels[2];
+                pixelsYY = pixels[3];
+                pixels[0]  = (pixelsXX * aXX2);
+                pixels[0] += (pixelsYX * aYX2);
+                pixels[1]  = (pixelsXY * aXX2);
+                pixels[1] += (pixelsYY * aYX2);
+                pixels[2]  = (pixelsXX * aXY2);
+                pixels[2] += (pixelsYX * aYY2);
+                pixels[3]  = (pixelsXY * aXY2);
                 pixels[3] += (pixelsYY * aYY2);
 
                 // Load spheroidal
