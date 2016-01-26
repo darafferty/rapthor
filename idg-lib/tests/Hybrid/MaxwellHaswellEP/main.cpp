e5f575b2f2b4b4af103d889221f2426b75f8a08f
Bram Veenboer <bram.veenboer@gmail.com>
2016-01-26 11:38:52 +0100

    Added test for Hybrid MaxwellHaswellEP

---
+++ idg-lib/tests/Hybrid/MaxwellHaswellEP/main.cpp
@@ -0,0 +1,15 @@
+#include "idg-hybrid-cuda.h" // needs to be before common
+#include "../../CPU/common/common.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    // Reads the following from ENV:
+    // NR_STATIONS, NR_CHANNELS, NR_TIMESTEPS, NR_TIMESLOTS, IMAGESIZE,
+    // GRIDSIZE; if non-default jobsize wanted, set jobsizes parameters.
+
+    int info = run_test<idg::proxy::hybrid::MaxwellHaswellEP>();
+
+    return info;
+}
