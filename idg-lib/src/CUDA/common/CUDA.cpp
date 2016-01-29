59319ea4409636b6d63cefe0beee33ec405cdb4f
Bram Veenboer <bram.veenboer@gmail.com>
2016-01-29 12:19:03 +0100

    Merged Maxwell/kernels and Kepler/kernels into common/kernels
    It turns out that the main thing to be changed between two CUDA architectures are the thread configuration and work sizes (nr_timesteps). A lot of duplication is avoided by only mainting one set of kernels.

--- idg-lib/src/CUDA/common/CUDA.cpp
+++ idg-lib/src/CUDA/common/CUDA.cpp
@@ -107,6 +107,58 @@ namespace idg {
                 #endif
             }
 
+            ProxyInfo CUDA::default_info() {
+                #if defined(DEBUG)
+                cout << "CUDA::" << __func__ << endl;
+                #endif
+
+                string srcdir = string(IDG_INSTALL_DIR)
+                    + "/lib/kernels/CUDA/";
+
+                #if defined(DEBUG)
+                cout << "Searching for source files in: " << srcdir << endl;
+                #endif
+
+                // Create temp directory
+                string tmpdir = make_tempdir();
+
+                // Create proxy info
+                ProxyInfo p = default_proxyinfo(srcdir, tmpdir);
+
+                return p;
+            }
+
+            ProxyInfo CUDA::default_proxyinfo(string srcdir, string tmpdir) {
+                ProxyInfo p;
+                p.set_path_to_src(srcdir);
+                p.set_path_to_lib(tmpdir);
+
+                string libgridder = "Gridder.ptx";
+                string libdegridder = "Degridder.ptx";
+                string libfft = "FFT.ptx";
+                string libscaler = "Scaler.ptx";
+                string libadder = "Adder.ptx";
+                string libsplitter = "Splitter.ptx";
+
+                p.add_lib(libgridder);
+                p.add_lib(libdegridder);
+                p.add_lib(libfft);
+                p.add_lib(libscaler);
+                p.add_lib(libadder);
+                p.add_lib(libsplitter);
+
+                p.add_src_file_to_lib(libgridder, "KernelGridder.cu");
+                p.add_src_file_to_lib(libdegridder, "KernelDegridder.cu");
+                p.add_src_file_to_lib(libfft, "KernelFFT.cu");
+                p.add_src_file_to_lib(libscaler, "KernelScaler.cu");
+                p.add_src_file_to_lib(libadder, "KernelAdder.cu");
+                p.add_src_file_to_lib(libsplitter, "KernelSplitter.cu");
+
+                p.set_delete_shared_objects(true);
+
+                return p;
+            }
+
             /* Sizeof routines */
             uint64_t CUDA::sizeof_subgrids(int nr_subgrids) {
                 auto nr_polarizations = mParams.get_nr_polarizations();
