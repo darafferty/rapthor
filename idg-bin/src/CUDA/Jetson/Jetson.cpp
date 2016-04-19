6ffadc2e231c415fc3c4edf9f09fda1bb7056e3a
Bram Veenboer <bram.veenboer@gmail.com>
2016-04-19 11:33:37 +0000

    Implemented jetson degrid_visibilities routine

--- idg-bin/src/CUDA/Jetson/Jetson.cpp
+++ idg-bin/src/CUDA/Jetson/Jetson.cpp
@@ -266,7 +266,145 @@ namespace idg {
                 #if defined(DEBUG)
                 cout << __func__ << endl;
                 #endif
-                cout << "Not implemented" << endl;
+
+                // Constants
+                auto nr_stations = mParams.get_nr_stations();
+                auto nr_baselines = mParams.get_nr_baselines();
+                auto nr_time = mParams.get_nr_time();
+                auto nr_timeslots = mParams.get_nr_timeslots();
+                auto nr_channels = mParams.get_nr_channels();
+                auto nr_polarizations = mParams.get_nr_polarizations();
+                auto gridsize = mParams.get_grid_size();
+                auto subgridsize = mParams.get_subgrid_size();
+                auto jobsize = mParams.get_job_size_gridder();
+
+                // Load kernels
+                unique_ptr<Splitter> kernel_splitter = get_kernel_splitter();
+                unique_ptr<GridFFT> kernel_fft = get_kernel_fft();
+                unique_ptr<Degridder> kernel_degridder = get_kernel_degridder();
+
+                // Initialize metadata
+                auto max_nr_timesteps = kernel_degridder->get_max_nr_timesteps();
+                auto plan = create_plan(uvw, wavenumbers, baselines,
+                                        aterm_offsets, kernel_size,
+                                        max_nr_timesteps);
+                auto nr_subgrids = plan.get_nr_subgrids();
+                const Metadata *metadata = plan.get_metadata_ptr();
+
+                // Initialize
+                cu::Context &context = get_context();
+                cu::Stream executestream;
+                const int nr_streams = 1;
+
+                // Get device memory pointers
+                cu::DeviceMemory d_visibilities((void *) visibilities);
+                cu::DeviceMemory d_uvw((void *) uvw);
+                cu::DeviceMemory d_wavenumbers((void *) wavenumbers);
+                cu::DeviceMemory d_spheroidal((void *) spheroidal);
+                cu::DeviceMemory d_aterm((void *) aterm);
+                cu::DeviceMemory d_grid((void *) grid);
+                cu::DeviceMemory d_metadata(sizeof_metadata(nr_subgrids));
+                d_metadata.set((void *) metadata);
+
+                // Performance measurements
+                double total_runtime_splitter = 0;
+                double total_runtime_fft = 0;
+                double total_runtime_degridder = 0;
+                PowerSensor::State startState = powerSensor.read();
+
+                // Initialize
+                context.setCurrent();
+                cu::Event executeFinished;
+
+                // Device memory
+                int max_nr_subgrids = plan.get_max_nr_subgrids(0, nr_baselines, jobsize);
+                cu::DeviceMemory d_subgrids(sizeof_subgrids(max_nr_subgrids));
+
+                for (unsigned int bl = 0; bl < nr_baselines; bl += jobsize) {
+                    // Compute the number of baselines to process in current iteration
+                    int current_nr_baselines = bl + jobsize > nr_baselines ? nr_baselines - bl : jobsize;
+
+                    // Number of elements in batch
+                    int uvw_elements          = nr_time * sizeof(UVW)/sizeof(float);
+                    int visibilities_elements = nr_time * nr_channels * nr_polarizations;
+
+                    // Number of subgrids for all baselines in job
+                    auto current_nr_subgrids = plan.get_nr_subgrids(bl, current_nr_baselines);
+
+                    // Pointers to data for current batch
+                    void *uvw_ptr          = d_uvw.get(bl * uvw_elements * sizeof(float));
+                    void *visibilities_ptr = d_visibilities.get(bl * visibilities_elements * sizeof(float));
+                    void *metadata_ptr     = d_metadata.get(plan.get_metadata_ptr(bl) - plan.get_metadata_ptr(0));
+
+                    // Power measurement
+                    PowerRecord powerRecords[4];
+
+                    // Create FFT plan
+                    kernel_fft->plan(subgridsize, current_nr_subgrids);
+
+                    // Launch splitter kernel
+                    powerRecords[0].enqueue(executestream);
+                    kernel_splitter->launch(
+                        executestream, current_nr_subgrids,
+                        d_metadata, d_subgrids, d_grid);
+                    powerRecords[1].enqueue(executestream);
+
+                    // Launch FFT
+                    kernel_fft->launch(executestream, d_subgrids, CUFFT_FORWARD);
+                    powerRecords[2].enqueue(executestream);
+
+                    // Launch degridder kernel
+                    kernel_degridder->launch(
+                        executestream, current_nr_subgrids, w_offset, d_uvw, d_wavenumbers,
+                        d_visibilities, d_spheroidal, d_aterm, d_metadata, d_subgrids);
+                    powerRecords[3].enqueue(executestream);
+                    executestream.record(executeFinished);
+
+                    executeFinished.synchronize();
+
+                    double runtime_splitter  = PowerSensor::seconds(powerRecords[0].state, powerRecords[1].state);
+                    double runtime_fft       = PowerSensor::seconds(powerRecords[1].state, powerRecords[2].state);
+                    double runtime_degridder = PowerSensor::seconds(powerRecords[2].state, powerRecords[3].state);
+                    #if defined(REPORT_VERBOSE)
+                    auxiliary::report(" splitter", runtime_splitter,
+                                                   kernel_splitter->flops(current_nr_subgrids),
+                                                   kernel_splitter->bytes(current_nr_subgrids),
+                                                   PowerSensor::Watt(powerRecords[3].state, powerRecords[4].state));
+                    auxiliary::report("      fft", runtime_fft,
+                                                   kernel_fft->flops(subgridsize, current_nr_subgrids),
+                                                   kernel_fft->bytes(subgridsize, current_nr_subgrids),
+                                                   PowerSensor::Watt(powerRecords[1].state, powerRecords[2].state));
+                    auxiliary::report("degridder", runtime_degridder,
+                                                   kernel_degridder->flops(current_nr_baselines, current_nr_subgrids),
+                                                   kernel_degridder->bytes(current_nr_baselines, current_nr_subgrids),
+                                                   PowerSensor::Watt(powerRecords[0].state, powerRecords[1].state));
+                    #endif
+                    #if defined(REPORT_TOTAL)
+                    total_runtime_splitter  += runtime_splitter;
+                    total_runtime_fft       += runtime_fft;
+                    total_runtime_degridder += runtime_degridder;
+                    #endif
+                } // end for s
+
+                #if defined(REPORT_VERBOSE) || defined(REPORT_TOTAL)
+                PowerSensor::State stopState = powerSensor.read();
+                uint64_t total_flops_degridder  = kernel_degridder->flops(nr_baselines, nr_subgrids);
+                uint64_t total_bytes_degridder  = kernel_degridder->bytes(nr_baselines, nr_subgrids);
+                uint64_t total_flops_fft        = kernel_fft->flops(subgridsize, nr_subgrids);
+                uint64_t total_bytes_fft        = kernel_fft->bytes(subgridsize, nr_subgrids);
+                uint64_t total_flops_splitter   = kernel_splitter->flops(nr_subgrids);
+                uint64_t total_bytes_splitter   = kernel_splitter->bytes(nr_subgrids);
+                uint64_t total_flops_degridding = total_flops_degridder + total_flops_fft;
+                uint64_t total_bytes_degridding = total_bytes_degridder + total_bytes_fft;
+                double total_runtime_degridding = PowerSensor::seconds(startState, stopState);
+                double total_watt_degridding    = PowerSensor::Watt(startState, stopState);
+                auxiliary::report("|splitter", total_runtime_splitter, total_flops_splitter, total_bytes_splitter);
+                auxiliary::report("|degridder", total_runtime_degridder, total_flops_degridder, total_bytes_degridder);
+                auxiliary::report("|fft", total_runtime_fft, total_flops_fft, total_bytes_fft);
+                auxiliary::report("|degridding", total_runtime_degridding, total_flops_degridding, total_bytes_degridding, 0);
+                auxiliary::report_visibilities("|degridding", total_runtime_degridding, nr_baselines, nr_time, nr_channels);
+                clog << endl;
+                #endif
             }
 
             unique_ptr<Gridder> Jetson::get_kernel_gridder() const {
