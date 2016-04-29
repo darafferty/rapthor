9e4ed2cbc1fa403d42ad1d68c6e1f30dd90072bc
Bram Veenboer <bram.veenboer@gmail.com>
2016-04-29 14:12:24 +0200

    Added Kernels.h and Kernels.cpp to src/common/. These should be used by all implementations to compute the number of flops and bytes of specific kernels. This is currently implemented for CPU and CUDA.

--- idg-lib/src/CPU/common/Kernels.cpp
+++ idg-lib/src/CPU/common/Kernels.cpp
@@ -1,6 +1,7 @@
 #include <cstdint> // unint64_t
 
 #include "idg-config.h"
+#include "../../common/Kernels.h"
 #include "Kernels.h"
 
 //#define COUNT_SINCOS_AS_FLOPS
@@ -25,70 +26,12 @@ namespace idg {
                   visibilities, spheroidal, aterm, metadata, subgrid);
             }
 
-            uint64_t gridder_flops(Parameters &parameters, int nr_baselines, int nr_subgrids) {
-                int subgridsize = parameters.get_subgrid_size();
-                int nr_time = parameters.get_nr_time();
-                int nr_channels = parameters.get_nr_channels();
-                int nr_polarizations = parameters.get_nr_polarizations();
-
-                // Number of flops per uvw coordinate
-                uint64_t flops_per_uvw = 0;
-                flops_per_uvw += 1ULL * nr_time * 5; // phase index
-                flops_per_uvw += 1ULL * nr_time * 5; // phase offset
-
-                // Number of flops per visibility
-                uint64_t flops_per_vis = 0;
-                flops_per_vis += 1ULL * nr_time * nr_channels * 2; // phase
-                #if defined(COUNT_SINCOS_AS_FLOPS)
-                flops_per_vis += 1ULL * nr_time * nr_channels * FLOPS_PER_SINCOS; // phasor
-                #endif
-
-                // Number of flops per pixel
-                uint64_t flops_per_pix = 0;
-                flops_per_pix += 1ULL * nr_baselines * nr_time * nr_channels * (nr_polarizations * 8); // update
-                flops_per_pix += 1ULL * nr_subgrids * nr_polarizations * 30; // aterm
-                flops_per_pix += 1ULL * nr_subgrids * nr_polarizations * 2; // spheroidal
-                flops_per_pix += 1ULL * nr_subgrids * 6; // shift
-
-                // Total number of flops
-                uint64_t flops_total = 0;
-                flops_total += 1ULL * nr_subgrids * subgridsize * subgridsize * (flops_per_uvw + flops_per_vis);
-                flops_total += 1ULL * subgridsize * subgridsize * flops_per_pix;
-                return flops_total;
-            }
-
             uint64_t Gridder::flops(int nr_baselines, int nr_subgrids) {
-                return gridder_flops(parameters, nr_baselines, nr_subgrids);
-            }
-
-            uint64_t gridder_bytes(Parameters &parameters, int nr_baselines, int nr_subgrids) {
-                int subgridsize = parameters.get_subgrid_size();
-                int nr_time = parameters.get_nr_time();
-                int nr_channels = parameters.get_nr_channels();
-                int nr_polarizations = parameters.get_nr_polarizations();
-
-                // Number of bytes per uvw coordinate
-                uint64_t bytes_per_uvw = 0;
-                bytes_per_uvw += 1ULL * 3 * sizeof(float); // uvw
-
-                // Number of bytes per visibility
-                uint64_t bytes_per_vis = 0;
-                bytes_per_vis += 1ULL * nr_channels * nr_polarizations * 2 * sizeof(float); // visibilities
-
-                // Number of bytes per pixel
-                uint64_t bytes_per_pix = 0;
-                bytes_per_pix += 1ULL * nr_polarizations * 2 * sizeof(float); // pixel
-
-                // Total number of bytes
-                uint64_t bytes_total = 0;
-                bytes_total += 1ULL * nr_baselines * nr_time * bytes_per_uvw;
-                bytes_total += 1ULL * nr_baselines * nr_time * bytes_per_vis;
-                bytes_total += 1ULL * nr_subgrids * subgridsize * subgridsize * bytes_per_pix;
-                return bytes_total;
+                return flops_gridder(parameters, nr_baselines, nr_subgrids);
             }
 
             uint64_t Gridder::bytes(int nr_baselines, int nr_subgrids) {
-                return gridder_bytes(parameters, nr_baselines, nr_subgrids);
+                return bytes_gridder(parameters, nr_baselines, nr_subgrids);
             }
 
 
@@ -107,11 +50,11 @@ namespace idg {
             }
 
             uint64_t Degridder::flops(int nr_baselines, int nr_subgrids) {
-                return gridder_flops(parameters, nr_baselines, nr_subgrids);
+                return flops_degridder(parameters, nr_baselines, nr_subgrids);
             }
 
             uint64_t Degridder::bytes(int nr_baselines, int nr_subgrids) {
-                return gridder_bytes(parameters, nr_baselines, nr_subgrids);
+                return bytes_degridder(parameters, nr_baselines, nr_subgrids);
             }
 
 
@@ -125,13 +68,11 @@ namespace idg {
             }
 
             uint64_t GridFFT::flops(int size, int batch) {
-                int nr_polarizations = parameters.get_nr_polarizations();
-        	    return 1ULL * batch * nr_polarizations * 5 * size * size * log(size * size);
+                return flops_fft(parameters, size, batch);
             }
 
             uint64_t GridFFT::bytes(int size, int batch) {
-                int nr_polarizations = parameters.get_nr_polarizations();
-        	    return 1ULL * 2 * batch * nr_polarizations * size * size * 2 * sizeof(float);
+                return bytes_fft(parameters, size, batch);
             }
 
 
@@ -145,23 +86,11 @@ namespace idg {
             }
 
             uint64_t Adder::flops(int nr_subgrids) {
-                int subgridsize = parameters.get_subgrid_size();
-                int nr_polarizations = parameters.get_nr_polarizations();
-                uint64_t flops = 0;
-                flops += 1ULL * nr_subgrids * subgridsize * subgridsize * 8; // shift
-                flops += 1ULL * nr_subgrids * subgridsize * subgridsize * nr_polarizations * 2; // add
-                return flops;
+                return flops_adder(parameters, nr_subgrids);
             }
 
             uint64_t Adder::bytes(int nr_subgrids) {
-                int subgridsize = parameters.get_subgrid_size();
-                int nr_polarizations = parameters.get_nr_polarizations();
-                uint64_t bytes = 0;
-                bytes += 1ULL * nr_subgrids * 2 * sizeof(int); // coordinate
-                bytes += 1ULL * nr_subgrids * subgridsize * subgridsize * 2 * sizeof(float); // grid in
-                bytes += 1ULL * nr_subgrids * subgridsize * subgridsize * 2 * sizeof(float); // subgrid in
-                bytes += 1ULL * nr_subgrids * subgridsize * subgridsize * 2 * sizeof(float); // subgrid out
-                return bytes;
+                return bytes_adder(parameters, nr_subgrids);
             }
 
 
@@ -175,20 +104,11 @@ namespace idg {
             }
 
             uint64_t Splitter::flops(int nr_subgrids) {
-                int subgridsize = parameters.get_subgrid_size();
-                uint64_t flops = 0;
-                flops += 1ULL * nr_subgrids * subgridsize * subgridsize * 8; // shift
-                return flops;
+                return flops_splitter(parameters, nr_subgrids);
             }
 
             uint64_t Splitter::bytes(int nr_subgrids) {
-                int subgridsize = parameters.get_subgrid_size();
-                int nr_polarizations = parameters.get_nr_polarizations();
-                uint64_t bytes = 0;
-                bytes += 1ULL * nr_subgrids * 2 * sizeof(int); // coordinate
-                bytes += 1ULL * nr_subgrids * subgridsize * subgridsize * 2 * sizeof(float); // grid in
-                bytes += 1ULL * nr_subgrids * subgridsize * subgridsize * 2 * sizeof(float); // subgrid out
-                return bytes;
+                return bytes_splitter(parameters, nr_subgrids);
             }
 
         } // namespace cpu
