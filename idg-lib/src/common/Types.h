d56f073ae36ce6bc5431d7438de3b0047b927d70
Bram Veenboer <bram.veenboer@gmail.com>
2016-04-29 08:45:33 +0200

    NR_CHANNELS is no longer passed during kernel compilation, instead a parameter nr_channels is passed to the kernels that require this parameter. The VisibilitiesType data structure now has a collapsed dimenson for nr_time and nr_channels, which is unpacked into VisibilityType inside the kernels.

--- idg-lib/src/common/Types.h
+++ idg-lib/src/common/Types.h
@@ -4,14 +4,19 @@
 #include <ostream>
 
 namespace idg {
+    /*
+        Structures
+    */
     typedef struct { float u, v, w; } UVW;
     typedef struct { int x, y; } Coordinate;
     typedef struct { int station1, station2; } Baseline;
 
-    // TODO: is int enough for offsets?
     typedef struct { int baseline_offset; int time_offset; int nr_timesteps;
                      int aterm_index; Baseline baseline; Coordinate coordinate; } Metadata;
 
+    /*
+        Output
+    */
     std::ostream& operator<<(std::ostream &out, Baseline &b);
     std::ostream& operator<<(std::ostream &out, Coordinate &c);
     std::ostream& operator<<(std::ostream &out, Metadata &m);
