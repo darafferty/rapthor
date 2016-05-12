6f7827e98da82cdb7b5abb7b60861e7c457b78b9
Bram Veenboer <bram.veenboer@gmail.com>
2016-05-12 08:57:44 +0200

    Removed superfluous newline

--- idg-bin/src/CUDA/common/kernels/KernelSplitter.cu
+++ idg-bin/src/CUDA/common/kernels/KernelSplitter.cu
@@ -36,7 +36,6 @@ __global__ void kernel_splitter(
             int x_dst = (x + (SUBGRIDSIZE/2)) % SUBGRIDSIZE;
             int y_dst = (y + (SUBGRIDSIZE/2)) % SUBGRIDSIZE;
 
-
             // Add subgrid value to grid
             subgrid[s][0][y_dst][x_dst] = grid[0][grid_y+y][grid_x+x];
             subgrid[s][1][y_dst][x_dst] = grid[1][grid_y+y][grid_x+x];
