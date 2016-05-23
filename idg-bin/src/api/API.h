b23b589d469967cfeb1ac7b1510abc4e1ac6a1a1
Matthias Petschow <m.petschow@googlemail.com>
2016-05-23 17:38:56 +0200

    Quick and dirty: implementing WSClean API

--- idg-bin/src/api/API.h
+++ idg-bin/src/api/API.h
@@ -1,96 +1,88 @@
-/*
- * API.h
- * Implementation of IDG's high level interfaces
- */
+/* /\* */
+/*  * API.h */
+/*  * Implementation of IDG's high level interfaces */
+/*  *\/ */
 
-#ifndef IDG_INTERFACE_H_
-#define IDG_INTERFACE_H_
+/* #ifndef IDG_INTERFACE_H_ */
+/* #define IDG_INTERFACE_H_ */
 
-#include <complex>
+/* #include <complex> */
 
-namespace idg {
-
-
-    enum class Type {
-        CPU_REFERENCE,
-        CPU_OPTIMIZED,
-        CUDA_KEPLER,
-        CUDA_MAXWELL
-    };
+/* namespace idg { */
 
 
-    class WSCLEAN_API
-    {
-    public:
-        virtual ~WSCLEAN_API() {};
-
-        virtual void set_frequencies(const double* frequencyList,
-                                     size_t channelCount) = 0;
-
-        virtual void set_stations(const size_t nr_stations) = 0;
-
-        virtual void set_image_size(const double imageSize) = 0;
-
-        virtual void set_w_kernel(size_t size) = 0;
-
-        virtual void set_spheroidal(
-            const double* spheroidal,
-            const size_t height,
-            const size_t width) = 0;
-
-        virtual void start_w_layer(double layerWInLambda) = 0;
-
-        virtual void finish_w_layer() = 0;
-
-        virtual void set_grid(
-            std::complex<double>* grid,
-            const size_t nr_polarizations,
-            const size_t height,
-            const size_t width) = 0;
-
-        virtual void bake() = 0;
+/*     enum class Type { */
+/*         CPU_REFERENCE, */
+/*         CPU_OPTIMIZED, */
+/*         CUDA_KEPLER, */
+/*         CUDA_MAXWELL */
+/*     }; */
 
 
-        // Gridding
+/*     class WSCLEAN_API */
+/*     { */
+/*     public: */
+/*         virtual ~WSCLEAN_API() {}; */
 
-        virtual void grid_visibilities(
-            const std::complex<float>* visibilities, // size CH x PL
-            const double* uvwInMeters,
-            size_t antenna1,
-            size_t antenna2,
-            size_t timeIndex) = 0;
+/*         virtual void set_frequencies(const double* frequencyList, */
+/*                                      size_t channelCount) = 0; */
 
-        virtual void start_aterm(
-            const std::complex<double>* aterm,
-            const size_t nrStations,
-            const size_t height,
-            const size_t width,
-            const size_t nrPolarizations) = 0;
+/*         virtual void set_stations(size_t nr_stations) = 0; */
 
-        virtual void finish_aterm() = 0;
+/*         virtual void set_image_size(double imageSize) = 0; */
 
+/*         virtual void set_w_kernel(size_t size) = 0; */
 
-        /* // Deridding */
-        /* virtual void queue_visibility_sampling( */
-        /*     //std::complex<float>* visibility, // size CH x PL */
-        /*     const double* uvwInMeters, */
-        /*     size_t antenna1, */
-        /*     size_t antenna2, */
-        /*     size_t timeIndex, */
-        /*     size_t rowId, */
-        /*     bool& isBufferFull) = 0; */
+/*         virtual void set_spheroidal( */
+/*             const double* spheroidal, */
+/*             size_t height, */
+/*             size_t width) = 0; */
 
-        virtual void flush() = 0;
+/*         virtual void start_w_layer(double layerWInLambda) = 0; */
 
-        /* virtual void finish_sampled_visibilities() = 0; */
+/*         virtual void finish_w_layer() = 0; */
 
-        /* virtual size_t get_sampling_buffer_size() const = 0; */
+/*         virtual void set_grid( */
+/*             std::complex<double>* grid, */
+/*             size_t nr_polarizations, */
+/*             size_t height, */
+/*             size_t width) = 0; */
 
-        /* virtual void get_sampling_buffer(size_t index, */
-        /*                                  std::complex<double>* data, */
-        /*                                  size_t& rowID) const = 0; */
-    };
+/*         virtual void bake() = 0; */
 
-} // end namespace idg
+/*         virtual void flush() = 0; */
 
-#endif
+/*         // Gridding */
+
+/*         virtual void grid_visibilities( */
+/*             const std::complex<float>* visibilities, // size CH x PL */
+/*             const double* uvwInMeters, */
+/*             size_t antenna1, */
+/*             size_t antenna2, */
+/*             size_t timeIndex) = 0; */
+
+/*         virtual void start_aterm( */
+/*             const std::complex<double>* aterm, */
+/*             size_t nrStations, */
+/*             size_t height, */
+/*             size_t width, */
+/*             size_t nrPolarizations) = 0; */
+
+/*         virtual void finish_aterm() = 0; */
+
+/*         /\* Deridding *\/ */
+/*         virtual void queue_visibility_sampling( */
+/*             const size_t rowId, */
+/*             const double* uvwInMeters, */
+/*             size_t antenna1, */
+/*             size_t antenna2, */
+/*             size_t timeIndex) = 0; */
+
+/*         virtual void get_sampling_buffer(size_t index, */
+/*                                          std::complex<double>* data, */
+/*                                          size_t& rowID) const = 0; */
+/*     }; */
+
+/* } // end namespace idg */
+
+/* #endif */
