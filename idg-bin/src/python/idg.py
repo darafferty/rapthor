7216dafad750b414988e6e23a313be2f8ebe4bc2
Matthias Petschow <m.petschow@googlemail.com>
2016-05-27 12:58:24 +0200

    Added python utility function to allocate and initialize the example data.

--- idg-bin/src/python/idg.py
+++ idg-bin/src/python/idg.py
@@ -2,6 +2,7 @@ import os
 import ctypes
 import numpy
 from ctypes.util import find_library
+from idgtypes import *
 
 def handle_error(library, e):
     if "libidg" in e.message:
@@ -45,25 +46,3 @@ try:
 except OSError:
     handle_error("utils", e)
 
-visibilitiestype = numpy.complex64
-uvwtype = numpy.dtype([('u', numpy.float32),
-                       ('v', numpy.float32),
-                       ('w', numpy.float32)])
-wavenumberstype = numpy.float32
-gridtype = numpy.complex64
-baselinetype = numpy.dtype([('station1', numpy.intc),
-                            ('station2', numpy.intc)])
-coordinatetype = numpy.dtype([('x', numpy.intc),
-                              ('y', numpy.intc)])
-metadatatype = numpy.dtype([ ('baseline_offset', numpy.intc),
-                             ('time_offset', numpy.intc),
-                             ('nr_timesteps', numpy.intc),
-                             ('aterm_index', numpy.intc),
-                             ('baseline', baselinetype),
-                             ('coordinate', coordinatetype)])
-atermtype = numpy.complex64
-atermoffsettype = numpy.intc
-spheroidaltype = numpy.float32
-
-FourierDomainToImageDomain = 0
-ImageDomainToFourierDomain = 1
