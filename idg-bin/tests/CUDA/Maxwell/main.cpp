a15820a671e69793624c59b212131f6c80bc111a
Bram Veenboer <bram.veenboer@gmail.com>
2016-06-02 09:09:07 +0200

    Added CUDA Maxwell test

---
+++ idg-bin/tests/CUDA/Maxwell/main.cpp
@@ -0,0 +1,12 @@
+#include "idg-cuda.h"
+#include "../../CPU/common/common.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    // Compares to reference implementation
+    int info = compare_to_reference<idg::proxy::cuda::Maxwell>();
+
+    return info;
+}
