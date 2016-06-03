f9d237a81b5d6619757cf7f6c64504679bd54b29
Bram Veenboer <bram.veenboer@gmail.com>
2016-06-03 13:53:25 +0200

    Updated OpenCL examples

--- idg-lib/examples/OpenCL/common/common.h
+++ idg-lib/examples/OpenCL/common/common.h
@@ -57,16 +57,16 @@ void run() {
     auto grid = new complex<float>[size_grid];
     auto baselines = new int[size_baselines];
 
-    idg::init_visibilities(visibilities, nr_baselines,
+    idg::init_example_visibilities(visibilities, nr_baselines,
                            nr_time,
                            nr_channels, nr_polarizations);
-    idg::init_uvw(uvw, nr_stations, nr_baselines, nr_time);
-    idg::init_wavenumbers(wavenumbers, nr_channels);
-    idg::init_aterm(aterm, nr_timeslots, nr_stations, subgridsize, nr_polarizations);
-    idg::init_aterm_offsets(aterm_offsets, nr_timeslots, nr_time);
-    idg::init_spheroidal(spheroidal, subgridsize);
-    idg::init_grid(grid, gridsize, nr_polarizations);
-    idg::init_baselines(baselines, nr_stations, nr_baselines);
+    idg::init_example_uvw(uvw, nr_stations, nr_baselines, nr_time);
+    idg::init_example_wavenumbers(wavenumbers, nr_channels);
+    idg::init_example_aterm(aterm, nr_timeslots, nr_stations, subgridsize, nr_polarizations);
+    idg::init_example_aterm_offsets(aterm_offsets, nr_timeslots, nr_time);
+    idg::init_example_spheroidal(spheroidal, subgridsize);
+    idg::init_example_grid(grid, gridsize, nr_polarizations);
+    idg::init_example_baselines(baselines, nr_stations, nr_baselines);
     clog << endl;
 
     // Initialize proxy
