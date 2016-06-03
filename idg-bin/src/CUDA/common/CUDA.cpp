4b458000eac43ebdd5dc8226ad10d87565181627
Bram Veenboer <bram.veenboer@gmail.com>
2016-06-03 11:42:58 +0200

    Updated CUDA performance reporting

--- idg-bin/src/CUDA/common/CUDA.cpp
+++ idg-bin/src/CUDA/common/CUDA.cpp
@@ -382,7 +382,8 @@ namespace idg {
 
                 // Initialize metadata
                 auto plan = create_plan(uvw, wavenumbers, baselines, aterm_offsets, kernel_size);
-                auto nr_subgrids = plan.get_nr_subgrids();
+                auto total_nr_subgrids   = plan.get_nr_subgrids();
+                auto total_nr_timesteps  = plan.get_nr_timesteps();
                 const Metadata *metadata = plan.get_metadata_ptr();
 
                 // Initialize
@@ -456,7 +457,8 @@ namespace idg {
                         int visibilities_elements = nr_time * nr_channels * nr_polarizations;
 
                         // Number of subgrids for all baselines in job
-                        auto current_nr_subgrids = plan.get_nr_subgrids(bl, current_nr_baselines);
+                        auto current_nr_subgrids  = plan.get_nr_subgrids(bl, current_nr_baselines);
+                        auto current_nr_timesteps = plan.get_nr_timesteps(bl, current_nr_baselines);
 
                         // Pointers to data for current batch
                         void *uvw_ptr          = (float *) h_uvw + bl * uvw_elements;
@@ -515,8 +517,8 @@ namespace idg {
                         double runtime_adder   = PowerSensor::seconds(powerRecords[3].state, powerRecords[4].state);
                         #if defined(REPORT_VERBOSE)
                         auxiliary::report("gridder", runtime_gridder,
-                                                     kernel_gridder->flops(current_nr_baselines, current_nr_subgrids),
-                                                     kernel_gridder->bytes(current_nr_baselines, current_nr_subgrids),
+                                                     kernel_gridder->flops(current_nr_timesteps, current_nr_subgrids),
+                                                     kernel_gridder->bytes(current_nr_timesteps, current_nr_subgrids),
                                                      PowerSensor::Watt(powerRecords[0].state, powerRecords[1].state));
                         auxiliary::report("    fft", runtime_fft,
                                                      kernel_fft->flops(subgridsize, current_nr_subgrids),
@@ -547,14 +549,14 @@ namespace idg {
 
                 #if defined(REPORT_VERBOSE) || defined(REPORT_TOTAL)
                 unique_ptr<GridFFT> kernel_fft = get_kernel_fft();
-                uint64_t total_flops_gridder  = kernel_gridder->flops(nr_baselines, nr_subgrids);
-                uint64_t total_bytes_gridder  = kernel_gridder->bytes(nr_baselines, nr_subgrids);
-                uint64_t total_flops_fft      = kernel_fft->flops(subgridsize, nr_subgrids);
-                uint64_t total_bytes_fft      = kernel_fft->bytes(subgridsize, nr_subgrids);
-                uint64_t total_flops_scaler   = kernel_scaler->flops(nr_subgrids);
-                uint64_t total_bytes_scaler   = kernel_scaler->bytes(nr_subgrids);
-                uint64_t total_flops_adder    = kernel_adder->flops(nr_subgrids);
-                uint64_t total_bytes_adder    = kernel_adder->bytes(nr_subgrids);
+                uint64_t total_flops_gridder  = kernel_gridder->flops(total_nr_timesteps, total_nr_subgrids);
+                uint64_t total_bytes_gridder  = kernel_gridder->bytes(total_nr_timesteps, total_nr_subgrids);
+                uint64_t total_flops_fft      = kernel_fft->flops(subgridsize, total_nr_subgrids);
+                uint64_t total_bytes_fft      = kernel_fft->bytes(subgridsize, total_nr_subgrids);
+                uint64_t total_flops_scaler   = kernel_scaler->flops(total_nr_subgrids);
+                uint64_t total_bytes_scaler   = kernel_scaler->bytes(total_nr_subgrids);
+                uint64_t total_flops_adder    = kernel_adder->flops(total_nr_subgrids);
+                uint64_t total_bytes_adder    = kernel_adder->bytes(total_nr_subgrids);
                 uint64_t total_flops_gridding = total_flops_gridder + total_flops_fft + total_flops_scaler + total_flops_adder;
                 uint64_t total_bytes_gridding = total_bytes_gridder + total_bytes_fft + total_bytes_scaler + total_bytes_adder;
                 double total_runtime_gridding = PowerSensor::seconds(startState, stopState);
@@ -602,7 +604,8 @@ namespace idg {
 
                 // Initialize metadata
                 auto plan = create_plan(uvw, wavenumbers, baselines, aterm_offsets, kernel_size);
-                auto nr_subgrids = plan.get_nr_subgrids();
+                auto total_nr_subgrids   = plan.get_nr_subgrids();
+                auto total_nr_timesteps  = plan.get_nr_timesteps();
                 const Metadata *metadata = plan.get_metadata_ptr();
 
                 // Initialize
@@ -675,7 +678,8 @@ namespace idg {
                         int visibilities_elements = nr_time * nr_channels * nr_polarizations;
 
                         // Number of subgrids for all baselines in job
-                        auto current_nr_subgrids = plan.get_nr_subgrids(bl, current_nr_baselines);
+                        auto current_nr_subgrids  = plan.get_nr_subgrids(bl, current_nr_baselines);
+                        auto current_nr_timesteps = plan.get_nr_timesteps(bl, current_nr_baselines);
 
                         // Pointers to data for current job
                         void *uvw_ptr          = (float *) h_uvw + bl * uvw_elements;
@@ -735,8 +739,8 @@ namespace idg {
                                                        kernel_fft->bytes(subgridsize, current_nr_subgrids),
                                                        PowerSensor::Watt(powerRecords[1].state, powerRecords[2].state));
                         auxiliary::report("degridder", runtime_degridder,
-                                                       kernel_degridder->flops(current_nr_baselines, current_nr_subgrids),
-                                                       kernel_degridder->bytes(current_nr_baselines, current_nr_subgrids),
+                                                       kernel_degridder->flops(current_nr_timesteps, current_nr_subgrids),
+                                                       kernel_degridder->bytes(current_nr_timesteps, current_nr_subgrids),
                                                        PowerSensor::Watt(powerRecords[3].state, powerRecords[4].state));
                         #endif
                         #if defined(REPORT_TOTAL)
@@ -758,12 +762,12 @@ namespace idg {
 
                 #if defined(REPORT_VERBOSE) || defined(REPORT_TOTAL)
                 unique_ptr<GridFFT> kernel_fft = get_kernel_fft();
-                uint64_t total_flops_splitter   = kernel_splitter->flops(nr_subgrids);
-                uint64_t total_bytes_splitter   = kernel_splitter->bytes(nr_subgrids);
-                uint64_t total_flops_fft        = kernel_fft->flops(subgridsize, nr_subgrids);
-                uint64_t total_bytes_fft        = kernel_fft->bytes(subgridsize, nr_subgrids);
-                uint64_t total_flops_degridder  = kernel_degridder->flops(nr_baselines, nr_subgrids);
-                uint64_t total_bytes_degridder  = kernel_degridder->bytes(nr_baselines, nr_subgrids);
+                uint64_t total_flops_splitter   = kernel_splitter->flops(total_nr_subgrids);
+                uint64_t total_bytes_splitter   = kernel_splitter->bytes(total_nr_subgrids);
+                uint64_t total_flops_fft        = kernel_fft->flops(subgridsize, total_nr_subgrids);
+                uint64_t total_bytes_fft        = kernel_fft->bytes(subgridsize, total_nr_subgrids);
+                uint64_t total_flops_degridder  = kernel_degridder->flops(total_nr_timesteps, total_nr_subgrids);
+                uint64_t total_bytes_degridder  = kernel_degridder->bytes(total_nr_timesteps, total_nr_subgrids);
                 uint64_t total_flops_degridding = total_flops_degridder + total_flops_fft + total_flops_splitter;
                 uint64_t total_bytes_degridding = total_bytes_degridder + total_bytes_fft + total_bytes_splitter;
                 double total_runtime_degridding = PowerSensor::seconds(startState, stopState);
