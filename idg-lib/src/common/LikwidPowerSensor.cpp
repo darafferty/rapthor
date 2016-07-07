6f871d0728e82447c4800fccc8a5c1867e082f79
Bram Veenboer <bram.veenboer@gmail.com>
2016-07-07 16:40:47 +0200

    Cleanup of LikwidPowerSensor compilation
    The Likwid power sensor is now automatically enabled when both required libraries (numa and likwid) are found

--- idg-lib/src/common/LikwidPowerSensor.cpp
+++ idg-lib/src/common/LikwidPowerSensor.cpp
@@ -8,13 +8,13 @@
 #include <omp.h>
 #include <unistd.h>
 
-#if defined(MEASURE_POWER_LIKWID)
+#if defined(HAVE_LIKWID)
 #if !defined __MIC__
 #include </usr/include/numa.h>
 #endif
 #endif
 
-#if defined(MEASURE_POWER_LIKWID)
+#if defined(HAVE_LIKWID)
 #include <likwid.h>
 #endif
 
@@ -23,7 +23,7 @@ LikwidPowerSensor::LikwidPowerSensor(const char *dumpFileName)
   dumpFile(dumpFileName == 0 ? 0 : new std::ofstream(dumpFileName)),
   stop(false)
 {
-#if defined(MEASURE_POWER_LIKWID)
+#if defined(HAVE_LIKWID)
 #if !defined __MIC__
   if (numa_init() != 0) {
     std::cerr << "numa_init() fails" << std::endl;
@@ -59,7 +59,7 @@ LikwidPowerSensor::~LikwidPowerSensor()
   if (dumpFile != 0) {
     stop = true;
 
-#if defined(MEASURE_POWER_LIKWID)
+#if defined(HAVE_LIKWID)
     if ((errno = pthread_join(thread, 0)) != 0)
       perror("pthread_join");
 
@@ -142,7 +142,7 @@ LikwidPowerSensor::State LikwidPowerSensor::read()
   state.consumedPKGenergy = 0;
   state.consumedDRAMenergy = 0;
 
-#if defined(MEASURE_POWER_LIKWID)
+#if defined(HAVE_LIKWID)
 #pragma omp critical (power)
   {
 #if !defined __MIC__
@@ -166,7 +166,7 @@ LikwidPowerSensor::State LikwidPowerSensor::read()
 
 double LikwidPowerSensor::Joules(const State &firstState, const State &secondState)
 {
-#if defined(MEASURE_POWER_LIKWID)
+#if defined(HAVE_LIKWID)
   // multiply with energy unit in this function and not in read(), to be tolerant to counter overflows
   return (secondState.consumedPKGenergy  - firstState.consumedPKGenergy ) * power_info.domains[PKG].energyUnit +
 	 (secondState.consumedDRAMenergy - firstState.consumedDRAMenergy) * power_info.domains[DRAM].energyUnit;
