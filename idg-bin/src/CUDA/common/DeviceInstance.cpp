218925ac05381e9e9b0c335e6a031cf8671a02c9
Bram Veenboer <bram.veenboer@gmail.com>
2016-07-29 11:47:14 +0200

    Tuned CUDA gridder batch and block size
    This does not significantly affect 8-channel performance, but slightly improves 1-channel performance

--- idg-bin/src/CUDA/common/DeviceInstance.cpp
+++ idg-bin/src/CUDA/common/DeviceInstance.cpp
@@ -156,12 +156,12 @@ namespace idg {
             }
 
             void DeviceInstance::set_parameters_pascal() {
-                block_gridder    = dim3(32, 4);
+                block_gridder = dim3(192);
                 block_degridder  = dim3(128);
                 block_adder      = dim3(128);
                 block_splitter   = dim3(128);
                 block_scaler     = dim3(128);
-                batch_gridder    = 32;
+                batch_gridder    = 64;
                 batch_degridder  = block_degridder.x;
             }
 
