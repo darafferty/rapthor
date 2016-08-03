28605a2a3554bdfcb75dac76073d03979d86ef34
Bram Veenboer <bram.veenboer@gmail.com>
2016-08-03 11:18:04 +0200

    Major cleanup of CUDA wrapper, code is now properly split in headers and implementation

--- idg-bin/src/CUDA/common/CU.h
+++ idg-bin/src/CUDA/common/CU.h
@@ -1,509 +1,220 @@
-#include <string.h>
-
-#if !defined CU_WRAPPER_H
+#ifndef CU_WRAPPER_H
 #define CU_WRAPPER_H
 
-#include <cuda.h>
-#include <vector_types.h>
-#include <exception>
 #include <string>
-#include <iostream>
+#include <cuda.h>
 
-#define checkCudaCall(val)	__checkCudaCall((val), #val, __FILE__, __LINE__)
+struct dim3;
 
 namespace cu {
-	class Error : public std::exception {
-		public:
-		Error(CUresult result):
-		_result(result)
-		{}
 
-		const char *what() const throw();
+    void init(unsigned flags = 0);
 
-		operator CUresult () const {
-			return _result;
-		}
+    class Error : public std::exception {
+        public:
+            Error(CUresult result);
 
-		private:
-			CUresult _result;
-	};
+            const char *what() const throw();
 
+            operator CUresult() const;
 
-	#if 1
-	inline void __checkCudaCall(CUresult result, char const *const func, const char *const file, int const line) {
-		if (result != CUDA_SUCCESS) {
-			std::cerr << "CUDA Error at " << file;
-			std::cerr << ":" << line;
-			std::cerr << " in function " << func;
-			std::cerr << ": " << Error(result).what();
-			std::cerr << std::endl;
-			exit(EXIT_FAILURE);
-		}
-	}
-	#else
-	inline void checkCudaCall(CUresult result) {
-		if (result != CUDA_SUCCESS) {
-			throw Error(result);
-		}
-	}
-	#endif
+        private:
+            CUresult _result;
+    };
 
 
-	inline void init(unsigned flags = 0) {
-		checkCudaCall(cuInit(flags));
-	}
+    class Device {
+        public:
+            Device();
+            Device(int ordinal);
+
+            static int getCount();
+            std::string getName() const;
+            size_t getTotalMem() const;
+            int getComputeCapability() const;
+            size_t free_memory();
+            size_t total_memory();
+            template <CUdevice_attribute attribute> int getAttribute() const;
+
+            operator CUdevice();
+
+        private:
+            CUdevice _device;
+    };
 
 
-	class Device {
-		public:
-			static int getCount() {
-				int nrDevices;
-				checkCudaCall(cuDeviceGetCount(&nrDevices));
-				return nrDevices;
-			}
+    class Context {
+        public:
+            Context();
+            Context(Device device, int flags = 0);
+            ~Context();
+            void setCurrent() const;
+            void setCacheConfig(CUfunc_cache config);
+            void setSharedMemConfig(CUsharedconfig config);
+            void synchronize();
 
-			Device() {}
+            operator CUcontext();
 
-			Device(int ordinal) {
-				checkCudaCall(cuDeviceGet(&_device, ordinal));
-			}
-
-			std::string getName() const {
-				char name[64];
-				checkCudaCall(cuDeviceGetName(name, sizeof(name), _device));
-				return std::string(name);
-			}
-
-            size_t getTotalMem() const {
-                size_t bytes = 0;
-                checkCudaCall(cuDeviceTotalMem(&bytes, _device));
-                return bytes;
-            }
-
-            int getComputeCapability() const {
-                int capability = 10 * getAttribute<CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR>() +
-                                      getAttribute<CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR>();
-                return capability;
-            }
-
-			template <CUdevice_attribute attribute> int getAttribute() const {
-				int value;
-				checkCudaCall(cuDeviceGetAttribute(&value, attribute, _device));
-				return value;
-			}
-
-			operator CUdevice () {
-				return _device;
-			}
-
-            size_t free_memory() {
-                size_t free;
-                size_t total;
-                cuMemGetInfo(&free, &total);
-                return free;
-            }
-
-            size_t total_memory() {
-                size_t free;
-                size_t total;
-                cuMemGetInfo(&free, &total);
-                return total;
-            }
-
-		private:
-			CUdevice _device;
-	};
+        private:
+            CUcontext _context;
+    };
 
 
-	class Context {
-		public:
-            Context() {
-                _context = NULL;
-            }
+    class HostMemory {
+        public:
+            HostMemory(size_t size, int flags = 0);
+            HostMemory(void *ptr, size_t size, int flags= 0);
+            ~HostMemory();
 
-			Context(Device device, int flags = 0) {
-				checkCudaCall(cuCtxCreate(&_context, flags, device));
-			}
+            size_t size();
+            void set(const void *in);
+            void set(void *in);
+            void set(void *in, size_t bytes);
+            void get(void *out);
+            void zero();
 
-			~Context() {
-                checkCudaCall(cuCtxDestroy(_context));
-			}
+            template <typename T> operator T *();
 
-			void setCurrent() const {
-				checkCudaCall(cuCtxSetCurrent(_context));
-			}
-
-			void setCacheConfig(CUfunc_cache config) {
-				checkCudaCall(cuCtxSetCacheConfig(config));
-			}
-
-			void setSharedMemConfig(CUsharedconfig config) {
-				checkCudaCall(cuCtxSetSharedMemConfig(config));
-			}
-
-            void synchronize() {
-                checkCudaCall(cuCtxSynchronize());
-            }
-
-			operator CUcontext () {
-				return _context;
-			}
-
-		private:
-			CUcontext _context;
-	};
-
-
-	class HostMemory {
-		public:
-			HostMemory(size_t size, int flags = 0) {
-				_size = size;
-				checkCudaCall(cuMemHostAlloc(&_ptr, size, flags));
-                free = true;
-			}
-
-            HostMemory(void *ptr, size_t size, int flags= 0) {
-                _size = size;
-                _ptr = ptr;
-                checkCudaCall(cuMemHostRegister(ptr, size, flags));
-                unregister = true;
-            }
-
-        	~HostMemory() {
-                if (free) {
-				    checkCudaCall(cuMemFreeHost(_ptr));
-                }
-                if (unregister) {
-                    cuMemHostUnregister(_ptr);
-                }
-			}
-
-            template <typename T> operator T * () {
-				return static_cast<T *>(_ptr);
-			}
-
-			size_t size() {
-				return _size;
-			}
-
-            void set(const void *in) {
-                memcpy(_ptr, in, (size_t) _size);
-            }
-
-            void set(void *in) {
-                memcpy(_ptr, in, (size_t) _size);
-            }
-
-            void set(void *in, size_t bytes) {
-                memcpy(_ptr, in, bytes);
-            }
-
-            void get(void *out) {
-                memcpy(out, _ptr, (size_t) _size);
-            }
-
-			void zero() {
-				memset(_ptr, 0, _size);
-			}
-
-		private:
-			void *_ptr;
-			size_t _size;
+        private:
+            void *_ptr;
+            size_t _size;
             bool free = false;
             bool unregister = false;
-	};
+    };
 
-	class DeviceMemory 	{
-		public:
-			DeviceMemory(size_t size) {
-				_size = size;
-				checkCudaCall(cuMemAlloc(&_ptr, size));
-                free = true;
-			}
+    class DeviceMemory  {
+        public:
+            DeviceMemory(size_t size);
+            DeviceMemory(void *ptr);
+            ~DeviceMemory();
 
-            DeviceMemory(void *ptr) {
-                cuMemHostGetDevicePointer(&_ptr, ptr, 0);
-            }
+            size_t size();
+            void set(void *in);
+            void get(void *out);
+            void* get(size_t offset);
+            void zero();
 
-			~DeviceMemory() {
-                if (free) {
-    				checkCudaCall(cuMemFree(_ptr));
-                }
-			}
+            operator CUdeviceptr();
+            operator const void*();
 
-			operator CUdeviceptr() {
-				return _ptr;
-			}
-
-			operator const void*() {
-				return &_ptr;
-			}
-
-			size_t size() {
-				return _size;
-			}
-
-            void set(void *in) {
-                cuMemcpyHtoD(_ptr, in, _size);
-            }
-
-            void get(void *out) {
-                cuMemcpyDtoH(out, _ptr, _size);
-            }
-
-            void* get(size_t offset) {
-                return (void *) (_ptr + offset);
-            }
-
-			void zero() {
-				cuMemsetD8(_ptr, 0, _size);
-			}
-
-		private:
-			CUdeviceptr _ptr;
-			size_t _size;
+        private:
+            CUdeviceptr _ptr;
+            size_t _size;
             bool free = false;
-
-	};
-
-
-	class Array {
-		public:
-			Array(unsigned width, CUarray_format format, unsigned numChannels) {
-				Array(width, 0, format, numChannels);
-			}
-
-			Array(unsigned width, unsigned height, CUarray_format format, unsigned numChannels) {
-				CUDA_ARRAY_DESCRIPTOR descriptor;
-				descriptor.Width       = width;
-				descriptor.Height      = height;
-				descriptor.Format      = format;
-				descriptor.NumChannels = numChannels;
-				checkCudaCall(cuArrayCreate(&_array, &descriptor));
-			}
-
-			Array(unsigned width, unsigned height, unsigned depth, CUarray_format format, unsigned numChannels) {
-				CUDA_ARRAY3D_DESCRIPTOR descriptor;
-				descriptor.Width       = width;
-				descriptor.Height      = height;
-				descriptor.Depth       = depth;
-				descriptor.Format      = format;
-				descriptor.NumChannels = numChannels;
-				descriptor.Flags       = 0;
-				checkCudaCall(cuArray3DCreate(&_array, &descriptor));
-			}
-
-			~Array() {
-				checkCudaCall(cuArrayDestroy(_array));
-			}
-
-			operator CUarray () {
-				return _array;
-			}
-
-		private:
-			CUarray _array;
-	};
+    };
 
 
-	class TexRef {
-		public:
-		TexRef(CUtexref texref):
-			_texref(texref)
-		{}
+    class Array {
+        public:
+            Array(unsigned width, CUarray_format format, unsigned numChannels);
+            Array(unsigned width, unsigned height, CUarray_format format, unsigned numChannels);
+            Array(unsigned width, unsigned height, unsigned depth, CUarray_format format, unsigned numChannels);
+            ~Array();
 
-		void setAddress(size_t &byte_offset, DeviceMemory &memory, size_t size) {
-			checkCudaCall(cuTexRefSetAddress(&byte_offset, _texref, memory, size));
-		}
+            operator CUarray();
 
-		void setArray(Array &array, unsigned flags = CU_TRSA_OVERRIDE_FORMAT) {
-			checkCudaCall(cuTexRefSetArray(_texref, array, flags));
-		}
-
-		void setAddressMode(int dim, CUaddress_mode am) {
-			checkCudaCall(cuTexRefSetAddressMode(_texref, dim, am));
-		}
-
-		void setFilterMode(CUfilter_mode fm) {
-			checkCudaCall(cuTexRefSetFilterMode(_texref, fm));
-		}
-
-		void setFlags(int flags) {
-			checkCudaCall(cuTexRefSetFlags(_texref, flags));
-		}
-
-		void setFormat(CUarray_format fmt, int numPackedComponents) {
-			checkCudaCall(cuTexRefSetFormat(_texref, fmt, numPackedComponents));
-		}
-
-		operator CUtexref() {
-			return _texref;
-		}
-
-		private:
-			CUtexref _texref;
-	};
+        private:
+            CUarray _array;
+    };
 
 
-	class Source {
-		public:
-			Source(const char *input_file_name);
+    class TexRef {
+        public:
+            TexRef(CUtexref texref);
 
-			void compile(const char *ptx_name, const char *compile_options = 0);
+            void setAddress(size_t &byte_offset, DeviceMemory &memory, size_t size);
+            void setArray(Array &array, unsigned flags = CU_TRSA_OVERRIDE_FORMAT);
+            void setAddressMode(int dim, CUaddress_mode am);
+            void setFilterMode(CUfilter_mode fm);
+            void setFlags(int flags);
+            void setFormat(CUarray_format fmt, int numPackedComponents);
 
-		private:
-			const char *input_file_name;
-	};
+            operator CUtexref();
 
-
-	class Module {
-		public:
-			Module(const char *file_name) {
-				checkCudaCall(cuModuleLoad(&_module, file_name));
-			}
-
-			Module(const void *data) {
-				checkCudaCall(cuModuleLoadData(&_module, data));
-			}
-
-			~Module() {
-				checkCudaCall(cuModuleUnload(_module));
-			}
-
-			TexRef getTexRef(const char *name) {
-				CUtexref texref;
-				checkCudaCall(cuModuleGetTexRef(&texref, _module, name));
-				return TexRef(texref);
-			}
-
-			operator CUmodule () {
-				return _module;
-			}
-
-		private:
-			CUmodule _module;
-	};
+        private:
+            CUtexref _texref;
+    };
 
 
-	class Function {
-		public:
-			Function(Module &module, const char *name) {
-				checkCudaCall(cuModuleGetFunction(&_function, module, name));
-			}
+    class Source {
+        public:
+            Source(const char *input_file_name);
 
-			int getAttribute(CUfunction_attribute attribute) {
-				int value;
-				checkCudaCall(cuFuncGetAttribute(&value, attribute, _function));
-				return value;
-			}
+            void compile(const char *ptx_name, const char *compile_options = 0);
 
-			void setCacheConfig(CUfunc_cache config) {
-				checkCudaCall(cuFuncSetCacheConfig(_function, config));
-			}
-
-			void paramSetTexRef(TexRef &texref) {
-				checkCudaCall(cuParamSetTexRef(_function, CU_PARAM_TR_DEFAULT, texref));
-			}
-
-			operator CUfunction () {
-				return _function;
-			}
-
-		private:
-			CUfunction _function;
-	};
+        private:
+            const char *input_file_name;
+    };
 
 
-	class Event {
-		public:
-			Event(int flags = CU_EVENT_DEFAULT) {
-				checkCudaCall(cuEventCreate(&_event, flags));
-			}
+    class Module {
+        public:
+            Module(const char *file_name);
+            Module(const void *data);
+            ~Module();
 
-			~Event() {
-				checkCudaCall(cuEventDestroy(_event));
-			}
+            TexRef getTexRef(const char *name);
 
-			void synchronize() {
-				checkCudaCall(cuEventSynchronize(_event));
-			}
+            operator CUmodule();
 
-			float elapsedTime(Event &second) {
-				float ms;
-				checkCudaCall(cuEventElapsedTime(&ms, second, _event));
-				return ms;
-			}
-
-			operator CUevent () {
-				return _event;
-			}
-
-		private:
-			CUevent _event;
-	};
+        private:
+            CUmodule _module;
+    };
 
 
-	class Stream {
-		public:
-			Stream(int flags = CU_STREAM_DEFAULT) {
-				checkCudaCall(cuStreamCreate(&_stream, flags));
-			}
+    class Function {
+        public:
+            Function(Module &module, const char *name);
 
-			~Stream() {
-				checkCudaCall(cuStreamDestroy(_stream));
-			}
+            int getAttribute(CUfunction_attribute attribute);
+            void setCacheConfig(CUfunc_cache config);
+            void paramSetTexRef(TexRef &texref);
 
-			void memcpyHtoDAsync(DeviceMemory &devPtr, const void *hostPtr) {
-				checkCudaCall(cuMemcpyHtoDAsync(devPtr, hostPtr, devPtr.size(), _stream));
-			}
+            operator CUfunction();
 
-			void memcpyHtoDAsync(CUdeviceptr devPtr, const void *hostPtr, size_t size) {
-				checkCudaCall(cuMemcpyHtoDAsync(devPtr, hostPtr, size, _stream));
-			}
+        private:
+            CUfunction _function;
+    };
 
-			void memcpyDtoHAsync(void *hostPtr, DeviceMemory &devPtr) {
-				checkCudaCall(cuMemcpyDtoHAsync(hostPtr, devPtr, devPtr.size(), _stream));
-			}
 
-			void memcpyDtoHAsync(void *hostPtr, CUdeviceptr devPtr, size_t size) {
-				checkCudaCall(cuMemcpyDtoHAsync(hostPtr, devPtr, size, _stream));
-			}
+    class Event {
+        public:
+            Event(int flags = CU_EVENT_DEFAULT);
+            ~Event();
 
-			void launchKernel(Function &function, unsigned gridX, unsigned gridY, unsigned gridZ, unsigned blockX, unsigned blockY, unsigned blockZ, unsigned sharedMemBytes, const void **parameters) {
-				checkCudaCall(cuLaunchKernel(function, gridX, gridY, gridZ, blockX, blockY, blockZ, sharedMemBytes, _stream, const_cast<void **>(parameters), 0));
-			}
+            void synchronize();
+            float elapsedTime(Event &second);
 
-			void launchKernel(Function &function, dim3 grid, dim3 block, unsigned sharedMemBytes, const void **parameters) {
-				checkCudaCall(cuLaunchKernel(function, grid.x, grid.y, grid.z, block.x, block.y, block.z, sharedMemBytes, _stream, const_cast<void **>(parameters), 0));
-			}
+            operator CUevent();
 
-			void query() {
-				checkCudaCall(cuStreamQuery(_stream));
-			}
+        private:
+            CUevent _event;
+    };
 
-			void synchronize() {
-				checkCudaCall(cuStreamSynchronize(_stream));
-			}
 
-			void waitEvent(Event &event) {
-				checkCudaCall(cuStreamWaitEvent(_stream, event, 0));
-			}
+    class Stream {
+        public:
+            Stream(int flags = CU_STREAM_DEFAULT);
+            ~Stream();
 
-			void addCallback(CUstreamCallback callback, void *userData, int flags = 0) {
-				checkCudaCall(cuStreamAddCallback(_stream, callback, userData, flags));
-			}
+            void memcpyHtoDAsync(DeviceMemory &devPtr, const void *hostPtr);
+            void memcpyHtoDAsync(CUdeviceptr devPtr, const void *hostPtr, size_t size);
+            void memcpyDtoHAsync(void *hostPtr, DeviceMemory &devPtr);
+            void memcpyDtoHAsync(void *hostPtr, CUdeviceptr devPtr, size_t size);
+            void launchKernel(Function &function, unsigned gridX, unsigned gridY, unsigned gridZ, unsigned blockX, unsigned blockY, unsigned blockZ, unsigned sharedMemBytes, const void **parameters);
+            void launchKernel(Function &function, dim3 grid, dim3 block, unsigned sharedMemBytes, const void **parameters);
+            void query();
+            void synchronize();
+            void waitEvent(Event &event);
+            void addCallback(CUstreamCallback callback, void *userData, int flags = 0);
+            void record(Event &event);
 
-			void record(Event &event) {
-				checkCudaCall(cuEventRecord(event, _stream));
-			}
+            operator CUstream();
 
-			operator CUstream () {
-				return _stream;
-			}
+        private:
+            CUstream _stream;
+    };
 
-		private:
-			CUstream _stream;
-	};
-}
+} // end namespace cu
 
 #endif
