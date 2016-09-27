f34a35c6da0446c5a4b9b2bcf17eabad2a931196
Bram Veenboer <bram.veenboer@gmail.com>
2016-09-27 09:56:56 +0200

    Added option to report ops instead of flops
    When enabled, both sincos and fma are counted as one operation. Previously sincos would be ignored and fma would be counted as two floating point operations.

--- idg-lib/src/common/Kernels.cpp
+++ idg-lib/src/common/Kernels.cpp
@@ -16,8 +16,9 @@ namespace kernel {
         flops_per_visibility += 5; // phase index
         flops_per_visibility += 5; // phase offset
         flops_per_visibility += nr_channels * 2; // phase
-        #if defined(COUNT_SINCOS_AS_FLOPS)
-        flops_per_visibility += nr_channels * FLOPS_PER_SINCOS; // phasor
+        #if defined(REPORT_OPS)
+        flops_per_visibility += nr_channels * 1; // phasor
+        flops_per_visibility += nr_channels * nr_polarizations * 8 / 2; // update
         #endif
         flops_per_visibility += nr_channels * nr_polarizations * 8; // update
 
@@ -41,21 +42,32 @@ namespace kernel {
 
         // Number of bytes per uvw coordinate
         uint64_t bytes_per_uvw = 0;
-        bytes_per_uvw += 1ULL * 3 * sizeof(float); // uvw
+        bytes_per_uvw += 1ULL * 3 * sizeof(float); // read uvw
 
         // Number of bytes per visibility
         uint64_t bytes_per_vis = 0;
-        bytes_per_vis += 1ULL * nr_channels * nr_polarizations * 2 * sizeof(float); // visibilities
+        bytes_per_vis += 1ULL * nr_channels * nr_polarizations * 2 * sizeof(float); // read visibilities
 
         // Number of bytes per pixel
         uint64_t bytes_per_pix = 0;
-        bytes_per_pix += 1ULL * nr_polarizations * 2 * sizeof(float); // pixel
+        bytes_per_pix += 1ULL * nr_polarizations * 2 * sizeof(float); // read pixel
+        bytes_per_pix += 1ULL * nr_polarizations * 2 * sizeof(float); // write pixel
+
+        // Number of bytes per aterm
+        uint64_t bytes_per_aterm = 0;
+        bytes_per_aterm += 1ULL * 2 * nr_polarizations * 2 * sizeof(float); // read aterm
+
+        // Number of bytes per spheroidal
+        uint64_t bytes_per_spheroidal = 0;
+        bytes_per_spheroidal += 1ULL * sizeof(float); // read spheroidal
 
         // Total number of bytes
         uint64_t bytes_total = 0;
         bytes_total += 1ULL * nr_timesteps * bytes_per_uvw;
         bytes_total += 1ULL * nr_timesteps * bytes_per_vis;
         bytes_total += 1ULL * nr_subgrids * subgridsize * subgridsize * bytes_per_pix;
+        bytes_total += 1ULL * nr_subgrids * subgridsize * subgridsize * bytes_per_aterm;
+        bytes_total += 1ULL * nr_subgrids * subgridsize * subgridsize * bytes_per_spheroidal;
         return bytes_total;
     }
 
