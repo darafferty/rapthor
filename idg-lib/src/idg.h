79188514ee38299e175409636aad8cd67fab16d3
Bram Veenboer <bram.veenboer@gmail.com>
2016-11-25 14:24:22 +0100

    Removed util and api. Added empty idg.cpp to still build libidg.so

--- idg-lib/src/idg.h
+++ idg-lib/src/idg.h
@@ -2,20 +2,14 @@
 #define IDG_HEADER_
 
 #include "idg-config.h"
-#include "api/GridderPlan.h"
-#include "api/DegridderPlan.h"
-#include "api/Datatypes.h"
-#include "api/Scheme.h"
 #if defined(BUILD_LIB_CPU)
 #include "idg-cpu.h"
 #endif
 #if defined(BUILD_LIB_CUDA)
-// TODO: fix
-//#include "idg-cuda.h"
+#include "idg-cuda.h"
 #endif
 #if defined(BUILD_LIB_OPENCL)
-// TODO: fix
-//#include "idg-opencl.h"
+#include "idg-opencl.h"
 #endif
 
 #endif
