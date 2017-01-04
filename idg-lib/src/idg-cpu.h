d10a9f64a83c7d275a681cbcc8dbb2d7e5845647
Bram Veenboer <bram.veenboer@gmail.com>
2017-01-04 11:56:14 +0100

    Added initial version of new CPU Reference2 proxy

--- idg-lib/src/idg-cpu.h
+++ idg-lib/src/idg-cpu.h
@@ -5,6 +5,7 @@
 #include "CPU/common/CPU.h"
 #include "CPU/common/Kernels.h"
 #include "CPU/Reference/Reference.h"
+#include "CPU/Reference2/Reference2.h"
 #include "CPU/Optimized/Optimized.h"
 #include "CPU/KNL/KNL.h"
 
