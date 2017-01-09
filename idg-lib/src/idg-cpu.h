ed63982f961f4e3c0c272e87287fc12f530a224a
Bram Veenboer <bram.veenboer@gmail.com>
2017-01-09 16:49:34 +0100

    Removed CPU proxy and Reference implementation
    The optimized and knl implementation have been disabled for now

--- idg-lib/src/idg-cpu.h
+++ idg-lib/src/idg-cpu.h
@@ -2,12 +2,10 @@
 #define IDG_CPU_HEADER
 
 #include "idg-common.h"
-#include "CPU/common/CPU.h"
 #include "CPU/common/CPU2.h"
 #include "CPU/common/KernelsCPU.h"
-#include "CPU/Reference/Reference.h"
 #include "CPU/Reference2/Reference2.h"
-#include "CPU/Optimized/Optimized.h"
-#include "CPU/KNL/KNL.h"
+//#include "CPU/Optimized/Optimized.h"
+//#include "CPU/KNL/KNL.h"
 
 #endif
