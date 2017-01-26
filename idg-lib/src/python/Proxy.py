ac189154a2c1720565c793fc79e8f5237a0f4d1b
Bram Veenboer <bram.veenboer@gmail.com>
2017-01-26 16:29:06 +0100

    Ongoing reimplementation of python interface

--- idg-lib/src/python/Proxy.py
+++ idg-lib/src/python/Proxy.py
@@ -24,7 +24,7 @@ class Proxy(object):
         baselines,
         grid,
         aterms,
-        aterms_offset,
+        aterms_offsets,
         spheroidal):
         """
         Grid visibilities onto grid.
@@ -47,7 +47,7 @@ class Proxy(object):
         :param aterms: numpy.ndarray(
                 shape=(nr_timeslots, nr_stations, height, width, nr_correlations),
                 dtype = idg.atermtype)
-        :param aterms_offset: numpy.ndarray(
+        :param aterms_offsets: numpy.ndarray(
                 shape=(nr_timeslots+1),
                 dtype = idg.atermoffsettype)
         :param spheroidal: numpy.ndarray(
@@ -62,18 +62,18 @@ class Proxy(object):
         visibilities_nr_correlations = visibilities.shape[3]
         uvw_nr_baselines             = uvw.shape[0]
         uvw_nr_timesteps             = uvw.shape[1]
-        uvw_nr_coordinates           = uvw.shape[2]
+        uvw_nr_coordinates           = 3
         baselines_nr_baselines       = baselines.shape[0]
-        baselines_two                = baselines.shape[1]
+        baselines_two                = 2
         grid_nr_correlations         = grid.shape[0]
-        grid_height                  = grid.shape[0]
-        grid_width                   = grid.shape[0]
+        grid_height                  = grid.shape[1]
+        grid_width                   = grid.shape[2]
         aterms_nr_timeslots          = aterms.shape[0]
         aterms_nr_stations           = aterms.shape[1]
         aterms_aterm_height          = aterms.shape[2]
         aterms_aterm_width           = aterms.shape[3]
         aterms_nr_correlations       = aterms.shape[4]
-        aterms_offsets_nr_timeslots  = aterms_offset.shape[0]
+        aterms_offsets_nr_timeslots  = aterms_offsets.shape[0]
         spheroidal_height            = spheroidal.shape[0]
         spheroidal_width             = spheroidal.shape[1]
 
@@ -124,7 +124,7 @@ class Proxy(object):
         baselines,
         grid,
         aterms,
-        aterms_offset,
+        aterms_offsets,
         spheroidal):
         """
         Degrid visibilities from grid.
@@ -147,7 +147,7 @@ class Proxy(object):
         :param aterms: numpy.ndarray(
                 shape=(nr_timeslots, nr_stations, height, width, nr_correlations),
                 dtype = idg.atermtype)
-        :param aterms_offset: numpy.ndarray(
+        :param aterms_offsets: numpy.ndarray(
                 shape=(nr_timeslots+1),
                 dtype = idg.atermoffsettype)
         :param spheroidal: numpy.ndarray(
@@ -162,23 +162,23 @@ class Proxy(object):
         visibilities_nr_correlations = visibilities.shape[3]
         uvw_nr_baselines             = uvw.shape[0]
         uvw_nr_timesteps             = uvw.shape[1]
-        uvw_nr_coordinates           = uvw.shape[2]
+        uvw_nr_coordinates           = 3
         baselines_nr_baselines       = baselines.shape[0]
-        baselines_two                = baselines.shape[1]
+        baselines_two                = 2
         grid_nr_correlations         = grid.shape[0]
-        grid_height                  = grid.shape[0]
-        grid_width                   = grid.shape[0]
+        grid_height                  = grid.shape[1]
+        grid_width                   = grid.shape[2]
         aterms_nr_timeslots          = aterms.shape[0]
         aterms_nr_stations           = aterms.shape[1]
         aterms_aterm_height          = aterms.shape[2]
         aterms_aterm_width           = aterms.shape[3]
         aterms_nr_correlations       = aterms.shape[4]
-        aterms_offsets_nr_timeslots  = aterms_offset.shape[0]
+        aterms_offsets_nr_timeslots  = aterms_offsets.shape[0]
         spheroidal_height            = spheroidal.shape[0]
         spheroidal_width             = spheroidal.shape[1]
 
         # call C function to do the work
-        self._cwrap_degriddding(
+        self._cwrap_degridding(
             w_offset,
             cell_size,
             kernel_size,
@@ -227,50 +227,90 @@ class Proxy(object):
         """
         # extract dimesions
         nr_correlations = grid.shape[0]
-        height          = grid.shape[0]
-        width           = grid.shape[0]
+        height          = grid.shape[1]
+        width           = grid.shape[2]
 
         # call C function to do the work
         self._cwrap_transform(
             direction,
             grid,
             nr_correlations,
-            grid_height,
-            grid_width)
+            height,
+            width)
 
  
     # TODO: create plan
 
     # Wrapper to C function (override for each class inheriting from this)
     def _cwrap_gridding(
-        self,
         w_offset,
         cell_size,
         kernel_size,
         frequencies,
+        nr_channels,
         visibilities,
+        visibilities_nr_baselines,
+        visibilities_nr_timesteps,
+        visibilities_nr_channels,
+        visibilities_nr_correlations,
         uvw,
+        uvw_nr_baselines,
+        uvw_nr_timesteps,
+        uvw_nr_coordinates,
         baselines,
+        baselines_nr_baselines,
+        baselines_two,
         grid,
+        grid_nr_correlations,
+        grid_height,
+        grid_width,
         aterms,
-        aterms_offset,
-        spheroidal):
+        aterms_nr_timeslots,
+        aterms_nr_stations,
+        aterms_aterm_height,
+        aterms_aterm_width,
+        aterms_nr_correlations,
+        aterms_offsets,
+        aterms_offsets_nr_timeslots, # plus one
+        spheroidal,
+        spheroidal_height,
+        spheroidal_width):
         pass
 
     # Wrapper to C function (override for each class inheriting from this)
     def _cwrap_degridding(
-        self,
         w_offset,
         cell_size,
         kernel_size,
         frequencies,
+        nr_channels,
         visibilities,
+        visibilities_nr_baselines,
+        visibilities_nr_timesteps,
+        visibilities_nr_channels,
+        visibilities_nr_correlations,
         uvw,
+        uvw_nr_baselines,
+        uvw_nr_timesteps,
+        uvw_nr_coordinates,
         baselines,
+        baselines_nr_baselines,
+        baselines_two,
         grid,
+        grid_nr_correlations,
+        grid_height,
+        grid_width,
         aterms,
-        aterms_offset,
-        spheroidal):
+        aterms_nr_timeslots,
+        aterms_nr_stations,
+        aterms_aterm_height,
+        aterms_aterm_width,
+        aterms_nr_correlations,
+        aterms_offsets,
+        aterms_offsets_nr_timeslots,
+        spheroidal,
+        spheroidal_height,
+        spheroidal_width):
         pass
 
     # Wrapper to C function (override for each class inheriting from this)
