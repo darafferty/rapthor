a16d34e82299f85df82a1b4c736c52afb0086d9f
Bram Veenboer <bram.veenboer@gmail.com>
2017-02-06 16:37:14 +0100

    Updated example according to new report_visibilities method

--- idg-bin/examples/cxx/common/common.h
+++ idg-bin/examples/cxx/common/common.h
@@ -210,6 +210,7 @@ void run()
     }
 
     // Report throughput
-    idg::auxiliary::report_visibilities("gridding", runtime_gridding, nr_baselines, nr_timesteps, nr_channels);
-    idg::auxiliary::report_visibilities("degridding", runtime_degridding, nr_baselines, nr_timesteps, nr_channels);
+    auto nr_visibilities = nr_baselines * nr_timesteps * nr_channels;
+    idg::auxiliary::report_visibilities("gridding", runtime_gridding, nr_visibilities);
+    idg::auxiliary::report_visibilities("degridding", runtime_degridding, nr_visibilities);
 }
