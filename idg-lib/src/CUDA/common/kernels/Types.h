02d99173bccaa61bd068069ea4c2f93eacc54be4
Sebastiaan van der Tol <tol@astron.nl>
2017-02-15 13:30:23 +0100

    Add w_index to metadata types for CUDA and OpenCL

--- idg-lib/src/CUDA/common/kernels/Types.h
+++ idg-lib/src/CUDA/common/kernels/Types.h
@@ -7,7 +7,8 @@ typedef struct { float u, v, w; } UVW;
 typedef struct { int x, y; } Coordinate;
 typedef struct { int station1, station2; } Baseline;
 typedef struct { int baseline_offset; int time_offset; int nr_timesteps;
-                 int aterm_index; Baseline baseline; Coordinate coordinate; } Metadata;
+                 int aterm_index; int w_index;
+                 Baseline baseline; Coordinate coordinate; } Metadata;
 
 /*
     Complex numbers
