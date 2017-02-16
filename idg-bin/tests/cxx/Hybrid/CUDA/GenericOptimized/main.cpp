2ea3141298c35a7b6fd2cb509471b89604991bfa
Bram Veenboer <bram.veenboer@gmail.com>
2017-02-16 11:10:48 +0100

    Added cxx test hybrid-cuda-generic-optimized

--- idg-bin/tests/cxx/Hybrid/CUDA/GenericOptimized/main.cpp
+++ idg-bin/tests/cxx/Hybrid/CUDA/GenericOptimized/main.cpp
@@ -1,12 +1,12 @@
 #include "idg-hybrid-cuda.h"
-#include "../../CPU/common/common.h"
+#include "common.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     // Compares to reference implementation
-    int info = compare_to_reference<idg::proxy::hybrid::HybridCUDA>();
+    int info = compare_to_reference<idg::proxy::hybrid::GenericOptimized>();
 
     return info;
 }
