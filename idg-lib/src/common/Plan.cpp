df50de2532d15b6f5b79b782eee9cea3c514011a
Sebastiaan van der Tol <tol@astron.nl>
2017-03-28 11:32:20 +0200

    Fix check of uv limits

--- idg-lib/src/common/Plan.cpp
+++ idg-lib/src/common/Plan.cpp
@@ -272,7 +272,7 @@ namespace idg {
                     int  uv_min_pixels = min(coordinate.x, coordinate.y);
                     int  w_index = coordinate.z;
                     bool uvw_in_range = uv_min_pixels >= 1 && 
-                                        uv_max_pixels < (grid_size - subgrid_size) &&
+                                        uv_max_pixels <= (grid_size - subgrid_size) &&
                                         w_index >= -nr_w_layers &&
                                         w_index < nr_w_layers;
 
