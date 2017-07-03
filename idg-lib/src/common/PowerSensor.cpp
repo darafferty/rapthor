2fe669874f652181e1f2c54aa1228080b8588197
Bram Veenboer <bram.veenboer@gmail.com>
2017-07-03 09:31:43 +0200

    Implemented get_power_sensor method in PowerSensor
    This method takes care of initializing both host and device power
    sensors, instead of relying of a single list which has to be compared by
    the list of compute devices to be used.

--- idg-lib/src/common/PowerSensor.cpp
+++ idg-lib/src/common/PowerSensor.cpp
@@ -1,5 +1,6 @@
 #include "PowerSensor.h"
 
+#include "idg-common.h"
 
 namespace powersensor {
 
@@ -15,6 +16,48 @@ namespace powersensor {
             return false;
         }
     }
+
+    PowerSensor* get_power_sensor(
+        const std::string name,
+        const int i)
+    {
+        // Determine which environment variable to read
+        std::string power_sensor_env_str;
+        if (name.compare(sensor_host) == 0) {
+            power_sensor_env_str = sensor_host;
+        } else if (name.compare(sensor_device) == 0) {
+            power_sensor_env_str = sensor_device;
+        } else {
+            power_sensor_env_str = sensor_default;
+        }
+
+        // Read environment variable
+        char *power_sensor_char = getenv(power_sensor_env_str.c_str());
+        if (!power_sensor_char) {
+            power_sensor_char = getenv(sensor_default.c_str());
+        }
+
+        // Split environment variable value
+        std::vector<std::string> power_sensors = idg::auxiliary::split_string(power_sensor_char, sensor_delimiter);
+
+        // Try to initialize the specified PowerSensor
+        if (power_sensors.size() > 0 && i < power_sensors.size()) {
+            std::string power_sensor_str = power_sensors[i];
+            if (power_sensor_str.compare(name_likwid)) {
+                return likwid::LikwidPowerSensor::create();
+            } else if (power_sensor_str.compare(name_rapl)) {
+                return rapl::RaplPowerSensor::create();
+            } else if (power_sensor_str.compare(name_nvml)) {
+                return nvml::NVMLPowerSensor::create(i, NULL);
+            } else if (power_sensor_str.find(name_arduino) != std::string::npos) {
+                return arduino::ArduinoPowerSensor::create(power_sensor_str.c_str(), NULL);
+            }
+        }
+
+        // Use the DummyPowerSensor as backup
+        return powersensor::DummyPowerSensor::create();
+    }
+
 
     #if not defined(HAVE_POWERSENSOR)
     class DummyPowerSensor_ : public DummyPowerSensor {
