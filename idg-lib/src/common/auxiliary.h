333114c0f185b998e2696825d7959cf8492d4780
Bram Veenboer <bram.veenboer@gmail.com>
2017-09-21 11:08:24 +0200

    Updated CPU proxy to new auxiliary routines

--- idg-lib/src/common/auxiliary.h
+++ idg-lib/src/common/auxiliary.h
@@ -47,12 +47,14 @@ namespace idg {
         uint64_t flops_degridder(
             uint64_t nr_channels,
             uint64_t nr_timesteps,
+            uint64_t nr_subgrids,
             uint64_t subgrid_size,
             uint64_t nr_correlations = 4);
 
         uint64_t bytes_degridder(
             uint64_t nr_channels,
             uint64_t nr_timesteps,
+            uint64_t nr_subgrids,
             uint64_t subgrid_size,
             uint64_t nr_correlations = 4);
 
