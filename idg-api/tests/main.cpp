ec5ae67e6a53b7153aea29aafd101624c093de08
Bram Veenboer <bram.veenboer@gmail.com>
2017-11-03 16:31:35 +0100

    Added dummy test

---
+++ idg-api/tests/main.cpp
@@ -0,0 +1,21 @@
+int main(int argc, char *argv[])
+{
+    /*
+    * idg::GridderBuffer plan(...);
+    * plan.set_grid(grid);
+    * plan.set_other_properties(...);
+    * plan.bake();
+    *
+    * for (auto row = 0; row < nr_rows; ++row) {
+    *    gridder.grid_visibilities(...);
+    * }
+    *
+    * // Make sure no visibilites are still in the buffer
+    * gridder.finished();
+    *
+    * // Transform the gridded visibilities to an image
+    * gridder.transform_grid();
+    */
+
+    return 0;
+}
