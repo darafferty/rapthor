cc122e5f0fbcfdd43f2ebdb1ed68b20ff56683d7
Bram Veenboer <bram.veenboer@gmail.com>
2018-02-16 11:26:16 +0100

    Added test and example for for CUDA Unified proxy

---
+++ idg-bin/tests/cxx/CUDA/Unified/main.cpp
@@ -0,0 +1,12 @@
+#include "idg-cuda.h"
+#include "common.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    // Compares to reference implementation
+    int info = compare_to_reference<idg::proxy::cuda::Unified>();
+
+    return info;
+}
