164d44b014f40b4009889dd55e111336895ece6b
Bram Veenboer <bram.veenboer@gmail.com>
2018-03-06 14:12:29 +0100

    Use new Data class in python example

--- idg-bin/examples/python/common.py
+++ idg-bin/examples/python/common.py
@@ -2,6 +2,7 @@
 # (or setting the PYTHONPATH manually), you can import the idg module
 import idg
 import util
+from data import Data
 import numpy
 import matplotlib.pyplot as plt
 import random
@@ -9,20 +10,21 @@ import random
 ############
 # paramaters
 ############
-_nr_stations      = 50
-_nr_baselines     = _nr_stations*(_nr_stations-1)/2
-_nr_channels      = 16
-_nr_timesteps     = 1024           # samples per baseline
+_nr_channels      = 8
+_nr_timesteps     = 1*60*60           # samples per baseline
 _nr_timeslots     = 16             # A-term time slots
-_image_size       = 0.05
 _subgrid_size     = 24
-_grid_size        = 2048
-_integration_time = 1
+_grid_size        = 1024
+_integration_time = 0.9
 _kernel_size      = (_subgrid_size / 2) + 1
 _nr_correlations  = 4
+_layout_file           = "SKA1_low_ecef"
+_nr_stations_limit     = -1    # no limit
+_baseline_length_limit = 5000 # m
+_start_frequency       = 150e6 # Mhz
 
-def get_nr_stations():
-    return _nr_stations
+def get_nr_stations_limit():
+    return _nr_stations_limit
 
 def get_nr_baselines():
     return _nr_baselines
@@ -36,14 +38,8 @@ def get_nr_timesteps():
 def get_nr_timeslots():
     return _nr_timeslots
 
-def get_image_size():
-    return _image_size
-
 def get_subgrid_size():
     return _subgrid_size
-
-def get_cell_size():
-    return _image_size / _grid_size
 
 def get_grid_size():
     return _grid_size
@@ -56,6 +52,15 @@ def get_kernel_size():
 
 def get_nr_correlations():
     return _nr_correlations
+
+def get_layout_file():
+    return _layout_file
+
+def get_baseline_length_limit():
+    return _baseline_length_limit
+
+def get_start_frequency():
+    return _start_frequency
 
 #################
 # initialize data
@@ -120,19 +125,28 @@ def main(proxyname):
     ######################################################################
     # Set parameters
     ######################################################################
-    nr_stations      = get_nr_stations()
-    nr_baselines     = get_nr_baselines()
     nr_channels      = get_nr_channels()
     nr_timesteps     = get_nr_timesteps()
     nr_timeslots     = get_nr_timeslots()
-    image_size       = get_image_size()
-    cell_size        = get_cell_size()
     subgrid_size     = get_subgrid_size()
     grid_size        = get_grid_size()
     integration_time = get_integration_time()
     kernel_size      = get_kernel_size()
     nr_correlations  = get_nr_correlations()
     w_offset         = 0.0
+    layout_file           = get_layout_file()
+    nr_stations_limit     = get_nr_stations_limit()
+    baseline_length_limit = get_baseline_length_limit()
+    start_frequency       = get_start_frequency()
+
+    ######################################################################
+    # initialize data generator
+    ######################################################################
+    data = Data(grid_size, nr_stations_limit, baseline_length_limit, layout_file, start_frequency)
+    nr_stations     = data.get_nr_stations()
+    nr_baselines    = data.get_nr_baselines()
+    image_size      = data.get_image_size()
+    cell_size       = image_size / grid_size
 
     ######################################################################
     # initialize proxy
@@ -157,9 +171,16 @@ def main(proxyname):
     ######################################################################
     # initialize data
     ######################################################################
-    uvw            = util.get_example_uvw(
-                        nr_baselines, nr_timesteps, integration_time)
-    frequencies    = util.get_example_frequencies(nr_channels)
+    channel_offset  = 5
+    baseline_offset = 1000
+    time_offset     = 100
+    nr_baselines    = 500
+
+    uvw            = numpy.zeros((nr_baselines, nr_timesteps), dtype=idg.uvwtype)
+    frequencies    = numpy.zeros((nr_channels), dtype=idg.frequenciestype)
+    data.get_frequencies(frequencies, nr_channels, channel_offset)
+    data.get_uvw(uvw, nr_baselines, nr_timesteps, baseline_offset, time_offset, integration_time)
+
     baselines      = util.get_example_baselines(nr_baselines)
     grid           = util.get_example_grid(
                         nr_correlations, grid_size)
