9522a88d262c7d4c49337547c3172520c31eaa41
Bram Veenboer <bram.veenboer@gmail.com>
2018-05-04 14:47:25 +0200

    Purged src/Hybird/CUDA/common

--- idg-lib/src/idg-hybrid-cuda.h
+++ idg-lib/src/idg-hybrid-cuda.h
@@ -4,7 +4,6 @@
 #include "idg-common.h"
 #include "idg-cpu.h"
 #include "idg-cuda.h"
-#include "Hybrid/CUDA/common/HybridCUDA.h"
 #include "Hybrid/CUDA/GenericOptimized/GenericOptimized.h"
 #include "Hybrid/CUDA/UnifiedOptimized/UnifiedOptimized.h"
 
