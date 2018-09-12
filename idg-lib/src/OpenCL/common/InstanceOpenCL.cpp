3b91916ccee63c0a3821aeb90465cf11fb2595d7
Bram Veenboer <bram.veenboer@gmail.com>
2018-09-12 09:52:21 +0200

    Update tuning parameters

--- idg-lib/src/OpenCL/common/InstanceOpenCL.cpp
+++ idg-lib/src/OpenCL/common/InstanceOpenCL.cpp
@@ -80,8 +80,8 @@ namespace idg {
 
             void InstanceOpenCL::set_parameters_default() {
                 batch_gridder   = 32;
-                batch_degridder = 192;
-                block_gridder   = cl::NDRange(128, 1);
+                batch_degridder = 128;
+                block_gridder   = cl::NDRange(256, 1);
                 block_degridder = cl::NDRange(256, 1);
                 block_adder     = cl::NDRange(128, 1);
                 block_splitter  = cl::NDRange(128, 1);
