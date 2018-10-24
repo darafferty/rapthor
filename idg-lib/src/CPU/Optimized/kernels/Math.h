cc1fa7b8f4540770d1649f204bd5b483fca72a40
Bram Veenboer <bram.veenboer@gmail.com>
2018-10-24 11:37:58 +0200

    Move sine/cosine lookup table code to Lookup_01.h

--- idg-lib/src/CPU/Optimized/kernels/Math.h
+++ idg-lib/src/CPU/Optimized/kernels/Math.h
@@ -6,21 +6,7 @@
 #include "common/Math.h"
 
 #if defined(USE_LOOKUP)
-// Floating-point PI values
-#define PI     float(M_PI)
-#define TWO_PI float(2 * M_PI)
-#define HLF_PI float(M_PI_2)
-
-// Integer representations of PI
-#define TWO_PI_INT        16384
-#define PI_INT            TWO_PI_INT / 2
-#define HLF_PI_INT        TWO_PI_INT / 4
-
-// Constants for sine/cosine lookup table
-#define NR_SAMPLES        TWO_PI_INT
-
-// Lookup table
-#define CREATE_LOOKUP float lookup[NR_SAMPLES]; compute_lookup(lookup);
+#include "Lookup_01.h"
 #endif
 
 // Alignment
@@ -43,50 +29,6 @@ inline void compute_sincos(
     }
     #endif
 }
-
-#if defined(USE_LOOKUP)
-inline void compute_lookup(
-    float* __restrict__ lookup)
-{
-    for (unsigned i = 0; i < NR_SAMPLES; i++) {
-        lookup[i] = sinf(i * (TWO_PI / TWO_PI_INT));
-    }
-}
-
-inline idg::float2 compute_sincos(
-    const float* __restrict__ lookup,
-    const float x)
-{
-    float p = x;
-
-    // Convert to integer pi range [0:NR_SAMPLES]
-    unsigned p_int = round(p * (TWO_PI_INT / TWO_PI));
-
-    // Shift p in range [0:2*pi]
-    p_int &= (TWO_PI_INT - 1);
-
-    return {
-        lookup[(p_int+HLF_PI_INT) & (TWO_PI_INT - 1)],
-        lookup[p_int]};
-}
-
-inline void compute_sincos(
-    const int                 n,
-    const float* __restrict__ x,
-    const float* __restrict__ lookup,
-    float*       __restrict__ sin,
-    float*       __restrict__ cos)
-{
-    #if defined(__INTEL_COMPILER)
-    #pragma vector aligned(x, sin, cos)
-    #endif
-    for (int i = 0; i < n; i++) {
-        idg::float2 phasor = compute_sincos(lookup, x[i]);
-        cos[i] = phasor.real;
-        sin[i] = phasor.imag;
-    }
-}
-#endif
 
 // http://bit.ly/2shIfmP
 #if defined(__AVX__)
