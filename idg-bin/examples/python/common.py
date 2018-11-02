b901c20ef917bd0d912129c36d1807b10dec704a
Bram Veenboer <bram.veenboer@gmail.com>
2018-11-02 11:34:20 +0100

    Update parameters for python example
    These were not working. They have probably been committed while the Data
    class was introduced.

--- idg-bin/examples/python/common.py
+++ idg-bin/examples/python/common.py
@@ -11,7 +11,7 @@ import random
 # paramaters
 ############
 _nr_channels      = 8
-_nr_timesteps     = 1*60*60           # samples per baseline
+_nr_timesteps     = 3*60*60           # samples per baseline
 _nr_timeslots     = 16             # A-term time slots
 _subgrid_size     = 24
 _grid_size        = 1024
@@ -19,15 +19,12 @@ _integration_time = 0.9
 _kernel_size      = (_subgrid_size / 2) + 1
 _nr_correlations  = 4
 _layout_file           = "SKA1_low_ecef"
-_nr_stations_limit     = -1    # no limit
-_baseline_length_limit = 5000 # m
+_nr_stations_limit     = 20
+_baseline_length_limit = 3000 # m
 _start_frequency       = 150e6 # Mhz
 
 def get_nr_stations_limit():
     return _nr_stations_limit
-
-def get_nr_baselines():
-    return _nr_baselines
 
 def get_nr_channels():
     return _nr_channels
@@ -133,7 +130,7 @@ def main(proxyname):
     integration_time = get_integration_time()
     kernel_size      = get_kernel_size()
     nr_correlations  = get_nr_correlations()
-    w_step         = 0.0
+    w_step           = 0.0
     layout_file           = get_layout_file()
     nr_stations_limit     = get_nr_stations_limit()
     baseline_length_limit = get_baseline_length_limit()
@@ -171,10 +168,9 @@ def main(proxyname):
     ######################################################################
     # initialize data
     ######################################################################
-    channel_offset  = 5
-    baseline_offset = 1000
-    time_offset     = 100
-    nr_baselines    = 500
+    channel_offset  = 0
+    baseline_offset = 0
+    time_offset     = 0
 
     uvw            = numpy.zeros((nr_baselines, nr_timesteps), dtype=idg.uvwtype)
     frequencies    = numpy.zeros((nr_channels), dtype=idg.frequenciestype)
