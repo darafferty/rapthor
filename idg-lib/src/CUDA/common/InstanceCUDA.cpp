fe6afa35285852c8b401875701da6b40b9a96f06
Bram Veenboer <bram.veenboer@gmail.com>
2018-12-13 19:58:09 +0100

    Update gp100 tuning parameters

--- idg-lib/src/CUDA/common/InstanceCUDA.cpp
+++ idg-lib/src/CUDA/common/InstanceCUDA.cpp
@@ -295,7 +295,8 @@ namespace idg {
 
             void InstanceCUDA::set_parameters_gp100() {
                 batch_gridder    = 256;
-                batch_degridder  = 256;
+                batch_degridder  = 128;
+                block_degridder  = 64;
             }
 
             void InstanceCUDA::set_parameters_pascal() {
