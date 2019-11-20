bc1acb41a13db9bcad883b13eeb9c38be90ba1cf
Bram Veenboer <bram.veenboer@gmail.com>
2019-11-20 11:14:55 +0100

    Add CUDA atomicAdd(double*, double) for older architectures
    The implementation is taken from:
    https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#atomic-functions

--- idg-lib/src/CUDA/common/kernels/math.cu
+++ idg-lib/src/CUDA/common/kernels/math.cu
@@ -42,6 +42,25 @@ inline  __device__ void atomicAdd(float2 *a, float2 b) {
     atomicAdd(&a->y, b.y);
 }
 
+#if __CUDA_ARCH__ < 600
+__device__ double atomicAdd(double* address, double val)
+{
+    unsigned long long int* address_as_ull = (unsigned long long int*) address;
+    unsigned long long int old = *address_as_ull, assumed;
+
+    do {
+        assumed = old;
+        old = atomicCAS(
+			address_as_ull, assumed,
+			__double_as_longlong(val + __longlong_as_double(assumed)));
+
+    // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
+    } while (assumed != old);
+
+    return __longlong_as_double(old);
+}
+#endif
+
 inline __device__ float raw_sin(float a)
 {
     float r;
