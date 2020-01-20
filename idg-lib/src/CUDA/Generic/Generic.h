1e3ed73a90188ee4ebac7e1c69ff97034c2badb7
Bram Veenboer <bram.veenboer@gmail.com>
2020-01-20 14:03:34 +0100

    Remove Generic::do_gridding_legacy

--- idg-lib/src/CUDA/Generic/Generic.h
+++ idg-lib/src/CUDA/Generic/Generic.h
@@ -39,22 +39,6 @@ namespace idg {
                         const Array1D<unsigned int>& aterms_offsets,
                         const Array2D<float>& spheroidal) override;
 
-                    virtual void do_gridding_legacy(
-                        const Plan& plan,
-                        const float w_step, // in lambda
-                        const Array1D<float>& shift,
-                        const float cell_size,
-                        const unsigned int kernel_size, // full width in pixels
-                        const unsigned int subgrid_size,
-                        const Array1D<float>& frequencies,
-                        const Array3D<Visibility<std::complex<float>>>& visibilities,
-                        const Array2D<UVW<float>>& uvw,
-                        const Array1D<std::pair<unsigned int,unsigned int>>& baselines,
-                        Grid& grid,
-                        const Array4D<Matrix2x2<std::complex<float>>>& aterms,
-                        const Array1D<unsigned int>& aterms_offsets,
-                        const Array2D<float>& spheroidal);
-
                     virtual void do_degridding(
                         const Plan& plan,
                         const float w_step, // in lambda
