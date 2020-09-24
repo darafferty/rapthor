39c0a2edb6f6ab282a8a6ce50437fd0fcb275032
Jakob Maljaars <jakob.maljaars@stcorp.nl>
2020-09-24 13:19:20 +0200

    Prepare gpu enabled ci

---
+++ scripts/load_modules.sh
@@ -0,0 +1,11 @@
+module load cmake/3.16.2
+module load openblas/0.3.6-gcc-8.3.0
+module load hdf5/1.10.6-gcc-8.3.0
+module load gcc/8.3.0 
+module load cfitsio/3.430-gcc-6.3.0
+module load fftw/3.3.8-gcc-8.3.0
+module load wcslib/5.18-gcc-6.3.0
+module load lua/5.3.5
+module load boost/1.73-gcc-8.3.0
+module load pybind11/2.4.3
+module load cuda101/toolkit
