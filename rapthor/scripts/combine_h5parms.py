9e0409667a9c1fdea598a0090ab29ffeace57ad4
David Rafferty <drafferty@hs.uni-hamburg.de>
2020-11-02 09:31:02 +0100

    Fix reweighting by calibrator flux

--- rapthor/scripts/combine_h5parms.py
+++ rapthor/scripts/combine_h5parms.py
@@ -175,20 +175,28 @@ def main(h5parm1, h5parm2, outh5parm, mode, solset1='sol000', solset2='sol000',
                 # Ensure window is odd
                 nstddev += 1
             losoto.operations.reweight.run(soltab_amp, mode='window', nmedian=5, nstddev=nstddev)
+        ho.close()
 
         # Use the input calibrator flux densities to adjust the weighting done above
         # to ensure that the average weights are proportional to the square of the
         # calibrator flux densities
-        dir_names = soltab_ph.dir[:]
+        ho = h5parm(outh5parm, readonly=False)
+        sso = ho.getSolset(solset='sol000')
+        soltab_ph = sso.getSoltab('phase000')
+        soltab_amp = sso.getSoltab('amplitude000')
+        dir_names = [d.strip('[]') for d in soltab_ph.dir[:]]
         cal_weights = []
         for dir_name in dir_names:
             cal_weights.append(cal_fluxes[cal_names.index(dir_name)])
+        cal_weights = [float(c) for c in cal_weights]
 
         # Assume that the uncertainty in the solutions scales inversely with the
         # flux density, so the weights scale with the square of the flux densities
         cal_weights = np.array(cal_weights)**2
-        weights_ph = soltab_ph.weight[:]
-        weights_amp = soltab_amp.weight[:]
+        weights_ph = np.array(soltab_ph.weight, dtype=np.float)
+        weights_amp = np.array(soltab_amp.weight, dtype=np.float)
+        weights_ph[weights_ph==0.0] = np.nan
+        weights_amp[weights_amp==0.0] = np.nan
 
         # Reweight, keeping the median value of the weights the same (to avoid
         # changing the overall normalization, which should be the inverse square of the
@@ -198,13 +206,23 @@ def main(h5parm1, h5parm2, outh5parm, mode, solset1='sol000', solset2='sol000',
         for d in range(len(dir_names)):
             # Input data are [time, freq, ant, dir, pol] for slow amplitudes
             # and [time, freq, ant, dir] for fast phases (scalarphase)
-            norm_factor = np.nanmean(weights_ph[:, :, :, d]) * cal_weights[d]
+            norm_factor = cal_weights[d] / np.nanmedian(weights_ph[:, :, :, d])
             weights_ph[:, :, :, d] *= norm_factor
-            norm_factor = np.nanmean(weights_amp[:, :, :, d, :]) * cal_weights[d]
+            norm_factor = cal_weights[d] / np.nanmedian(weights_amp[:, :, :, d, :])
             weights_amp[:, :, :, d, :] *= norm_factor
         weights_ph *= global_median_ph / np.nanmedian(weights_ph)
         weights_amp *= global_median_amp / np.nanmedian(weights_amp)
+        weights_ph[np.isnan(weights_ph)] = 0.0
+        weights_amp[np.isnan(weights_amp)] = 0.0
 
+        # Rescale to fit in float16
+        float16max = 65504.0
+        weights_ph[weights_ph>float16max] = float16max
+        weights_amp[weights_amp>float16max] = float16max
+
+        # Write new weights
+        soltab_ph.setValues(weights_ph, weight=True)
+        soltab_amp.setValues(weights_amp, weight=True)
         ho.close()
 
 
