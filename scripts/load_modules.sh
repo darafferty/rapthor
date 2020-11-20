141983342511c788431a71e6609ffa080c7f327d
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2020-11-20 07:43:54 +0100

    Use new openblas modulefile with correct BLAS_LIB.

--- scripts/load_modules.sh
+++ scripts/load_modules.sh
@@ -9,6 +9,3 @@ module load lua/5.3.5
 module load boost/1.73-gcc-8.3.0
 module load pybind11/2.4.3
 module load cuda101/toolkit
-
-# The openblas module sets BLAS_LIB to /cm/shared/package/openblas/0.3.6-gcc-8.3.0/lib (without the "64"), which does not exist.
-export OPENBLAS_LIB="/cm/shared/package/openblas/0.3.6-gcc-8.3.0/lib64"
