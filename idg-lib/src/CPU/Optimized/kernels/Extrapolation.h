d162da804441bd75c19657db276e84e5d42eabf9
Bram Veenboer <bram.veenboer@gmail.com>
2020-11-25 14:08:18 +0100

    Add compute_extrapolation_avx_fma

--- idg-lib/src/CPU/Optimized/kernels/Extrapolation.h
+++ idg-lib/src/CPU/Optimized/kernels/Extrapolation.h
@@ -65,6 +65,41 @@ void compute_extrapolation_avx(
   *offset += vector_length * ((inner_dim - *offset) / vector_length);
 }
 
+void compute_extrapolation_avx_fma(
+        int *offset,
+  const int outer_dim,
+  const int inner_dim,
+        float *input_real,
+        float *input_imag,
+  const float *delta_real,
+  const float *delta_imag,
+        float *output_real,
+        float *output_imag)
+{
+#if defined(__FMA__)
+  const int vector_length = 8;
+
+  for (int o = 0; o < outer_dim; o++) {
+    for (int i = *offset; i < (inner_dim / vector_length) * vector_length; i += vector_length) {
+      __m256 value_current_r = _mm256_load_ps(&input_real[i]);
+      __m256 value_current_i = _mm256_load_ps(&input_imag[i]);
+      _mm256_storeu_ps(&output_real[o * inner_dim + i], value_current_r);
+      _mm256_storeu_ps(&output_imag[o * inner_dim + i], value_current_i);
+      __m256 delta_r = _mm256_load_ps(&delta_real[i]);
+      __m256 delta_i = _mm256_load_ps(&delta_imag[i]);
+      __m256 value_next_r = _mm256_mul_ps(value_current_r, delta_r);
+      __m256 value_next_i = _mm256_mul_ps(value_current_r, delta_i);;
+      value_next_r = _mm256_fnmadd_ps(value_current_i, delta_i, value_next_r);
+      value_next_i = _mm256_fmadd_ps(value_current_i, delta_r, value_next_i);
+      _mm256_store_ps(&input_real[i], value_next_r);
+      _mm256_store_ps(&input_imag[i], value_next_i);
+    }
+  }
+
+  *offset += vector_length * ((inner_dim - *offset) / vector_length);
+#endif
+}
+
 void compute_extrapolation(
   const int outer_dim,
   const int inner_dim,
@@ -76,6 +111,11 @@ void compute_extrapolation(
         float *output_imag)
 {
   int offset = 0;
+
+  compute_extrapolation_avx_fma(
+    &offset, outer_dim, inner_dim,
+    input_real, input_imag, delta_real, delta_imag,
+    output_real, output_imag);
 
   compute_extrapolation_avx(
     &offset, outer_dim, inner_dim,
