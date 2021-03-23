82ff2a9d5ffc2f0e4bb5dfc58fd2d9dff8f2d375
Bram Veenboer <bram.veenboer@gmail.com>
2021-03-23 11:37:26 +0100

    Make Plan constructors public again
    This is needed for a W-Tiling test program, that interacts with Plan and
    InstanceCUDA directly.

--- idg-lib/src/common/Plan.h
+++ idg-lib/src/common/Plan.h
@@ -61,7 +61,6 @@ class Plan {
 
   Plan(Plan&&) = default;
 
- private:
   Plan(const int kernel_size, const int subgrid_size, const int grid_size,
        const float cell_size, const Array1D<float>& shift,
        const Array1D<float>& frequencies, const Array2D<UVW<float>>& uvw,
@@ -76,13 +75,6 @@ class Plan {
        const Array1D<unsigned int>& aterms_offsets, WTiles& wtiles,
        Options options = Options());
 
-  // The constructors are private
-  // only these classes can instantiate a Plan
-  friend proxy::Proxy;
-  friend proxy::cpu::CPU;
-  friend proxy::hybrid::UnifiedOptimized;
-
- public:
   // Destructor
   virtual ~Plan() = default;
 
