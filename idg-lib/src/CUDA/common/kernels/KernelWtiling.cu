447a091d0bcdefc513974d99db2ae137e3e53f06
Bram Veenboer <bram.veenboer@gmail.com>
2021-03-26 13:48:40 +0100

    CUDA W-Tiling: implement kernel_subgrids_from_wtiles

--- idg-lib/src/CUDA/common/kernels/KernelWtiling.cu
+++ idg-lib/src/CUDA/common/kernels/KernelWtiling.cu
@@ -269,7 +269,57 @@ __global__ void kernel_subgrids_from_wtiles(
     const float2*   __restrict__ tiles,
           float2                 scale)
 {
+    // Map blockIdx.x to subgrids
+    int s = blockIdx.x + subgrid_offset;
 
+    // Map thread indices to thread id
+    int tidx = threadIdx.x;
+    int tidy = threadIdx.y;
+    int tid = tidx + tidy * blockDim.x;
+
+    // Compute the number of threads working on one subgrid
+    int nr_threads = blockDim.x * blockDim.y;
+
+    // Load tile coordinates
+    const Metadata &m = metadata[s];
+    int tile_index = m.wtile_index;
+    int tile_top = m.wtile_coordinate.x * tile_size -
+                    subgrid_size / 2 + grid_size / 2;
+    int tile_left = m.wtile_coordinate.y * tile_size -
+                    subgrid_size / 2 + grid_size / 2;
+
+    // Compute position in tile
+    int subgrid_x = m.coordinate.x - tile_top;
+    int subgrid_y = m.coordinate.y - tile_left;
+
+    // Iterate all pixels in subgrid
+    for (int i = tid; i < subgrid_size * subgrid_size; i += nr_threads) {
+        int y = i / subgrid_size;
+        int x = i % subgrid_size;
+        float pi = (float) M_PI;
+        float phase = pi * (x+y-subgrid_size)/subgrid_size;
+        float2 phasor = make_float2(cosf(phase) * scale.x,
+                                    sinf(phase) * scale.y);
+
+        if (y < subgrid_size)
+        {
+            // Compute shifted position in subgrid
+            int x_src = (x + (subgrid_size/2)) % subgrid_size;
+            int y_src = (y + (subgrid_size/2)) % subgrid_size;
+
+            // Compute position in grid
+            int x_dst = subgrid_x + x;
+            int y_dst = subgrid_y + y;
+
+            // Set subgrid value from grid
+            #pragma unroll 4
+            for (int pol = 0; pol < NR_POLARIZATIONS; pol++) {
+                long src_idx = index_grid(tile_size + subgrid_size, tile_index, pol, y_dst, x_dst);
+                long dst_idx = index_subgrid(subgrid_size, s, pol, y_src, x_src);
+                subgrid[dst_idx] = tiles[src_idx] * phasor;
+            }
+        }
+    }
 }
 
 __global__ void kernel_wtiles_from_grid(
