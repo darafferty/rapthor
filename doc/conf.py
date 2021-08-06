dd62fd8d862de7198b845066cfbd5b31a38594dd
Tammo Jan Dijkema <dijkema@astron.nl>
2021-08-06 10:21:13 +0000

    Build doc in CI and deploy

--- doc/conf.py
+++ doc/conf.py
@@ -47,9 +47,8 @@ html_theme = 'sphinx_rtd_theme'
 
 html_logo = 'idg-logo.svg'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_css_files = ['extra.css']
+
 html_static_path = ['_static']
 
 # Breathe Configuration
