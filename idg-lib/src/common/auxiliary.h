03b96abde6a99ef2348bea1a23c08773579a6585
Bram Veenboer <bram.veenboer@gmail.com>
2021-09-23 11:12:43 +0200

    Remove default argument from auxiliary::sizeof_grid

--- idg-lib/src/common/auxiliary.h
+++ idg-lib/src/common/auxiliary.h
@@ -79,7 +79,7 @@ uint64_t sizeof_subgrids(unsigned int nr_subgrids, unsigned int subgrid_size,
 
 uint64_t sizeof_metadata(unsigned int nr_subgrids);
 
-uint64_t sizeof_grid(unsigned int grid_size, uint64_t nr_correlations = 4);
+uint64_t sizeof_grid(unsigned int grid_size, uint64_t nr_correlations);
 
 uint64_t sizeof_wavenumbers(unsigned int nr_channels);
 
