33f5854159d7b73d54745eba95afe4e11c9a5f75
Bram Veenboer <bram.veenboer@gmail.com>
2022-04-13 17:01:52 +0200

    Define M_PI in CUDA when it is not defined yet

--- idg-lib/src/CUDA/common/kernels/math.cu
+++ idg-lib/src/CUDA/common/kernels/math.cu
@@ -1,6 +1,10 @@
 // Copyright (C) 2020 ASTRON (Netherlands Institute for Radio Astronomy)
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 #include <cuComplex.h>
 inline __device__ float2 conj(float2 a) {
     return cuConjf(a);
