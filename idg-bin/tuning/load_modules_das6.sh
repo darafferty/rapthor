4dbce99a62ce57bef2a525c9e5c2205379e51063
Bram Veenboer <veenboer@astron.nl>
2022-05-03 06:33:06 +0000

    Update modules for DAS-6

--- idg-bin/tuning/load_modules_das6.sh
+++ idg-bin/tuning/load_modules_das6.sh
@@ -4,7 +4,7 @@ module load spack/9.4.0
 module load python/3.9.9
 module load py-pycuda/2021.1
 module load py-pynvml/8.0.4
-module load py-pip/21.1.2
-module load py-scipy/1.5.4
-module load py-numpy/1.22.0
-module load cuda/11.5.1
+module load py-pip/21.3.1
+module load py-scipy/1.8.0
+module load py-numpy/1.22.3
+module load cuda/11.6.2
