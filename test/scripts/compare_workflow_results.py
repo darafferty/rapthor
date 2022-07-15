41c535bf800c73fa2d69ee403f57d60e0a7c7b8a
Marcel Loose <loose@astron.nl>
2022-07-15 17:55:31 +0200

    WIP: Update documentation

--- test/scripts/compare_workflow_results.py
+++ test/scripts/compare_workflow_results.py
@@ -28,17 +28,10 @@ logger = None
 
 def check_all_files_present(dcmp):
     """
-    Checks that all files are present in both compared directories
+    Checks that all files are present in both compared directories.
 
-    Parameters
-    ----------
-    dcmp : dircmp object
-        The result of dircmp(left, right)
-
-    Returns
-    -------
-    agree : bool
-        True if all files are present, False if not
+    :param dircmp dcmp: the result of `dircmp(left, right)`
+    :return bool: True if all files are present, False if not
     """
     logger.debug("Comparing directory contents of '%s' and '%s'", dcmp.left, dcmp.right)
     agree = True
@@ -54,14 +47,14 @@ def check_all_files_present(dcmp):
 
 def fits_files_are_similar(file1, file2, rtol, verbosity):
     """
-    Compare FITS files `file1` and `file2`, using `FITSDiff` in `astropy`.
+    Compare FITS files ``file1`` and ``file2``, using `FITSDiff` in `astropy`.
     FITS files are considered similar if none of the values have a relative
     difference exceeding `rtol`.
-    :param file1: First FITS file
-    :param file2: Second FITS file
-    :param rtol: Relative tolerance threshold
-    :param verbosity: Verbosity level, higher is more verbose
-    :return: True if files are similar, else False
+    :param str file1: First FITS file
+    :param str file2: Second FITS file
+    :param float rtol: Relative tolerance threshold
+    :param int verbosity: Verbosity level, higher is more verbose
+    :return bool: True if FITS files are similar, else False
     """
     logger.debug("Comparing '%s' and '%s'", file1, file2)
     diff = astropy.io.fits.FITSDiff(file1, file2, rtol=rtol)
@@ -74,18 +67,19 @@ def fits_files_are_similar(file1, file2, rtol, verbosity):
                          file1, file2, rtol, diff.report())
         else:
             logger.error("FITS files '%s' and '%s' differ (rtol=%s)", file1, file2, rtol)
+    return agree
 
 
 def h5_files_are_similar(file1, file2, rtol, verbosity):
     """
-    Check if HDF-5 `file1` and `file2` are similar. Files are considered
-    similar if none of the values have a relative difference exceeding `rtol`.
+    Check if HDF5 ``file1`` and ``file2`` are similar. Files are considered
+    similar if none of the values have a relative difference exceeding ``rtol``.
     This method uses the `h5diff` command that is part of the HDF5 Tools.
-    :param file1: First HDF-5 file
-    :param file2: Second HDF-5 file
-    :param rtol: Relative tolerance threshold
-    :param verbosity: Verbosity level, higher is more verbose
-    :return: True if files are similar, else False
+    :param str file1: First HDF-5 file
+    :param str file2: Second HDF-5 file
+    :param float rtol: Relative tolerance threshold
+    :param int verbosity: Verbosity level, higher is more verbose
+    :return bool: True if HDF5 files are similar, else False
     """
     logger.debug("Comparing '%s' and '%s'", file1, file2)
     command = ["h5diff"]
@@ -122,20 +116,23 @@ def image_files_are_similar(file1, file2, rtol, verbosity):
     """
     Compare two FITS image files, by comparing the RMS noise values.
     Files are considered similar if the the noise ratio differs less than
-    `rtol` from 1.
-    :param file1: Filename of first FITS image
-    :param file2: Filename of second FITS image
-    :param rtol: Relative tolerance threshold
-    :param verbosity: Verbosity level, higher is more verbose
-    :return: True if images are similar, else False
+    ``rtol`` from 1.
+    :param str file1: Filename of first FITS image
+    :param str file2: Filename of second FITS image
+    :param float rtol: Relative tolerance threshold
+    :param int verbosity: Verbosity level, higher is more verbose
+    :return bool: True if images files are similar, else False
     """
     def image_noise(fname):
         """
-        Calculate the RMS value of the noise in the image file `fname`
+        Calculate the RMS value of the noise in the image file ``fname``.
+        :param str fname: Filename of the FITS image
+        :return float: RMS noise in the image in Jy/b
         """
         image = rapthor.lib.image.FITSImage(fname)
         image.calc_noise()
         return image.noise
+
     logger.debug("Comparing noise in images '%s' and '%s'", file1, file2)
     noise1 = image_noise(file1)
     noise2 = image_noise(file2)
@@ -156,21 +153,21 @@ def skymodel_files_are_similar(file1, file2, atol, rtol, verbosity):
     Compare two skymodel files. Skymodels are considered similar if their
     statistics are similar. This function uses LSMTool to determine the skymodel
     statistics. The skymodels are considered to be similar, if the relevant
-    statistics differ less than `atol` or `rtol`.
-    :param file1: Filename of the first skymodel
-    :param file2: Filename of the second skymodel
-    :param atol: Absolute tolerance threshold
-    :param rtol: Relative tolerance threshold
-    :param verbosity: Verbosity level, higher is more verbose
-    :return: True is skymodels are similar, else False
+    statistics differ less than ``atol`` or ``rtol``.
+    :param str file1: Filename of the first skymodel
+    :param str file2: Filename of the second skymodel
+    :param float atol: Absolute tolerance threshold
+    :param float rtol: Relative tolerance threshold
+    :param int verbosity: Verbosity level, higher is more verbose
+    :return bool: True is skymodels file are similar, else False
     """
     def compare_stats(stats, atol, rtol):
         """
         Compare statistics generated by LSMTool. Check if the flux ratio is
-        close to 1, and that the position offsets are less than `atol`
-        :param atol: Absolute tolerance used to compare position offsets
-        :param rtol: Relative tolerance used to compare flux ratio
-        :return: True if statistics are similar, else False
+        close to 1, and that the position offsets are less than ``atol``
+        :param float atol: Absolute tolerance used to compare position offsets
+        :param float rtol: Relative tolerance used to compare flux ratio
+        :return bool: True if statistics are similar, else False
         """
         if not np.isclose(stats['meanRatio'], 1, rtol=rtol):
             return False
@@ -179,6 +176,7 @@ def skymodel_files_are_similar(file1, file2, atol, rtol, verbosity):
         if not np.isclose(stats['meanDecOffsetDeg'], 0, atol=atol):
             return False
         return True
+
     logger.debug("Comparing skymodels '%s' and '%s'", file1, file2)
     sm1 = lsmtool.load(file1)
     sm2 = lsmtool.load(file2)
@@ -205,10 +203,10 @@ def skymodel_files_are_similar(file1, file2, atol, rtol, verbosity):
 def compare_results(dcmp, atol, rtol, verbosity):
     """
     Compare the results between the files produced by each CWL workflow.
-    :param dcmp: a `dircmp` object with the directory contents of the two workflows
-    :param atol: absolute tolerance threshold
-    :param rtol: relative tolerance threshold
-    :param verbosity: verbosity level, higher means more verbose
+    :param dircmp dcmp: the directory contents of the two workflows
+    :param float atol: absolute tolerance threshold
+    :param float rtol: relative tolerance threshold
+    :param int verbosity: verbosity level, higher means more verbose
     """
     logger.info("*** Comparing results in '%s' and '%s' ***", dcmp.left, dcmp.right)
     agree = True
@@ -239,7 +237,7 @@ def compare_results(dcmp, atol, rtol, verbosity):
 def parse_arguments():
     """
     Parse command-line arguments.
-    :return: argparse.Namespace
+    :return argparse.Namespace: parsed command-line arguments
     """
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -287,6 +285,7 @@ def init_logger(verbosity):
     """
     Initialize a global logger object.
     Set the loglevel depending on the `verbosity`.
+    :param int verbosity: Verbosity level, higher means more verbose logging
     """
     global logger
     logger = logging.getLogger()
@@ -303,11 +302,11 @@ def init_logger(verbosity):
 
 def main(args):
     """
-    Compare the images, skymodels and solutions in the `args.first` directory to
-    those in the `args.second` directory.
-    :param args: Output of command-line parsing using `argparse`
-    :return: True if all files in `args.first` are similar to those in `args.second`,
-    else False.
+    Compare the images, skymodels and solutions in the ``args.first`` directory
+    to those in the ``args.second`` directory.
+    :param argparse.Namespace args: parsed command-line
+    :return bool: True if all files in ``args.first`` are similar to those in
+                  ``args.second``, else False.
     """
     # Set level of verbosity to -1 if command-line option `-q` is given, else
     # to the number of times the command-line option `-v` is given.
