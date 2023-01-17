c855420d029faf0c4c035a0d0fd9553cc8161aac
Marcel Loose <loose@astron.nl>
2023-01-17 17:11:32 +0100

    Fix paths in test file fails.py

--- debug/everybeam/fails.py
+++ debug/everybeam/fails.py
@@ -1,5 +1,5 @@
 import lsmtool
 s = lsmtool.load(
-    '/rapthor/Share/rapthor/run_480ch_5chunks_flag/skymodels/calibrate_1/calibration_skymodel.txt', 
-    beamMS='/rapthor/Share/rapthor/L632477_480ch_50min/allbands.ms.mjd5020562823_field.ms')
+    '/project/rapthor/Share/rapthor/run_480ch_5chunks_flag/skymodels/calibrate_1/calibration_skymodel_small.txt',
+    beamMS='/project/rapthor/Share/rapthor/L632477_480ch_50min/allbands.ms.mjd5020562823_field.ms')
 s.getColValues('I', applyBeam=True)   
