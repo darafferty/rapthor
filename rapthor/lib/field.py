a052a4c6cee7264128e661dba7a72042dee84ce4
Frits Sweijen <tikk3r@users.noreply.github.com>
2023-04-13 11:54:42 +0200

    Replace sys.exit with raise exception in operation.py

--- rapthor/lib/field.py
+++ rapthor/lib/field.py
@@ -533,13 +533,6 @@ class Field(object):
                 # If there was insufficient flux in the sky model to meet the
                 # target flux, LSMTool will put all sources into a single patch
                 # with the name 'Patch'. So, check for this case and exit if found
-<<<<<<< HEAD
-=======
-                self.log.critical('No groups found that meet the target flux density. Please '
-                                  'check the sky model (in working_dir/skymodels/calibrate_{}/) '
-                                  'for problems or lower the target flux density. '
-                                  'Exiting...'.format(index))
->>>>>>> b65b972 (Raise exceptions in field.py instead of using sys.exit)
                 raise RuntimeError('No groups found that meet the target flux density. Please '
                     'check the sky model (in working_dir/skymodels/calibrate_{}/) '
                     'for problems or lower the target flux density.'.format(index))
