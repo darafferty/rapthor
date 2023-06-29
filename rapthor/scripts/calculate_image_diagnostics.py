e85246d2c742346ffebcbfc83ce17fe69a777f2e
David Rafferty <drafferty@hs.uni-hamburg.de>
2023-06-29 16:16:36 +0200

    Add check for download problems

--- rapthor/scripts/calculate_image_diagnostics.py
+++ rapthor/scripts/calculate_image_diagnostics.py
@@ -109,97 +109,102 @@ def main(flat_noise_image, flat_noise_rms_image, true_sky_image, true_sky_rms_im
         # Download a TGSS sky model around the midpoint of the input sky model,
         # using a 5-deg radius to ensure the field is fully covered
         _, _, midRA, midDec = s_in._getXY()
-        s_comp = lsmtool.load('tgss', VOPosition=[midRA, midDec], VORadius=5.0)
+        try:
+            s_comp = lsmtool.load('tgss', VOPosition=[midRA, midDec], VORadius=5.0)
+        except IORegistryError:
+            # Comparison catalog not downloaded successfully
+            s_comp = None
     else:
         s_comp = lsmtool.load(comparison_skymodel)
 
-    # Do cross matching of the comparison model with the PyBDSF-derived one.
-    # To do this, we need to write the comparison catalog to FITS file for
-    # use with astropy
-    catalog_comp_filename = output_root+'.comparison.fits'
-    s_comp.table.columns['Ra'].format = None
-    s_comp.table.columns['Dec'].format = None
-    s_comp.table.columns['I'].format = None
-    s_comp.write(catalog_comp_filename, format='fits', clobber=True)
-    if 'ReferenceFrequency' in s_comp.getColNames():
-        freq_comp = np.mean(s_comp.getColValues('ReferenceFrequency'))
-    else:
-        freq_comp = s_comp.table.meta['ReferenceFrequency']
+    # Do cross matching of the comparison model with the PyBDSF-derived one
+    if s_comp and s_in:
+        # Write the comparison catalog to FITS file for use with astropy
+        catalog_comp_filename = output_root+'.comparison.fits'
+        s_comp.table.columns['Ra'].format = None
+        s_comp.table.columns['Dec'].format = None
+        s_comp.table.columns['I'].format = None
+        s_comp.write(catalog_comp_filename, format='fits', clobber=True)
+        if 'ReferenceFrequency' in s_comp.getColNames():
+            freq_comp = np.mean(s_comp.getColValues('ReferenceFrequency'))
+        else:
+            freq_comp = s_comp.table.meta['ReferenceFrequency']
 
-    # Read in PyBDSF-derived and comparison catalogs and filter to keep
-    # only comparison sources within a radius of FWHM / 2 of phase center
-    catalog = Table.read(input_catalog, format='fits')
-    catalog_comp = Table.read(catalog_comp_filename, format='fits')
-    obs = Observation(obs_ms[beam_ind])
-    phase_center = SkyCoord(ra=obs.ra*u.degree, dec=obs.dec*u.degree)
-    coords_comp = SkyCoord(ra=catalog_comp['Ra'], dec=catalog_comp['Dec'])
-    separation = phase_center.separation(coords_comp)
-    sec_el = 1.0 / np.sin(obs.mean_el_rad)
-    fwhm_deg = 1.1 * ((3.0e8 / img_true_sky.freq) / obs.diam) * 180 / np.pi * sec_el
-    catalog_comp = catalog_comp[separation < fwhm_deg/2*u.degree]
-
-    # Cross match the coordinates and keep only matches that have a
-    # separation of 5 arcsec or less
-    #
-    # Note: we require at least 10 sources for the comparison, as using
-    # fewer can give unreliable estimates
-    min_number = 10
-    if len(catalog_comp) >= min_number:
-        coords = SkyCoord(ra=catalog['RA'], dec=catalog['DEC'])
+        # Read in PyBDSF-derived and comparison catalogs and filter to keep
+        # only comparison sources within a radius of FWHM / 2 of phase center
+        catalog = Table.read(input_catalog, format='fits')
+        catalog_comp = Table.read(catalog_comp_filename, format='fits')
+        obs = Observation(obs_ms[beam_ind])
+        phase_center = SkyCoord(ra=obs.ra*u.degree, dec=obs.dec*u.degree)
         coords_comp = SkyCoord(ra=catalog_comp['Ra'], dec=catalog_comp['Dec'])
-        idx, sep2d, _ = match_coordinates_sky(coords_comp, coords)
-        constraint = sep2d < 5*u.arcsec
-        catalog_comp = catalog_comp[constraint]
-        catalog = catalog[idx[constraint]]
+        separation = phase_center.separation(coords_comp)
+        sec_el = 1.0 / np.sin(obs.mean_el_rad)
+        fwhm_deg = 1.1 * ((3.0e8 / img_true_sky.freq) / obs.diam) * 180 / np.pi * sec_el
+        catalog_comp = catalog_comp[separation < fwhm_deg/2*u.degree]
 
-        # Find the mean flux ratio (input / comparison). We use the total island
-        # flux density from PyBDSF as it gives less scatter than the Gaussian
-        # fluxes when comparing to a lower-resolution catalog (such as TGSS).
-        # Lastly, a correction factor is used to correct for the potentially
-        # different frequencies of the LOFAR image and the comparison catalog
+        # Cross match the coordinates and keep only matches that have a
+        # separation of 5 arcsec or less
+        #
+        # Note: we require at least 10 sources for the comparison, as using
+        # fewer can give unreliable estimates
+        min_number = 10
         if len(catalog_comp) >= min_number:
-            alpha = -0.7  # adopt a typical spectral index
-            freq_factor = (freq_comp / img_true_sky.freq)**alpha
-            ratio = catalog['Isl_Total_flux'] / catalog_comp['I'] * freq_factor
-            meanRatio = np.mean(ratio)
-            stdRatio = np.std(ratio)
-            clippedRatio = sigma_clip(ratio)
-            meanClippedRatio = np.mean(clippedRatio)
-            stdClippedRatio = np.std(clippedRatio)
-            stats = {'meanRatio_pybdsf': meanRatio,
-                     'stdRatio_pybdsf': stdRatio,
-                     'meanClippedRatio_pybdsf': meanClippedRatio,
-                     'stdClippedRatio_pybdsf': stdClippedRatio}
-            cwl_output.update(stats)
+            coords = SkyCoord(ra=catalog['RA'], dec=catalog['DEC'])
+            coords_comp = SkyCoord(ra=catalog_comp['Ra'], dec=catalog_comp['Dec'])
+            idx, sep2d, _ = match_coordinates_sky(coords_comp, coords)
+            constraint = sep2d < 5*u.arcsec
+            catalog_comp = catalog_comp[constraint]
+            catalog = catalog[idx[constraint]]
 
-    # Group the comparison sky model into sources using a FWHM of 40 arcsec,
-    # the approximate TGSS resolution
-    s_comp.group('threshold', FWHM='40.0 arcsec', threshold=0.05)
+            # Find the mean flux ratio (input / comparison). We use the total island
+            # flux density from PyBDSF as it gives less scatter than the Gaussian
+            # fluxes when comparing to a lower-resolution catalog (such as TGSS).
+            # Lastly, a correction factor is used to correct for the potentially
+            # different frequencies of the LOFAR image and the comparison catalog
+            if len(catalog_comp) >= min_number:
+                alpha = -0.7  # adopt a typical spectral index
+                freq_factor = (freq_comp / img_true_sky.freq)**alpha
+                ratio = catalog['Isl_Total_flux'] / catalog_comp['I'] * freq_factor
+                meanRatio = np.mean(ratio)
+                stdRatio = np.std(ratio)
+                clippedRatio = sigma_clip(ratio)
+                meanClippedRatio = np.mean(clippedRatio)
+                stdClippedRatio = np.std(clippedRatio)
+                stats = {'meanRatio_pybdsf': meanRatio,
+                         'stdRatio_pybdsf': stdRatio,
+                         'meanClippedRatio_pybdsf': meanClippedRatio,
+                         'stdClippedRatio_pybdsf': stdClippedRatio}
+                cwl_output.update(stats)
 
-    # Keep POINT-only sources
-    source_type = s_comp.getColValues('Type')
-    patch_names = s_comp.getColValues('Patch')
-    non_point_patch_names = set(patch_names[np.where(source_type != 'POINT')])
-    ind = []
-    for patch_name in non_point_patch_names:
-        ind.extend(s_comp.getRowIndex(patch_name))
-    s_comp.remove(np.array(ind))
+        # Group the comparison sky model into sources using a FWHM of 40 arcsec,
+        # the approximate TGSS resolution
+        s_comp.group('threshold', FWHM='40.0 arcsec', threshold=0.05)
 
-    # Check if there is a sufficient number of sources to do the comparison with.
-    # If there is, do it and append the resulting diagnostics dict to the
-    # existing one
-    #
-    # Note: the various ratios are all calculated as (s_in / s_comp) and the
-    # differences as (s_in - s_comp). If there are no successful matches,
-    # the compare() method returns None
-    if (s_comp
-            and len(s_in.getPatchNames()) >= min_number
-            and len(s_comp.getPatchNames()) >= min_number):
-        flux_astrometry_diagnostics = s_in.compare(s_comp, radius='5 arcsec',
-                                                   excludeMultiple=True, make_plots=False)
-        if flux_astrometry_diagnostics is not None:
-            cwl_output.update(flux_astrometry_diagnostics)
+        # Keep POINT-only sources
+        source_type = s_comp.getColValues('Type')
+        patch_names = s_comp.getColValues('Patch')
+        non_point_patch_names = set(patch_names[np.where(source_type != 'POINT')])
+        ind = []
+        for patch_name in non_point_patch_names:
+            ind.extend(s_comp.getRowIndex(patch_name))
+        s_comp.remove(np.array(ind))
 
+        # Check if there is a sufficient number of sources to do the comparison with.
+        # If there is, do it and append the resulting diagnostics dict to the
+        # existing one
+        #
+        # Note: the various ratios are all calculated as (s_in / s_comp) and the
+        # differences as (s_in - s_comp). If there are no successful matches,
+        # the compare() method returns None
+        if (s_comp
+                and len(s_in.getPatchNames()) >= min_number
+                and len(s_comp.getPatchNames()) >= min_number):
+            flux_astrometry_diagnostics = s_in.compare(s_comp, radius='5 arcsec',
+                                                       excludeMultiple=True, make_plots=False)
+            if flux_astrometry_diagnostics is not None:
+                cwl_output.update(flux_astrometry_diagnostics)
+
+    # Write out the diagnostics
     with open(output_root+'.image_diagnostics.json', 'w') as fp:
         json.dump(cwl_output, fp)
 
