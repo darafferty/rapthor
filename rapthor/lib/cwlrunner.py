74e0ba7d33fc7206520027d81d3c4967f7723263
David Rafferty <drafferty@hs.uni-hamburg.de>
2024-01-25 14:48:50 +0100

    Handle Toil args with altered parsing

--- rapthor/lib/cwlrunner.py
+++ rapthor/lib/cwlrunner.py
@@ -7,6 +7,7 @@ import os
 import shutil
 import subprocess
 from typing import TYPE_CHECKING
+from importlib.metadata import version
 
 if TYPE_CHECKING:
     from rapthor.lib.operation import Operation
@@ -174,8 +175,13 @@ class ToilRunner(CWLRunner):
         if os.path.exists(self.operation.jobstore):
             self.args.extend(['--restart'])
         self.args.extend(['--writeLogs', self.operation.log_dir])
-        self.args.extend(['--writeLogsFromAllJobs'])  # also keep logs of successful jobs
-        self.args.extend(['--maxLogFileSize', '0'])  # disable truncation of log files
+        if int(version('toil').split('.')[0]) >= 6:
+            # With Toil v6.0.0, the way in which several args are parsed was changed
+            self.args.extend(['--writeLogsFromAllJobs', 'True'])  # also keep logs of successful jobs
+            self.args.extend(['--maxLogFileSize', '1gb'])  # set to large value to prevent truncation
+        else:
+            self.args.extend(['--writeLogsFromAllJobs'])  # also keep logs of successful jobs
+            self.args.extend(['--maxLogFileSize', '0'])  # disable truncation of log files
         if self.operation.scratch_dir is not None:
             # Note: option --workDir seems to take precedence over both --tmpdir-prefix,
             #       and --tmp-outdir-prefix. So, we may not want to set it.
