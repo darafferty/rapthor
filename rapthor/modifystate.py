c0ef16a0720fcba5024a37ca3550da15a05bd1eb
David Rafferty <drafferty@hs.uni-hamburg.de>
2024-04-16 13:43:06 +0200

    Add new op to op list

--- rapthor/modifystate.py
+++ rapthor/modifystate.py
@@ -40,7 +40,7 @@ def run(parset_file):
 
     # Check each operation for started pipelines (workflows)
     # Note: the order here should match the order in which the operations were run
-    operation_list = ['concatenate', 'calibrate', 'predict_di', 'calibrate_di',
+    operation_list = ['concatenate', 'predict_nc', 'calibrate', 'predict_di', 'calibrate_di',
                       'predict', 'image', 'mosaic']  # in order of execution
     while True:
         pipelines = []
