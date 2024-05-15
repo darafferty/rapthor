62f3993401d4ceb35d2f6e57af4615b0b8c8d17d
Marcel Loose <loose@astron.nl>
2024-05-15 17:38:45 +0200

    Add script to convert parset to JSON
    Added a script that can convert a Rapthor parset file to a JSON file.
    Removed two now obsoleted scripts.

--- examples/full_parset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -eu
-
-# Grep for keys, optional (commented) keys, and sections;
-# uncomment and convert ini to json
-egrep '^((#[[:blank:]]*)?[[:alnum:]_]+ =|\[[^]]*\])' rapthor.parset \
-  | sed 's,^# *,,' \
-  > rapthor_full.parset
