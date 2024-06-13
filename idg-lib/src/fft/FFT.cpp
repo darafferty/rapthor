7cf96005a4c5baa4ba170670d57285fe7fc18715
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2024-06-13 13:10:41 +0000

    AST-1553 Add Ubuntu 24 build

--- idg-lib/src/fft/FFT.cpp
+++ idg-lib/src/fft/FFT.cpp
@@ -3,9 +3,9 @@
 
 #include "FFT.h"
 
-#include <fftw3.h>
+#include <cstdint>
 
-using namespace std;
+#include <fftw3.h>
 
 namespace idg {
 
@@ -103,7 +103,7 @@ void kernel_fft(unsigned batch, int height, int width,
   }
 }
 
-void fft2f(unsigned batch, int m, int n, complex<float>* data) {
+void fft2f(unsigned batch, int m, int n, std::complex<float>* data) {
   ifftshift(batch, m, n, data);
   kernel_fft(batch, m, n, data, FFTW_FORWARD);
   fftshift(batch, m, n, data);
@@ -113,7 +113,7 @@ void fft2f(int m, int n, std::complex<float>* data) { fft2f(1, m, n, data); }
 
 void fft2f(int n, std::complex<float>* data) { fft2f(n, n, data); }
 
-void ifft2f(unsigned batch, int m, int n, complex<float>* data) {
+void ifft2f(unsigned batch, int m, int n, std::complex<float>* data) {
   ifftshift(batch, m, n, data);
   kernel_fft(batch, m, n, data, FFTW_BACKWARD);
   fftshift(batch, m, n, data);
@@ -123,7 +123,7 @@ void ifft2f(int m, int n, std::complex<float>* data) { ifft2f(1, n, n, data); }
 
 void ifft2f(int n, std::complex<float>* data) { ifft2f(n, n, data); }
 
-void fft2f_r2c(int m, int n, float* data_in, complex<float>* data_out) {
+void fft2f_r2c(int m, int n, float* data_in, std::complex<float>* data_out) {
   fftwf_complex* tmp = (fftwf_complex*)data_out;
   fftwf_plan plan;
 
@@ -135,11 +135,11 @@ void fft2f_r2c(int m, int n, float* data_in, complex<float>* data_out) {
   fftwf_destroy_plan(plan);
 }
 
-void fft2f_r2c(int n, float* data_in, complex<float>* data_out) {
+void fft2f_r2c(int n, float* data_in, std::complex<float>* data_out) {
   fft2f_r2c(n, n, data_in, data_out);
 }
 
-void ifft2f_c2r(int m, int n, complex<float>* data_in, float* data_out) {
+void ifft2f_c2r(int m, int n, std::complex<float>* data_in, float* data_out) {
   fftwf_complex* tmp = (fftwf_complex*)data_in;
   fftwf_plan plan;
 
@@ -151,12 +151,12 @@ void ifft2f_c2r(int m, int n, complex<float>* data_in, float* data_out) {
   fftwf_destroy_plan(plan);
 }
 
-void ifft2f_c2r(int n, complex<float>* data_in, float* data_out) {
+void ifft2f_c2r(int n, std::complex<float>* data_in, float* data_out) {
   ifft2f_c2r(n, n, data_in, data_out);
 }
 
-void resize2f(int m_in, int n_in, complex<float>* data_in, int m_out, int n_out,
-              complex<float>* data_out) {
+void resize2f(int m_in, int n_in, std::complex<float>* data_in, int m_out,
+              int n_out, std::complex<float>* data_out) {
   // scale before FFT
   float s = 1.0f / (m_in * n_in);
   for (int i = 0; i < m_in; i++) {
