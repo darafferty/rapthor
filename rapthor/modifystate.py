3ca3f7b7518ae703e083c5cb26f8e69deee52958
David Rafferty <drafferty@hs.uni-hamburg.de>
2024-07-01 14:28:31 +0200

    Add new operation

--- rapthor/modifystate.py
+++ rapthor/modifystate.py
@@ -40,13 +40,16 @@ def run(parset_file):
 
     # Check each operation for started pipelines (workflows)
     # Note: the order here should match the order in which the operations were run
-    operation_list = ['concatenate', 'predict_nc', 'calibrate', 'predict_di', 'calibrate_di',
-                      'predict', 'image', 'mosaic']  # in order of execution
+    operation_list = ['initial_image', 'concatenate', 'predict_nc', 'calibrate',
+                      'predict_di', 'calibrate_di', 'predict', 'image', 'mosaic']
     while True:
         pipelines = []
         for index, step in enumerate(strategy_steps):
             for opname in operation_list:
-                operation = os.path.join(parset['dir_working'], 'pipelines', '{0}_{1}'.format(opname, index+1))
+                if opname == 'initial_image':
+                    operation = os.path.join(parset['dir_working'], 'pipelines', '{0}'.format(opname))
+                else:
+                    operation = os.path.join(parset['dir_working'], 'pipelines', '{0}_{1}'.format(opname, index+1))
                 if os.path.exists(operation):
                     pipelines.append(os.path.basename(operation))
 
