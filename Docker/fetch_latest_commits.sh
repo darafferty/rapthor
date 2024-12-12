86fc7a128f46ebe228a52f44083f0355fb7d3746
Marcel Loose <loose@astron.nl>
2024-12-12 10:35:08 +0100

    Use latest EveryBeam release (v0.6.2), just prior to the one requiring C++-20

--- Docker/fetch_latest_commits.sh
+++ Docker/fetch_latest_commits.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 git ls-remote https://git.astron.nl/RD/idg.git HEAD            | awk '{ print "IDG_COMMIT="$1 }'
-git ls-remote https://git.astron.nl/RD/EveryBeam.git v0.6.1    | awk '{ print "EVERYBEAM_COMMIT="$1 }'
+git ls-remote https://git.astron.nl/RD/EveryBeam.git v0.6.2    | awk '{ print "EVERYBEAM_COMMIT="$1 }'
 git ls-remote https://github.com/aroffringa/dysco.git HEAD     | awk '{ print "DYSCO_COMMIT="$1 }'
 git ls-remote https://gitlab.com/aroffringa/aoflagger.git HEAD | awk '{ print "AOFLAGGER_COMMIT="$1 }'
 git ls-remote https://gitlab.com/aroffringa/wsclean.git HEAD   | awk '{ print "WSCLEAN_COMMIT="$1 }'
