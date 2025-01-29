dfb9b9d6302a2d4049d09f9bc0d9d1319484f85a
David Rafferty <drafferty@hs.uni-hamburg.de>
2025-01-29 10:03:42 +0100

    Address review comments

--- rapthor/process.py
+++ rapthor/process.py
@@ -111,8 +111,8 @@ def run(parset_file, logging_level='info'):
             log.info("Screens will be used for calibration and imaging")
             if final_step['peel_outliers']:
                 # Currently, when screens are used peeling cannot be done
-                log.info("Peeling of outliers is currently not supported when using "
-                         "screens. Peeling will be skipped")
+                log.warning("Peeling of outliers is currently not supported when using "
+                            "screens. Peeling will be skipped")
                 final_step['peel_outliers'] = False
 
         # Set the data chunking to match the longest solution interval set in
