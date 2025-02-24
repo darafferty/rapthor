7b7709b4f0e075152f646a64071e68bd52917bb6
Marcel Loose <loose@astron.nl>
2025-02-24 13:11:35 +0100

    Build image with DP3 metadata-compression and gdb

--- Docker/fetch_commit_hashes.sh
+++ Docker/fetch_commit_hashes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 git ls-remote https://gitlab.com/aroffringa/aoflagger.git     HEAD | awk '{ print "AOFLAGGER_COMMIT="$1 }'
 git ls-remote https://github.com/casacore/casacore.git        HEAD | awk '{ print "CASACORE_COMMIT="$1 }'
-git ls-remote https://git.astron.nl/RD/DP3.git                HEAD | awk '{ print "DP3_COMMIT="$1 }'
+git ls-remote https://git.astron.nl/RD/DP3.git                metadata-compression | awk '{ print "DP3_COMMIT="$1 }'
 git ls-remote https://git.astron.nl/RD/EveryBeam.git          HEAD | awk '{ print "EVERYBEAM_COMMIT="$1 }'
 git ls-remote https://git.astron.nl/RD/idg.git                HEAD | awk '{ print "IDG_COMMIT="$1 }'
 git ls-remote https://github.com/casacore/python-casacore.git HEAD | awk '{ print "PYTHONCASACORE_COMMIT="$1 }'
