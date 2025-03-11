6acc8ad8d9de5a54c692ff34c52cdfc3de0082d4
Marcel Loose <loose@astron.nl>
2025-03-11 14:15:22 +0100

    Add missing import
    Added missing import that was accidentally removed in a refactoring change. Also removed the `try`/`except` around the call of `main()`. It makes little sense, better to let the exception escape so that a clear error message is printed.

--- rapthor/scripts/add_sector_models.py
+++ rapthor/scripts/add_sector_models.py
@@ -2,13 +2,14 @@
 """
 Script to add sector model data
 """
-from argparse import ArgumentParser, RawTextHelpFormatter
-import casacore.tables as pt
-import logging
-import numpy as np
 import os
-import subprocess
 import shutil
+import subprocess
+from argparse import ArgumentParser, RawTextHelpFormatter
+
+import casacore.tables as pt
+import numpy as np
+from rapthor.lib import miscellaneous as misc
 
 
 def get_nchunks(msin, nsectors, fraction=1.0, compressed=False):
@@ -198,11 +199,7 @@ if __name__ == '__main__':
     parser.add_argument('--infix', help='Infix for output files', type=str, default='')
     args = parser.parse_args()
 
-    try:
-        main(args.msin, args.msmod, msin_column=args.msin_column,
-             model_column=args.model_column, out_column=args.out_column,
-             use_compression=args.use_compression,
-             starttime=args.starttime, quiet=args.quiet, infix=args.infix)
-    except Exception as e:
-        log = logging.getLogger('rapthor:add_sector_models')
-        log.critical(e)
+    main(args.msin, args.msmod, msin_column=args.msin_column,
+         model_column=args.model_column, out_column=args.out_column,
+         use_compression=args.use_compression,
+         starttime=args.starttime, quiet=args.quiet, infix=args.infix)
