Fix source distance calculation

rapthor/lib/field.py
@@ -998,7 +998,7 @@ class Field(object):
             names.append(name)
             source_ra.append(coord[0])
             source_dec.append(coord[1])
-        source_coord = SkyCoord(ra=source_ra, dec=source_dec)
+        source_coord = SkyCoord(ra=source_ra*u.degree, dec=source_dec*u.degree)
         separation = phase_center_coord.separation(source_coord)
         distances = [sep.value for sep in separation]
         return np.array(names), np.array(distances)

rapthor/operations/calibrate.py
@@ -447,7 +447,8 @@ class CalibrateDD(Operation):
                                zip(skymodel.getColValues('Name'),
                                    skymodel.getColValues('RA'),
                                    skymodel.getColValues('Dec'))}
-                radius = int(np.max(self.field.get_source_distances(source_dict)) / cellsize)  # pixels
+                _, source_distances = self.field.get_source_distances(source_dict)
+                radius = int(np.max(source_distances) / cellsize)  # pixels
                 size = [radius * 2, radius * 2]
         else:
             # Sky model generated in previous cycle's imaging step. Use the center and size