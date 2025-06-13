8c936134ca0a5d805735ecf2935889facf6142ab
Marcel Loose <loose@astron.nl>
2025-06-13 11:06:30 +0200

    Add test skeleton for rapthor.lib.operation module
    Added empty tests for the `Operation` class in the `rapthor.lib.operation` module.

---
+++ tests/lib/test_operation.py
@@ -0,0 +1,29 @@
+"""
+Test cases for the `rapthor.lib.operation` module.
+"""
+
+from rapthor.lib.operation import Operation
+
+
+class TestOperation:
+    """
+    Test cases for the `Operation` class in the `rapthor.lib.operation` module.
+    """
+
+    def test_set_parset_parameters(self):
+        pass
+
+    def test_set_input_parameters(self):
+        pass
+
+    def test_setup(self):
+        pass
+
+    def test_finalize(self):
+        pass
+
+    def test_is_done(self):
+        pass
+
+    def test_run(self):
+        pass
