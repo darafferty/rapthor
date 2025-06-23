14e9c003498f7748f28aa4975ca66a4bb96d8647
Marcel Loose <loose@astron.nl>
2025-06-23 12:45:59 +0200

    Add test for blank_image.py script
    Added one simple test, that doesn't check generated output but only the return status.
    TODO: add more tests

---
+++ tests/scripts/test_blank_image.py
@@ -0,0 +1,31 @@
+"""
+Tests for the blank_image script.
+"""
+
+import pytest
+from rapthor.scripts.blank_image import main
+
+
+def test_main(tmp_path):
+    """
+    Test the main function of the blank_image script.
+    """
+    # Define test parameters
+    output_image_file = tmp_path / "test_blank_image.fits"
+    input_image_file = None  # No input image, we will create a blank one
+    vertices_file = None  # No vertices file for this test
+    reference_ra_deg = 10.684  # Example RA in degrees
+    reference_dec_deg = 41.269  # Example Dec in degrees
+    cellsize_deg = 0.001  # Example cell size in degrees
+    imsize = "100,100"  # Example image size
+    region_file = "[]"  # No region file for this test
+    main(
+        output_image_file,
+        input_image_file,
+        vertices_file=vertices_file,
+        reference_ra_deg=reference_ra_deg,
+        reference_dec_deg=reference_dec_deg,
+        cellsize_deg=cellsize_deg,
+        imsize=imsize,
+        region_file=region_file,
+    )
