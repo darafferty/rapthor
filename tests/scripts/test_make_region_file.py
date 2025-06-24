98a58b83bde175776c02bf4cb9ee8facfa870b8f
Marcel Loose <loose@astron.nl>
2025-06-24 13:15:17 +0200

    Add skeleton tests for make_region_file.py
    Added empty test for the `main` function in `make_region_file.py`
    TODO: add real tests

---
+++ tests/scripts/test_make_region_file.py
@@ -0,0 +1,20 @@
+"""
+Test for the make_region_file script in the rapthor package.
+"""
+
+from rapthor.scripts.make_region_file import main
+
+
+def test_main():
+    """
+    Test the main function of the make_region_file script.
+    """
+    # # Define test parameters
+    # skymodel = "test.skymodel"
+    # ra_mid = 180.0  # Midpoint RA in degrees
+    # dec_mid = 0.0   # Midpoint Dec in degrees
+    # width_ra = 1.0  # Width in RA in degrees
+    # width_dec = 1.0  # Width in Dec in degrees
+    # region_file = "test_region_file.reg"
+    # main(skymodel, ra_mid, dec_mid, width_ra, width_dec, region_file)
+    pass
