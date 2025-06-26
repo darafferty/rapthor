9fd5b0f43f4a2e45db2b670d8e5d241d55e47f6e
Marcel Loose <loose@astron.nl>
2025-06-26 12:56:13 +0200

    Update test to work with global variables
    Updated first test (`lib/test_facet.py`) to work with global variables set through `pytest_config` in `conftest.py`.
    More tests will follow.

--- tests/lib/test_facet.py
+++ tests/lib/test_facet.py
@@ -3,6 +3,7 @@ Test cases for the rapthor.lib.facet module.
 """
 
 import os
+import pytest
 
 import numpy as np
 from rapthor.lib import facet
@@ -71,11 +72,11 @@ def test_make_facet_polygons():
     )
 
 
-def test_read_ds9_region_file():
+def test_read_ds9_region_file(request):
     """
     Test reading a DS9 region file.
     """
-    facets = facet.read_ds9_region_file("resources/test.reg")
+    facets = facet.read_ds9_region_file(request.config.resource_dir / "test.reg")
     assert len(facets) == 15
     assert facets[0].name == "Patch_1"
     assert facets[0].ra == 318.2026666666666
@@ -85,13 +86,15 @@ def test_read_ds9_region_file():
     assert facets[3].name == "Patch_12"
 
 
-def test_write_ds9_region_file():
+def test_write_ds9_region_file(request, tmp_path):
     """
     Test writing a DS9 region file.
     """
-    facets = facet.read_ds9_region_file("resources/test.reg")
-    facet.make_ds9_region_file(facets, "test_region_write.reg")
-    facets = facet.read_ds9_region_file("test_region_write.reg")
+    reg_in = request.config.resource_dir / "test.reg"
+    reg_out = tmp_path / "test_region_write.reg"
+    facets = facet.read_ds9_region_file(reg_in)
+    facet.make_ds9_region_file(facets, reg_out)
+    facets = facet.read_ds9_region_file(reg_out)
     assert len(facets) == 15
     assert facets[0].name == "Patch_1"
     assert facets[0].ra == 318.2026666666666
@@ -99,4 +102,3 @@ def test_write_ds9_region_file():
     assert facets[1].name == "Patch_10_with_spaces"
     assert facets[2].name == "Patch_11"
     assert facets[3].name == "Patch_12"
-    os.system("rm test_region_write.reg")
