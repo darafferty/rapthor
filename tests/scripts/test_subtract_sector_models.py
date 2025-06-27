d85e812776b6a40e29e4b1136dafeffb04ead4ed
Marcel Loose <loose@astron.nl>
2025-06-27 11:31:17 +0200

    Use more descriptive variable names for mocks
    The variable names for mocks used in two test scripts have been given more descriptive names. Instead of showing they were mocking `os.popen` and `subprocess.check_output`, they now show _which_ command run in the sub-process they're mocking.

--- tests/scripts/test_subtract_sector_models.py
+++ tests/scripts/test_subtract_sector_models.py
@@ -21,16 +21,16 @@ def test_get_nchunks(test_ms):
     from unittest.mock import patch
 
     with (
-        patch("os.popen") as mock_popen,
-        patch("subprocess.check_output") as mock_check_output,
+        patch("os.popen") as mock_free,
+        patch("subprocess.check_output") as mock_du,
     ):
-        mock_popen.return_value.readlines.return_value = [
+        mock_free.return_value.readlines.return_value = [
             "               total        used        free      shared  buff/cache   available\n",
             "Mem:           15840        8695        2997        1918        6403        7145\n",
             "Swap:          20479         765       19714\n",
             "Total:         36320        9461       22711\n",
         ]
-        mock_check_output.return_value = b"36039\tdummy.ms\n"
+        mock_du.return_value = b"36039\tdummy.ms\n"
 
         nchunks = get_nchunks(msin, nsectors, fraction, reweight, compressed)
         assert nchunks == 32, f"Expected 32 chunks, got {nchunks}"
