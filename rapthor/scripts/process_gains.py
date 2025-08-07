7f43f410cf32a04ddc85adca206e71f8017be2eb
astromancer <hannes@saao.ac.za>
2025-08-07 12:11:51 +0200

    GEC-141: Skip normalize if no core stations

--- rapthor/scripts/process_gains.py
+++ rapthor/scripts/process_gains.py
@@ -119,14 +119,11 @@ def normalize_direction(soltab, max_station_delta=0.0, scale_delta_with_dist=Fal
 
         # First, renormalize the direction so that core stations have a median
         # amplitude of unity
-        core_stations = np.array(['CS' in stat for stat in soltab.ant[:]])
-        if not core_stations.any():
-            # no stations are marked with 'CS' => use all stations
-            core_stations = slice(None)
-        
-        median_dir = get_median_amp(parms[:, :, core_stations, dir, :],
-                                    weights[:, :, core_stations, dir, :])
-        parms[:, :, :, dir, :] /= median_dir
+        if any(core_stations := ['CS' in stat for stat in soltab.ant[:]]):
+            # no stations are marked with 'CS' => skip 
+            median_dir = get_median_amp(parms[:, :, core_stations, dir, :],
+                                        weights[:, :, core_stations, dir, :])
+            parms[:, :, :, dir, :] /= median_dir
 
         # Now renormalize station-by-station, allowing some delta from unity
         for s in range(len(soltab.ant[:])):
