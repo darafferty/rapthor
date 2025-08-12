37775fa5cf106ddcbcb5693db9a2509266a8c497
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2025-08-12 15:01:33 +0200

    Use misc.WCS_ORIGIN instead of 0

--- rapthor/lib/miscellaneous.py
+++ rapthor/lib/miscellaneous.py
@@ -249,7 +249,9 @@ def read_vertices(filename, wcs):
         vertices_ra, vertices_dec = pickle.load(f)
 
     # Convert to x, y coordinates.
-    vertices_x, vertices_y = wcs.wcs_world2pix(vertices_ra, vertices_dec, 0)
+    vertices_x, vertices_y = wcs.wcs_world2pix(vertices_ra,
+                                               vertices_dec,
+                                               WCS_ORIGIN)
 
     # Convert to a list of (x, y) tuples.
     return list(zip(vertices_x, vertices_y))
