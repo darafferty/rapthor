9911ffd602a1e5f3574f1805b17d1dd0a542fac6
astromancer <hannes.breytenbach@skao.int>
2025-11-06 10:26:24 +0200

    GEC-182: Update strategy

--- examples/custom_benchmark_strategy.py
+++ examples/custom_benchmark_strategy.py
@@ -10,8 +10,9 @@ detailed information on each parameter.
 """
 
 strategy_steps = []
-min_selfcal_loops = 4
-max_selfcal_loops = 8
+do_phase_only_solves = True
+min_selfcal_loops = 4 if do_phase_only_solves else 2
+max_selfcal_loops = 8 if do_phase_only_solves else 6
 
 for i in range(max_selfcal_loops):
     strategy_steps.append(step := {})
@@ -23,18 +24,20 @@ for i in range(max_selfcal_loops):
     # amplitude solutions (per station) is set to 0.3, to allow for small adjustments to
     # the station calibration (done in LINC).
     step['do_calibrate'] = True
-    step['do_slowgain_solve'] = True
-    step['peel_outliers'] = False
-    step['solve_min_uv_lambda'] = 0.0
-    step['peel_bright_sources'] = False
+    step['peel_outliers'] = (i == 0)
+    step['do_slowgain_solve'] = not do_phase_only_solves if i < 2 else True
+    step['solve_min_uv_lambda'] = 2000 if i == 2 and do_phase_only_solves else 750
     step['do_fulljones_solve'] = False
+    step['peel_bright_sources'] = False
     step['max_normalization_delta'] = 0.3
     step['scale_normalization_delta'] = True
+    step['fast_timestep_sec'] = 32.0
+    step['slow_timestep_sec'] = 600.0
 
     # Here we use the same solution intervals for every cycle, but it is possible to
     # adjust the intervals so that, for example, the early cycles have longer intervals to
     # compensate for the generally lower signal-to-noise ratios of the solves
-    step['fast_timestep_sec'] = 16.0
+    step['fast_timestep_sec'] = 32.0
     step['slow_timestep_sec'] = 600.0
 
     # We do not do any flux-scale normalization
@@ -49,11 +52,22 @@ for i in range(max_selfcal_loops):
     # is set to 2, which has been found to be a sufficient number of iterations in
     # most cases
     step['do_image'] = True
-    step['auto_mask'] = 3.0
-    step['threshisl'] = 3.0
-    step['threshpix'] = 3.0
-    step['max_nmiter'] = 20 if i == 0 else 25
-    step['auto_mask_nmiter'] = 5
+    if i < 2 and do_phase_only_solves:
+        step['auto_mask'] = 5.0
+        step['threshisl'] = 4.0
+        step['threshpix'] = 5.0
+        step['max_nmiter'] = 8
+    elif (i == 2 and do_phase_only_solves) or (i == 0 and not do_phase_only_solves):
+        step['auto_mask'] = 4.0
+        step['threshisl'] = 3.0
+        step['threshpix'] = 5.0
+        step['max_nmiter'] = 10
+    else:
+        step['auto_mask'] = 3.0
+        step['threshisl'] = 3.0
+        step['threshpix'] = 5.0
+        step['max_nmiter'] = 12
+    step['auto_mask_nmiter'] = 2
 
     # Here we set the calibrator selection strategy, decreasing the target
     # minimum flux density for sources to be used as calibrators as selfcal
@@ -61,19 +75,27 @@ for i in range(max_selfcal_loops):
     # (directions) to ensure that the resource requirements and runtimes don't
     # grow unnecessarily large. We also limit the distance from the phase center
     # that calibrators can have to exclude distant sources
-    if i == 0:
-        step['target_flux'] = 0.3
-        step['max_directions'] = 20
-        step['max_distance'] = 3.0
+    step['regroup_model'] = True
+    step['max_distance'] = 3.0     # overwritten below for cycle <2
+    step['target_flux'] = 0.3      # overwritten below for cycle 0, 1
+    step['max_directions'] = 40    # overwritten below for cycle 0, 1
+    if do_phase_only_solves:
+        if i == 0:
+            step['target_flux'] = 0.6
+            step['max_directions'] = 20
+        elif i == 1:
+            step['target_flux'] = 0.4
+            step['max_directions'] = 30
+        elif i == 2:
+            step['max_distance'] = 3.5
+        else:
+            step['max_distance'] = 4.0
+
+    # do_phase_only_solves = False
     elif i == 1:
-        step['target_flux'] = 0.25
-        step['max_directions'] = 40
         step['max_distance'] = 3.5
     else:
-        step['target_flux'] = 0.25
-        step['max_directions'] = 50
         step['max_distance'] = 4.0
-    step['regroup_model'] = True
 
     # Here we specify that the convergence/divergence checks are done only when
     # needed, to prevent the selfcal from stopping early (before
