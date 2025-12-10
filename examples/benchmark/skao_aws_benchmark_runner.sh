62bd0f8cd0cedd08781149e5d428951d7887eade
astromancer <hannes.breytenbach@skao.int>
2025-12-10 13:48:06 +0200

    GEC-182: load benchmon module

--- examples/benchmark/skao_aws_benchmark_runner.sh
+++ examples/benchmark/skao_aws_benchmark_runner.sh
@@ -3,23 +3,26 @@
 ###############################################################################
 # Rapthor Benchmark Script for SKAO AWS cluster
 #
-# This script is intended to be run by the `run_bench_all.sh` script in
+# This script is intended to be run by the `run_all_tests.sh` script in
 # ska-sdp-spack/scripts/:
-# https://gitlab.com/ska-telescope/sdp/ska-sdp-spack/-/blob/sp-5859-add-bench-scripts/scripts/run_test.sh
+# https://gitlab.com/ska-telescope/sdp/ska-sdp-spack/-/blob/sp-5859-add-bench-scripts/scripts/run_all_tests.sh
 # The wrapper script loads the environmental variables from the file
 # `ska-sdp-spack/scripts/test_specs/_ical.sh`, and executes this script by
-# running the benchmark wrapper script `run_bench.sh`.
+# running the benchmark wrapper script `run_test.sh`.
 
 # Environment variables:
 # ---------------------------------------------------------------------------- #
-# The following are set in `run_bench.sh`:
+# The `run_all_tests.sh` wrapper script will set the 
+# META_MODULE variable to the latest release before executing this script.
+#
+# The following path variables are set in `run_test.sh`:
 #     INPUT_PATH   : Path containing input Measurement Set (*.ms),
 #                    pipeline config file, and any extra inputs.
 #     WORK_PATH    : Scratch/work directory
 #     OUTPUT_PATH  : Directory where pipeline outputs will be written
 #     REPORT_PATH  : Directory where reports will be copied
 #     CODE_PATH    : Path to the rapthor code repository
-#     META_MODULE  : Spack meta-module to load dependencies
+#     LOG_PATH     : Directory where logs will be written
 #
 # The following are set by `_ical.sh`:
 #     PARTITION    : SLURM partition to use
@@ -27,19 +30,19 @@
 # 
 # The following environmental variables should be set to configure compute
 # resources for rapthor. These are used to populate the `[cluster]` section of
-# the parset file.:
-#    batch_system = $RAPTHOR_BATCH_SYSTEM
-#    max_nodes = $RAPTHOR_MAX_NODES
-#    cpus_per_task = $RAPTHOR_CPUS_PER_TASK
-#    mem_per_node_gb = $RAPTHOR_MEM_PER_NODE_GB
-#    max_cores = $RAPTHOR_MAX_CORES
-#    max_threads = $RAPTHOR_MAX_THREADS
-#    deconvolution_threads = $RAPTHOR_DECONV_THREADS
-#    parallel_gridding_threads = $RAPTHOR_PARALLEL_GRIDDING_THREADS
+# the template parset file.:
+#    RAPTHOR_BATCH_SYSTEM
+#    RAPTHOR_MAX_NODES
+#    RAPTHOR_CPUS_PER_TASK
+#    RAPTHOR_MEM_PER_NODE_GB
+#    RAPTHOR_MAX_CORES
+#    RAPTHOR_MAX_THREADS
+#    RAPTHOR_DECONV_THREADS
+#    RAPTHOR_PARALLEL_GRIDDING_THREADS
 #
 # Parset and Strategy:
 # ---------------------------------------------------------------------------- #
-# The `run_bench.sh` wrapper script will synchronise the parset and strategy
+# The `run_test.sh` wrapper script will synchronise the parset and strategy
 # files into the INPUT_PATH before executing this script. The parset used for
 # this run is available in the rapthor repository under
 # `/examples/rapthor_skao_benchmark_template.parset`. 
@@ -69,36 +72,7 @@ echo "Starting Rapthor benchmark script on $(hostname) at $(date)"
 
 # Load spack modules
 # ---------------------------------------------------------------------------- #
-# echo "Loading rapthor modules"
-module load py-rapthor py-ska-sdp-benchmark-monitor
-
-# Ensure bdsf can find libboost_numpy311.so.1.86.0
-module show boost
-export LD_LIBRARY_PATH=$BOOST_ROOT/lib:$LD_LIBRARY_PATH
-echo $LD_LIBRARY_PATH
-
-# Install rapthor from branch
-# ---------------------------------------------------------------------------- #
-echo -e "Installing rapthor\n\
--------------------------------------------------------------------------------"
-# Check repo status
-cd $CODE_PATH
-git -C $CODE_PATH status --porcelain
-echo "Installing rapthor from branch: $(git -C $CODE_PATH rev-parse --abbrev-ref HEAD)"
-
-# Create and activate virtual environment
-python -m venv $WORK_PATH/.venv
-source $WORK_PATH/.venv/bin/activate
-
-# Install using pip
-INSTALL_LOG=$LOG_PATH/rapthor-install.log
-pip install --upgrade pip 2>&1 > $INSTALL_LOG
-pip install $CODE_PATH 2>&1 >> $INSTALL_LOG
-
-# Check installation
-echo "Installed $(rapthor --version)"
-echo -e "Environment ready!\n\
--------------------------------------------------------------------------------"
+module load py-rapthor@master py-ska-sdp-benchmark-monitor
 
 # Configure
 # ---------------------------------------------------------------------------- #
