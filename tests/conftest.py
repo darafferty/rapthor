de3d626f1d1b1c971ff8d277b551be3a366d808f
Mancini <mattia.mancini@skao.int>
2026-01-13 10:08:04 +0100

    GEC-254: Make conftest more generic

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,37 @@ This files contains the configuration for pytest, including fixtures and hooks
 for this directory.
 """
 
+import configparser
 import shutil
 from pathlib import Path
 
 import pytest
 
+from rapthor.lib.field import Field
+from rapthor.lib.parset import parset_read
+
+
 RESOURCE_DIR = Path(__file__).parent / "resources"
+
+def pytest_configure(config):
+    config.resource_dir = RESOURCE_DIR
+
+"""
+This files contains the configuration for pytest, including fixtures and hooks
+for this directory.
+"""
+
+import configparser
+import shutil
+from pathlib import Path
+
+import pytest
+
+from rapthor.lib.field import Field
+from rapthor.lib.parset import parset_read
+
+RESOURCE_DIR = (Path(__file__).parent / "resources").resolve()
+
 
 def pytest_configure(config):
     config.resource_dir = RESOURCE_DIR
@@ -20,5 +45,35 @@ def test_ms(tmp_path):
     Fixture to provide a copy of the test MS in the resources directory.
     Yield the POSIX path to the copy of the MS.
     """
-    shutil.copytree(RESOURCE_DIR / "test.ms", tmp_path / "test.ms")
+    shutil.copytree(RESOURCE_DIR / "test.ms", tmp_path/ "test.ms")
     yield (tmp_path / "test.ms").as_posix()
+
+
+@pytest.fixture
+def parset(test_ms, tmp_path):
+    """
+    Fixture to create a parset dictionary for testing.
+    """
+    cfg = configparser.ConfigParser(interpolation=None)
+    cfg.read(RESOURCE_DIR / "test.parset")
+    cfg.set("global", "dir_working", tmp_path.as_posix())
+    cfg.set("global", "input_ms", test_ms)
+    cfg.set("global", "input_skymodel", (RESOURCE_DIR / "test_true_sky.txt").as_posix())
+    cfg.set("global", "apparent_skymodel", (RESOURCE_DIR / "test_apparent_sky.txt").as_posix())
+
+    parset_file = tmp_path / "test.parset"
+    with parset_file.open("w") as fh:
+        cfg.write(fh)
+
+    parset_dict = parset_read(parset_file, use_log_file=False)
+    yield parset_dict
+
+
+@pytest.fixture
+def field(parset):
+    """
+    Fixture to create a Field object for testing.
+    """
+    return Field(parset)
+
+
