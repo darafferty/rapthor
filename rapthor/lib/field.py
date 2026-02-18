Fix chunk spacing calculation

rapthor/lib/field.py
@@ -331,10 +331,12 @@ class Field(object):
                 else:
                     chunked_observations.append(obs)
             else:
-                steptime = (
-                    obs_mintime * (tottime / obs_mintime - nchunks) / nchunks
-                    + obs_mintime
-                )
+                # Calculate the start time of each chunk so that they are spaced out
+                # evently over the full observation. This is calculated as:
+                #   time_between_gaps = total_time_in_gaps / (nchunks - 1) + chunk_time
+                steptime = (tottime - nchunks * obs_mintime) / (
+                    nchunks - 1
+                ) + obs_mintime
                 starttimes = np.arange(target_starttime, target_endtime, steptime)
                 endtimes = np.arange(
                     target_starttime + obs_mintime,