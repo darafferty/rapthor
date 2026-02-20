c6f4a3ed057240c5dfe5f0f7b398c784537ccd3b
astromancer <hannes.breytenbach@skao.int>
2026-02-20 09:57:55 +0200

    GEC-217: Wrap long strings

--- rapthor/lib/parset.py
+++ rapthor/lib/parset.py
@@ -578,7 +578,7 @@ def parset_read(parset_file, use_log_file=True):
 def check_skymodel_settings(parset_dict):
     """
     En‌sure·‌the·‌initial·‌sky·‌model·‌is·‌present·‌or,·‌if·‌not,·‌that·‌generation·‌or
-    download·‌is·‌requested
+    download·‌is·‌requested.
 
     Parameters
     ----------
@@ -590,8 +590,7 @@ def check_skymodel_settings(parset_dict):
     FileNotFoundError
         If the input sky model file is not found.
     """
-    
-    input_skymodel = parset_dict["input_skymodel"]
+
     generate_initial_skymodel = parset_dict["generate_initial_skymodel"]
     download_initial_skymodel = parset_dict["download_initial_skymodel"]
     apparent_skymodel = parset_dict["apparent_skymodel"]
@@ -604,16 +603,18 @@ def check_skymodel_settings(parset_dict):
             # If sky model is given but generation requested, disable
             # generation and use the given skymodel.
             log.warning(
-                "Sky model generation requested, but user-provided sky model is present. "
-                "Disabling generation and using sky model provided by the user."
+                "Sky model generation requested, but user-provided sky model is"
+                " present. Disabling generation and using sky model provided by"
+                " the user."
             )
             parset_dict["download_initial_skymodel"] = False
         elif download_initial_skymodel:
             # If sky model is given but download requested, use the given
             # skymodel and disable download.
             log.warning(
-                "Sky model download requested, but user-provided sky model is present. "
-                "Disabling download and using sky model provided by the user."
+                "Sky model download requested, but user-provided sky model is "
+                "present. Disabling download and using sky model provided by "
+                "the user."
             )
             parset_dict["download_initial_skymodel"] = False
 
@@ -639,7 +640,10 @@ def check_skymodel_settings(parset_dict):
             )
     else:
         log.warning(
-
+            "No input sky model file given and neither generation nor download "
+            "of sky model requested. If no calibration is to be done, this "
+            "warning can be ignored."
+        )
 
     # If `astrometry_skymodel` or `photometry_skymodel` is given, check if the
     # file exists, if not raise an error.
