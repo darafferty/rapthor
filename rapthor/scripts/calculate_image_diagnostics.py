6e956faf4a75ceee6fcf0cb41207e29819116a0b
astromancer <hannes.breytenbach@skao.int>
2026-02-20 17:16:58 +0200

    GEC-217: Use `NamedTemporaryFile` as context manager

--- rapthor/scripts/calculate_image_diagnostics.py
+++ rapthor/scripts/calculate_image_diagnostics.py
@@ -201,11 +201,9 @@ def fits_to_makesourcedb(catalog, reference_freq, flux_colname="Isl_Total_flux")
             )
         ),
     ]
-    skymodel_file = tempfile.NamedTemporaryFile()
-    with open(skymodel_file.name, "w") as f:
-        f.writelines(out_lines)
-
-    return lsmtool.load(skymodel_file.name)
+    with tempfile.NamedTemporaryFile("w+") as skymodel_file:
+        skymodel_file.write("\n".join(out_lines))
+        return lsmtool.load(skymodel_file.name)
 
 
 def check_photometry(
