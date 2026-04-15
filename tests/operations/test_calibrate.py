daecacee3f4402b18cd353023b7c2a58c6468324
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2026-04-15 09:13:47 +0000

    Use is_id / with_slow shortcuts

--- tests/operations/test_calibrate.py
+++ tests/operations/test_calibrate.py
@@ -248,6 +248,9 @@ class TestCalibrateDI:
 class TestCalibrate:
     @pytest.mark.parametrize("scenario", ["dd_fast_only", "dd_with_slowgain", "di_fulljones"])
     def test_finalize(self, mocker, field, tmp_path, scenario):
+        is_dd = scenario.startswith("dd")
+        with_slow = scenario == "dd_with_slowgain"
+
         # Setup mocks
         flagged_fraction = 0.042
         mocker.patch(
@@ -259,15 +262,13 @@ class TestCalibrate:
 
         # Setup working directory
         workdir_path = tmp_path / "working"
-        name = "calibrate_2" if scenario.startswith("dd") else "calibrate_di_4"
+        name = "calibrate_2" if is_dd else "calibrate_di_4"
         solutions_path = workdir_path / "solutions" / name
         solutions_path.mkdir(parents=True)
 
         # Create an existing solutions file. finalize() should remove it.
-        if scenario.startswith("dd"):
-            h5parm_path = solutions_path / "field-solutions.h5"
-        else:
-            h5parm_path = solutions_path / "fulljones-solutions.h5"
+        h5parm_filename = "field-solutions.h5" if is_dd else "fulljones-solutions.h5"
+        h5parm_path = solutions_path / h5parm_filename
         h5parm_path.touch()
 
         pipelines_path = workdir_path / "pipelines" / name
@@ -278,19 +279,16 @@ class TestCalibrate:
         field.generate_screens = False
         field.do_slowgain_solve = scenario == "dd_with_slowgain"
 
-        if scenario.startswith("dd"):
-            calibrate = CalibrateDD(field, index=2)
-        else:
-            calibrate = CalibrateDI(field, index=4)
+        calibrate = CalibrateDD(field, index=2) if is_dd else CalibrateDI(field, index=4)
 
-        if scenario.startswith("dd"):
+        if is_dd:
             calibrate.combined_h5parms = "combined.test.h5"
             calibrate.fast_h5parm = "fast.test.h5"
-        if scenario == "dd_with_slowgain":
-            calibrate.slow_h5parm = "slow.test.h5"
-            calibrate.medium1_h5parm = "medium1.test.h5"
-            calibrate.medium2_h5parm = "medium2.test.h5"
-        elif scenario == "di_fulljones":
+            if with_slow:
+                calibrate.slow_h5parm = "slow.test.h5"
+                calibrate.medium1_h5parm = "medium1.test.h5"
+                calibrate.medium2_h5parm = "medium2.test.h5"
+        else:
             calibrate.collected_h5parm_fulljones = "collected_fulljones.h5"
 
         # Ignore os.makedirs calls from the base Operation class constructor.
@@ -300,22 +298,22 @@ class TestCalibrate:
         calibrate.finalize()
 
         # Assert
-        if scenario.startswith("dd"):
+        if is_dd:
             assert field.h5parm_filename == str(h5parm_path)
             assert field.fast_phases_h5parm_filename == str(
                 solutions_path / "field-solutions-fast-phase.h5"
             )
-        if scenario == "dd_with_slowgain":
-            assert field.medium1_phases_h5parm_filename == str(
-                solutions_path / "field-solutions-medium1-phase.h5"
-            )
-            assert field.medium2_phases_h5parm_filename == str(
-                solutions_path / "field-solutions-medium2-phase.h5"
-            )
-            assert field.slow_gains_h5parm_filename == str(
-                solutions_path / "field-solutions-slow-gain.h5"
-            )
-        elif scenario == "di_fulljones":
+            if with_slow:
+                assert field.medium1_phases_h5parm_filename == str(
+                    solutions_path / "field-solutions-medium1-phase.h5"
+                )
+                assert field.medium2_phases_h5parm_filename == str(
+                    solutions_path / "field-solutions-medium2-phase.h5"
+                )
+                assert field.slow_gains_h5parm_filename == str(
+                    solutions_path / "field-solutions-slow-gain.h5"
+                )
+        else:
             assert field.fulljones_h5parm_filename == str(h5parm_path)
 
         check_makedirs(mock_makedirs, solutions_path, plots_path)
@@ -323,9 +321,9 @@ class TestCalibrate:
         # Check removing and copying solutions.
         mock_remove.assert_any_call(str(h5parm_path))
 
-        if scenario == "dd_with_slowgain":
+        if with_slow:
             solution_src_dst_list = [
-                ("combined.test.h5", h5parm_path.name),
+                ("combined.test.h5", h5parm_filename),
                 ("slow.test.h5", "field-solutions-slow-gain.h5"),
                 ("medium1.test.h5", "field-solutions-medium1-phase.h5"),
                 ("medium2.test.h5", "field-solutions-medium2-phase.h5"),
@@ -333,12 +331,12 @@ class TestCalibrate:
             ]
         elif scenario == "dd_fast_only":
             solution_src_dst_list = [
-                ("fast.test.h5", h5parm_path.name),
+                ("fast.test.h5", h5parm_filename),
                 ("fast.test.h5", "field-solutions-fast-phase.h5"),
             ]
-        elif scenario == "di_fulljones":
+        else:
             solution_src_dst_list = [
-                ("collected_fulljones.h5", h5parm_path.name),
+                ("collected_fulljones.h5", h5parm_filename),
             ]
 
         for src, dst in solution_src_dst_list:
@@ -346,7 +344,7 @@ class TestCalibrate:
 
         field.scan_h5parms.assert_called_once()
 
-        if scenario.startswith("dd"):
+        if is_dd:
             assert field.calibration_diagnostics == [
                 {
                     "cycle_number": 2,
