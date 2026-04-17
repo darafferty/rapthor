f8f70815d390f14205b95497bc686e01f89429f4
Maryna Shedzko <maryna.shedzko@skao.int>
2026-04-17 10:07:00 +0100

    GEC-299: Refactor to use calibrate_field fixture

--- tests/operations/test_calibrate.py
+++ tests/operations/test_calibrate.py
@@ -3,20 +3,9 @@ Test cases for the `rapthor.operations.calibrate` module.
 """
 
 import pytest
+
 from rapthor.operations.calibrate import CalibrateDD, CalibrateDI
 
-BASELINES_CORE_CASES = [
-    (
-        'LBA',
-        ['CS001LBA', 'CS002LBA', 'RS106LBA', 'DE601LBA', 'UK608LBA'],
-        '[CR]*&&;!DE601LBA;!UK608LBA',
-    ),
-    (
-        'HBA',
-        ['CS003HBA0', 'RS106HBA0', 'DE601HBA', 'UK902HBA'],
-        '[CR]*&&;!DE601HBA;!UK902HBA',
-    ),
-]
 
 @pytest.fixture
 def calibrate_field(operation_parset, mocker):
@@ -59,11 +48,25 @@ class TestCalibrateDD:
         # calibrate_dd.set_input_parameters()
         pass
 
-    @pytest.mark.parametrize('antenna, stations, expected', BASELINES_CORE_CASES)
-    def test_get_baselines_core(self, antenna, stations, expected):
-        calibrate_dd = CalibrateDD(field=None, index=1)
-        calibrate_dd.field.antenna = antenna
-        calibrate_dd.field.stations = stations
+    BASELINES_CORE_CASES = [
+        (
+            "LBA",
+            ["CS001LBA", "CS002LBA", "RS106LBA", "DE601LBA", "UK608LBA"],
+            "[CR]*&&;!DE601LBA;!UK608LBA",
+        ),
+        (
+            "HBA",
+            ["CS003HBA0", "RS106HBA0", "DE601HBA", "UK902HBA"],
+            "[CR]*&&;!DE601HBA;!UK902HBA",
+        ),
+    ]
+
+    @pytest.mark.parametrize("antenna, stations, expected", BASELINES_CORE_CASES)
+    def test_get_baselines_core(self, calibrate_field, antenna, stations, expected):
+        calibrate_field.antenna = antenna
+        calibrate_field.stations = stations
+        calibrate_dd = CalibrateDD(field=calibrate_field, index=1)
+
         baselines = calibrate_dd.get_baselines_core()
         assert baselines == expected
 
