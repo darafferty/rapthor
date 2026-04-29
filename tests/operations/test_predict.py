c5d14c1045f513b10e94ca064dc7e53e43562d86
Maryna Shedzko <maryna.shedzko@skao.int>
2026-04-29 13:17:53 +0000

    GEC-428: Add test for predict.set_parset_parameters

--- tests/operations/test_predict.py
+++ tests/operations/test_predict.py
@@ -1,15 +1,40 @@
 """
 Test cases for the `rapthor.operations.predict` module.
 """
+from pathlib import Path
 
 import pytest
+
+import rapthor
 from rapthor.operations.predict import PredictDD, PredictDI
 
 
 @pytest.fixture
-def field():
-    # Mock or create a field object as needed for testing
-    return "mock_field"
+def predict_field(operation_parset, mocker, single_source_sky_model):
+    class Field:
+        def __init__(self, parset):
+            self.parset = parset
+
+            self.imaging_sectors = []
+            self.bright_source_sectors = []
+            self.outlier_sectors = []
+            self.predict_sectors = []
+            self.sectors = []
+
+            self.observations = []
+
+            self.data_colname = "DATA"
+            self.h5parm_filename = "h5.parm"
+            self.normalize_h5parm = "norm.h5"
+
+            self.apply_amplitudes = False
+            self.apply_normalizations = False
+
+            self.onebeamperpatch = True
+            self.sagecalpredict = False
+            self.correct_smearing_in_calibration = False
+
+    return Field(operation_parset)
 
 
 @pytest.fixture
@@ -31,9 +56,6 @@ def predict_di(field, index=1):
 
 
 class TestPredictDD:
-    def test_set_parset_parameters(self, predict_dd):
-        # predict_dd.set_parset_parameters()
-        pass
 
     def test_set_input_parameters(self, predict_dd):
         # predict_dd.set_input_parameters()
@@ -45,9 +67,6 @@ class TestPredictDD:
 
 
 class TestPredictDI:
-    def test_set_parset_parameters(self, predict_di):
-        # predict_di.set_parset_parameters()
-        pass
 
     def test_set_input_parameters(self, predict_di):
         # predict_di.set_input_parameters()
@@ -56,3 +75,32 @@ class TestPredictDI:
     def test_finalize(self, predict_di):
         # predict_di.finalize()
         pass
+
+class TestPredict:
+    @pytest.mark.parametrize(
+        "mode, batch_system, max_cores, expected_cores",
+        [
+            ("dd", "some_other_system", 42, 42),
+            ("dd", "slurm", 42, None),
+            ("di", "some_other_system", 42, 42),
+            ("di", "slurm", 42, None),
+            
+        ],
+)
+    def test_set_parset_parameters(
+        self, predict_field, mode, batch_system, max_cores, expected_cores
+    ):
+        # Arrange
+        predict_field.parset["cluster_specific"]["batch_system"] = batch_system
+        predict_field.parset["cluster_specific"]["max_cores"] = max_cores
+        predict_field.rapthor_pipeline_dir = "/tmp/pipeline"
+
+        predict = PredictDD(predict_field, index = 1) if mode == "dd" else PredictDI(predict_field, index = 1)
+        # Act
+        predict.set_parset_parameters()
+
+        # Assert
+        rapthor_pipeline_path = Path(rapthor.__file__).parent / "pipeline"
+
+        assert predict.parset_parms["rapthor_pipeline_dir"] == str(rapthor_pipeline_path)
+        assert predict.parset_parms["max_cores"] == expected_cores
