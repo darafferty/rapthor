1714e2fa7a0a3d21473560e4421adcb62f977cf2
Mancini <mattia.mancini@skao.int>
2026-04-30 16:52:30 +0200

    GEC-422: Add test for DD fast medium phase

---
+++ tests/integration/test_dd_calibration.py
@@ -0,0 +1,87 @@
+import pytest
+from pathlib import Path
+from .utils import (
+    find_step_logs,
+    get_working_dir_from_parset,
+    parse_dp3_args_from_log,
+    update_parset_path
+    )
+import subprocess
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize(
+    "generated_parset_path",
+    [
+        (
+            "tests/resources/integration_template.parset",
+            "tests/resources/integration_true_sky.txt",
+            "tests/resources/integration_apparent_sky.txt",
+        )
+    ],
+    indirect=True,
+)
+def test_rapthor_run_dd_fast_phase_medium_phase(generated_parset_path, 
+                                 single_loop_strategy_path):
+    """Test a calibration-only loop with mocked DP3."""
+
+    updated_parset_path = update_parset_path(
+        generated_parset_path,
+        {
+            "allow_internet_access": "False",
+            "strategy": str(single_loop_strategy_path),
+        },
+    )
+
+    working_dir = get_working_dir_from_parset(updated_parset_path)
+    print("---Rapthor working dir: ", working_dir)
+
+    command = ["rapthor", str(updated_parset_path)]
+    result = subprocess.run(
+        command,
+        capture_output=True,
+        text=True,
+        check=True
+    )
+    output = f"{result.stdout}\n{result.stderr}"
+    assert result.returncode == 0, f"Rapthor failed with output:\n{output}"
+    assert "Operation calibrate_1 completed" in output
+    assert "Operation predict_1 completed" in output
+    assert "Operation image_1 completed" in output
+    assert "Operation mosaic_1 completed" in output
+    assert "Rapthor has finished :)" in output
+
+    calibrate_logs_dir = Path(working_dir) / "logs" / "calibrate_1"
+    calibrate_log = find_step_logs(calibrate_logs_dir, "ddecal_solve.cwl")
+    assert calibrate_log, "Expected calibration logs to be present"
+    dp3_arguments = parse_dp3_args_from_log(calibrate_log[0])
+
+    assert "steps" in dp3_arguments
+    assert "solve1" in dp3_arguments["steps"]
+    assert "solve2" in dp3_arguments["steps"]
+    assert "fast_phase_0.h5parm" == dp3_arguments["solve1.h5parm"]
+    assert "medium1_phase_0.h5parm" == dp3_arguments["solve2.h5parm"]
+    assert "scalarphase" == dp3_arguments["solve1.mode"]
+    assert "scalarphase" == dp3_arguments["solve2.mode"]
+    assert int(dp3_arguments["solve1.solint"]) < \
+           int(dp3_arguments["solve2.solint"])
+    
+    
+    
+def test_fast_test():
+    working_dir = "/tmp/ical-lsd46y14/work"
+    calibrate_logs_dir = Path(working_dir) / "logs" / "calibrate_1"
+    calibrate_log = find_step_logs(calibrate_logs_dir, "ddecal_solve.cwl")
+    assert calibrate_log, f"Expected calibration logs to be present {calibrate_log}"
+    dp3_arguments = parse_dp3_args_from_log(calibrate_log[0])
+
+    assert "steps" in dp3_arguments
+    assert "solve1" in dp3_arguments["steps"]
+    assert "solve2" in dp3_arguments["steps"]
+    assert "fast_phase_0.h5parm" == dp3_arguments["solve1.h5parm"]
+    assert "medium1_phase_0.h5parm" == dp3_arguments["solve2.h5parm"]
+    assert "scalarphase" == dp3_arguments["solve1.mode"]
+    assert "scalarphase" == dp3_arguments["solve2.mode"]
+    assert int(dp3_arguments["solve1.solint"]) < \
+           int(dp3_arguments["solve2.solint"])
+    
