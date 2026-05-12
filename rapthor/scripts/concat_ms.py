bf4608ef13d5c197c5087cc7b2bc18cf28ffe3a6
David Rafferty <drafferty@hs.uni-hamburg.de>
2026-05-12 13:06:36 +0000

    Fix ordering of MS files when doing concantenation
    This MR adds code to ensure the correct order of MS files when doing concatenation over time (needed for WSClean, which requires that the input data are ordered in time).
    
    See merge request RD/rapthor!426

--- rapthor/scripts/concat_ms.py
+++ rapthor/scripts/concat_ms.py
@@ -201,11 +201,18 @@ def concat_time_command(msfiles, output_file):
     cmd : list of str
         Command to be executed by subprocess.run()
     """
+    starttimes = []
+    for ms in msfiles:
+        # Get the start time of each file (MJD time in seconds)
+        with pt.table(ms, ack=False) as tab:
+            starttimes.append(np.min(tab.getcol('TIME')))
+    ind = np.argsort(starttimes)
+
     cmd = [
         "taql",
         "select",
         "from",
-        f"[{','.join(msfiles)}]",
+        f"[{','.join(np.array(msfiles)[ind])}]",
         "giving",
         str(output_file),
         "AS",
