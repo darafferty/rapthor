4862d7954aeb5fafca080f4896672a44399d5c53
Sarod Yatawatta <sarod@dop373.nfra.nl>
2026-05-19 10:13:35 +0200

    add cleanup

--- rapthor/scripts/wsclean_predict.py
+++ rapthor/scripts/wsclean_predict.py
@@ -20,28 +20,24 @@ import casacore.tables as ct
 import numpy as np
 
 
-def add_columns_to_ms(msname, colnames):
+def remove_columns_from_ms(msfile, ds9_region_file):
     """
-    Add additional columns to table
-    Do this in one call, rather than using taql and doing it in serial
+    Remove extra columns added from prediction
     """
-    tt = ct.table(msname, readonly=False)
-    cl = tt.getcol("DATA")
-    (nrows, nchans, npols) = cl.shape
-    vl = np.zeros(shape=cl.shape, dtype=cl.dtype)
-    dmi = tt.getdminfo("DATA")
-    for colname in colnames:
-        dmi["NAME"] = colname
-        mkd = ct.maketabdesc(
-            ct.makearrcoldesc(
-                colname,
-                shape=np.array(np.zeros([nchans, npols])).shape,
-                valuetype="complex",
-                value=0.0,
-            )
-        )
-        tt.addcols(mkd, dmi)
-        tt.putcol(colname, vl)
+    # Check pre-conditions
+    if not os.path.exists(msfile):
+        raise ValueError(f"Input measurement set {msfile!r} does not exist")
+    if not os.path.exists(ds9_region_file):
+        raise ValueError(f"DS9 region file {ds9_region_file!r} does not exist")
+
+    tt = ct.table(msfile, readonly=False)
+    colnames = tt.colnames()
+
+    facets = read_ds9_region_file(ds9_region_file)
+    for facet in facets:
+        colname = facet.name
+        if colname in colnames:
+            tt.removecols(colname)
     tt.close()
 
 
@@ -52,6 +48,8 @@ def predict(msfile, ds9_region_file, model_image):
     ----------
     msfile : MS name, output will be written to separate columns
     ds9_region_file: DS9 region file, specifying facet regions and names
+    Note: names in region file should be with {}, like {Patch_1}, After
+    parsing the {} will be dropped
     model_image: FITS image to use as model
 
     Returns
@@ -82,8 +80,6 @@ def predict(msfile, ds9_region_file, model_image):
     for facet in facets:
         facet_names.append(facet.name)
 
-    add_columns_to_ms(msfile, facet_names)
-
     err_code = 0
     # Run the command
     for facet in facet_names:
@@ -111,7 +107,13 @@ def predict(msfile, ds9_region_file, model_image):
 
 
 def main():
-    """ """
+    """
+    Script can be used in two ways:
+    1) To add columns and predict model
+      wsclean_predict.py --region sector_1_facets_ds9.reg --msin small.ms --model
+    2) To remove extra columns created from step 1
+      wsclean_predict.py --region sector_1_facets_ds9.reg --msin small.ms --cleanup
+    """
     descriptiontext = "Predict model data using WSClean.\n"
     parser = ArgumentParser(
         description=descriptiontext, formatter_class=RawTextHelpFormatter
@@ -121,8 +123,13 @@ def main():
     )
     parser.add_argument("--region", help="DS9 region file", type=str, default="")
     parser.add_argument("--model", help="Model FITS image", type=str, default="")
+    parser.add_argument("--cleanup", action="store_true", help="Remove exra columns")
     args = parser.parse_args()
-    return predict(args.msin, args.region, args.model)
+
+    if args.cleanup:
+        return remove_columns_from_ms(args.msin, args.region)
+    else:
+        return predict(args.msin, args.region, args.model)
 
 
 if __name__ == "__main__":
