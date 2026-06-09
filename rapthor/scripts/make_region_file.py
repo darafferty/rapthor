4e84c4aa74ba6c1a0a26c578653604dce12e56f9
David Rafferty <drafferty@hs.uni-hamburg.de>
2026-06-09 12:14:56 +0200

    Remove unused import

--- rapthor/scripts/make_region_file.py
+++ rapthor/scripts/make_region_file.py
@@ -6,7 +6,6 @@ Script to make a ds9 region file for use with WSClean and faceting
 from argparse import ArgumentParser, RawTextHelpFormatter
 import ast
 from lsmtool.facet import make_ds9_region_file, read_skymodel
-from rapthor.lib import miscellaneous as misc
 
 
 def main(
