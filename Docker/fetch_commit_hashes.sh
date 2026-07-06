df67648f8e796712dc5aefd97dc075982b6a68a8
Gemma Danks <gemma.danks@skao.int>
2026-07-06 20:48:49 +0100

    GEC-468: Pin DP3

--- Docker/fetch_commit_hashes.sh
+++ Docker/fetch_commit_hashes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 git ls-remote https://gitlab.com/aroffringa/aoflagger.git     HEAD | awk '{ print "AOFLAGGER_COMMIT="$1 }'
 git ls-remote https://github.com/casacore/casacore.git        HEAD | awk '{ print "CASACORE_COMMIT="$1 }'
-printf '%s\n' 'DP3_COMMIT=8bc500d20ee6fdb400ecdde7d8e6d3898836ba2c'
+printf '%s\n' 'DP3_COMMIT=5cc94dd01f091ffa7ebeef92fbfd942a86c73021'
 git ls-remote https://git.astron.nl/RD/EveryBeam.git          HEAD | awk '{ print "EVERYBEAM_COMMIT="$1 }'
 git ls-remote https://git.astron.nl/RD/idg.git                HEAD | awk '{ print "IDG_COMMIT="$1 }'
 git ls-remote https://github.com/casacore/python-casacore.git HEAD | awk '{ print "PYTHONCASACORE_COMMIT="$1 }'
