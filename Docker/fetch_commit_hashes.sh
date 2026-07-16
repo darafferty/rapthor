5fc9059ac00ca9a2b2a450e437d5f568b564f37a
Gemma Danks <gemma.danks@skao.int>
2026-07-16 17:59:40 +0100

    GEC-468: Pin SAGECal

--- Docker/fetch_commit_hashes.sh
+++ Docker/fetch_commit_hashes.sh
@@ -5,6 +5,7 @@ printf '%s\n' 'DP3_COMMIT=5cc94dd01f091ffa7ebeef92fbfd942a86c73021'
 git ls-remote https://git.astron.nl/RD/EveryBeam.git          HEAD | awk '{ print "EVERYBEAM_COMMIT="$1 }'
 git ls-remote https://git.astron.nl/RD/idg.git                HEAD | awk '{ print "IDG_COMMIT="$1 }'
 git ls-remote https://github.com/casacore/python-casacore.git HEAD | awk '{ print "PYTHONCASACORE_COMMIT="$1 }'
-git ls-remote https://github.com/nlesc-dirac/sagecal.git      HEAD | awk '{ print "SAGECAL_COMMIT="$1 }'
+# Keep libdirac compatible with Rapthor's GLib-free build.
+printf '%s\n' 'SAGECAL_COMMIT=33d21c45000bf13e5e29077ba3413405c42c503f'
 git ls-remote https://gitlab.com/aroffringa/wsclean.git       HEAD | awk '{ print "WSCLEAN_COMMIT="$1 }'
 printf '%s\n' 'WSCLEAN_COMMIT=8bc500d20ee6fdb400ecdde7d8e6d3898836ba2c'
