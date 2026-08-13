eb9ae90a23afc6814c34c673265d3981caf25853
Gemma Danks <gemma.danks@skao.int>
2026-08-13 12:20:51 +0000

    GEC-589: Pin EveryBeam

--- Docker/fetch_commit_hashes.sh
+++ Docker/fetch_commit_hashes.sh
@@ -2,7 +2,9 @@
 git ls-remote https://gitlab.com/aroffringa/aoflagger.git     HEAD | awk '{ print "AOFLAGGER_COMMIT="$1 }'
 git ls-remote https://github.com/casacore/casacore.git        HEAD | awk '{ print "CASACORE_COMMIT="$1 }'
 git ls-remote https://git.astron.nl/RD/DP3.git                18e793a4 | awk '{ print "DP3_COMMIT="$1 }'
-git ls-remote https://git.astron.nl/RD/EveryBeam.git          HEAD | awk '{ print "EVERYBEAM_COMMIT="$1 }'
+# git ls-remote https://git.astron.nl/RD/EveryBeam.git          HEAD | awk '{ print "EVERYBEAM_COMMIT="$1 }'
+# Pin EveryBeam for testing
+printf '%s\n' 'EVERYBEAM_COMMIT=d9f60948d8d002ac8994ec2b198538601da085e0'
 git ls-remote https://git.astron.nl/RD/idg.git                HEAD | awk '{ print "IDG_COMMIT="$1 }'
 git ls-remote https://github.com/casacore/python-casacore.git HEAD | awk '{ print "PYTHONCASACORE_COMMIT="$1 }'
 # Keep libdirac compatible with Rapthor's GLib-free build.
